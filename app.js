8bc607a6c18f68f03c4452fd385732b290a93124
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-01-30 19:17:21 +0700

    pembuatan Api

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://localhost/groovy');
 var index = require('./routes/index');
 var subscribe = require('./routes/subs');
 var employee = require('./routes/employee');
-
+var home = require('./routes/home');
 var app = express();
 
 // view engine setup
@@ -29,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/subscribe', subscribe);
 app.use('/employee', employee);
+app.use('/home', home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
