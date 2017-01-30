dde2881cb277258009b1427574e26307d57dda41
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-01-30 19:51:03 +0700

    pembuatan Api

---
+++ routes/package.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+var Package = require('../models/package');
+
+/* GET packageloye listing. */
+router.get('/listpackage', function(req, res, next) {
+     Package.find(function(err, packages) {
+       console.log( packages );
+       res.json(packages);
+   });
+});
+
+/* GET detail package. */
+router.get('/package/:id', function(req, res, next) {
+Package.findById(req.params.id, function(err, packages) {
+       console.log( packages );
+       res.json(packages);
+   });
+});
+
+/* Add package */
+router.post('/addpackage', function(req, res, next) {
+  var package = new package();
+
+    package.packageid= req.body.packageid;
+    package.propertiname= req.body.propertiname;
+    package.type= req.body.type;
+    //package.passwth= req.body.datebrith;
+    package.blokfloor= req.body.blokfloor;
+    package.nopackage= req.body.nopackage;
+    package.city= req.body.city;
+    package.fo= req.body.fo;
+
+    package.save(function(err) {
+      if (err)
+          res.send(err);
+      res.json({ message: 'Data created!' });
+  });
+});
+
+router.put('/putpackage/:id', function(req, res, next) {
+
+        Package.findById(req.params.id, function(err, package) {
+
+            if (err)
+                res.send(err);
+
+                package.packageid= req.body.packageid;
+                package.propertiname= req.body.propertiname;
+                package.type= req.body.type;
+                //package.passwth= req.body.datebrith;
+                package.blokfloor= req.body.blokfloor;
+                package.nopackage= req.body.nopackage;
+                package.city= req.body.city;
+                package.fo= req.body.fo;
+              if (err)
+                res.send(err);
+
+            package.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Data updated!' });
+            });
+        });
+});
+
+module.exports = router;
