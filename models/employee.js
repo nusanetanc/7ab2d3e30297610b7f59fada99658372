bced76f995e240f75e613bb1ef3ff03a25356939
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-01-30 16:09:48 +0700

    pembuatan Api

--- models/employee.js
+++ models/employee.js
@@ -1,13 +1,15 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var SubSchema   = new Schema({
+var EmpSchema   = new Schema({
     idemployee: Number,
     name: String,
+    email: String,
+    password: String,
     departement: String,
     titlejob: String,
     accessrole: String,
     photo: String
 });
 
-module.exports = mongoose.model('Sub', SubSchema);
+module.exports = mongoose.model('Emp', EmpSchema);
--- routes/employee.js
+++ routes/employee.js
@@ -1,21 +1,41 @@
 var express = require('express');
 var router = express.Router();
-var Sub = require('../models/subs')
+var Emp = require('../models/employee')
 
 /* GET employe listing. */
-router.get('/list', function(req, res, next) {
+router.get('/listemp', function(req, res, next) {
      Sub.find(function(err, subs) {
        console.log( subs );
        res.json(subs);
    });
 });
 
-/* GET detail employe listing. */
+/* GET detail employe. */
 router.get('/emp/:id', function(req, res, next) {
 Sub.findById(req.params.id, function(err, subs) {
        console.log( subs );
        res.json(subs);
    });
+});
+
+/* Add employe */
+router.post('/addemp', function(req, res, next) {
+  var emp = new ({
+    idemployee: req.body.idemployee,
+    name: req.body.name,
+    email: req.body.email,
+    password: passwordHash.generate(req.body.password)
+    departement: req.body.departement,
+    titlejob: req.body.titlejob,
+    accessrole: req.body.accessrole,
+    photo: req.body.photo,
+
+  })
+    sub.save(function(err) {
+      if (err)
+          res.send(err);
+      res.json({ message: 'SUB created!' });
+  });
 });
 
 module.exports = router;
