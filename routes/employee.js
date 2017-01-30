714df804e5795768183ce47525e1ad0b5d1c7625
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-01-30 16:35:29 +0700

    pembuatan Api

--- routes/employee.js
+++ routes/employee.js
@@ -20,14 +20,14 @@ Emp.findById(req.params.id, function(err, subs) {
 
 /* Add employe */
 router.post('/addemp', function(req, res, next) {
-    emp.idemployee= req.body.idemployee,
-    emp.name= req.body.name,
-    emp.email= req.body.email,
-    emp.password= passwordHash.generate(req.body.password),
-    emp.departement= req.body.departement,
-    emp.titlejob= req.body.titlejob,
-    emp.accessrole= req.body.accessrole,
-    emp.photo= req.body.photo
+    emp.idemployee= req.body.idemployee;
+    emp.name= req.body.name;
+    emp.email= req.body.email;
+    emp.password= passwordHash.generate(req.body.password);
+    emp.departement= req.body.departement;
+    emp.titlejob= req.body.titlejob;
+    emp.accessrole= req.body.accessrole;
+    emp.photo= req.body.photo;
 
     emp.save(function(err) {
       if (err)
