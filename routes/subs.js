dcc672fc1a78279c4f5f142d30d4853a06af330f
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-01-31 17:13:42 +0700

    perbaikan api

--- routes/subs.js
+++ routes/subs.js
@@ -3,7 +3,7 @@ var passwordHash = require('password-hash');
 var shortid = require('shortid');
 var router = express.Router();
 var Sub = require('../models/subs');
-var GenerateId = require("generate-id")
+var GenerateId = require("generate-id");
 
 /* GET subloye listing. */
 router.get('/listsub', function(req, res, next) {
@@ -24,7 +24,7 @@ Sub.findById(req.params.id, function(err, subs) {
 /* Add sub */
 router.post('/addsub', function(req, res, next) {
   var sub = new Sub();
-    sub.subid= GenerateId.generate(5);
+    sub.subid= new GenerateId();
     sub.name= req.body.name;
     sub.email= req.body.email;
     sub.password= passwordHash.generate(req.body.password);
