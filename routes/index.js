78c9cd3df3ac6edb6db602f320cb2f41b46c2545
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-01 17:47:47 +0700

    perbaikan api

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page.
+/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-app.use(subdomain('api', router));
 module.exports = router;
-*/
-//api specific routes
-router.get('/', function(req, res) {
-    res.send('Welcome to our API!');
-});
-
-router.get('/users', function(req, res) {
-    res.json([
-        { name: "Brian" }
-    ]);
-});
