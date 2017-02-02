7a22830d65c3ecb72e1a93121fb95d992acea7eb
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-02 10:54:40 +0700

    perbaikan api

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('homepage', { title: 'Groovy - Home' });
 });
 
+router.get('/features', function(req, res, next) {
+  res.render('features', { title: 'Groovy - Features' });
+});
+
+router.get('/packages', function(req, res, next) {
+  res.render('packages', { title: 'Groovy - Packages' });
+});
+
+router.get('/support', function(req, res, next) {
+  res.render('support', { title: 'Groovy - Support' });
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin', { title: 'Groovy - Signin' });
+});
 module.exports = router;
