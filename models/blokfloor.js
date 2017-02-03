fcb42968e0c2ab71ba284a205db7adfef5ceb449
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-03 14:18:45 +0700

    perbaikan

--- models/blokfloor.js
+++ models/blokfloor.js
@@ -6,4 +6,4 @@ var BlokfloorSchema   = new Schema({
   blokfloor: String,
     cluster: {type: Schema.Types.ObjectId, ref: 'Cluster'}
 });
-module.exports = mongoose.model('Blokfloor', BlokfloorSchema);
+module.exports = mongoose.model('Blokfloor', BlokfloorSchema)
