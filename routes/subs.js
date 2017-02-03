b74254cdbdaa61f2e4c612ffb2b2917c7615db90
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-03 15:04:18 +0700

    perbaikan

--- routes/subs.js
+++ routes/subs.js
@@ -21,7 +21,11 @@ Sub.findById(req.params.id, function(err, subs) {
 /* Add sub */
 router.post('/addsub', function(req, res, next) {
   var sub = new Sub();
-    sub.groovyid = require('node-sid')().create();
+    sub.groovyid = require('node-sid')({
+   seed:'0123456789abcdefghijklmnopqrstuvwxyz',
+   len:6,
+   headerField:'x-node-sid'
+ }).create();
     sub.name= req.body.name;
     sub.email= req.body.email;
     sub.password= passwordHash.generate(req.body.password);
