afaaf3ce20c5579d7e9f2e09439d178aa70c7631
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-03 14:23:08 +0700

    perbaikan

--- models/blokfloor.js
+++
@@ -1,9 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-var Cluster = require('../models/cluster');
-
-var BlokfloorSchema   = new Schema({
-  blokfloor: String,
-    cluster: {type: Schema.Types.ObjectId, ref: 'Cluster'}
-});
-module.exports = mongoose.model('Blokfloor', BlokfloorSchema)
--- models/city.js
+++
@@ -1,8 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var CitySchema   = new Schema({
-  city: String
-});
-
-module.exports = mongoose.model('City', CitySchema);
