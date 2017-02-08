6ae3d7b1926df7375b97e5f0d4bcee80a778dbda
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-08 16:32:49 +0700

    perubahan subid

--- routes/subs.js
+++ routes/subs.js
@@ -24,7 +24,8 @@ Sub.findById(req.params.id, function(err, subs) {
 /* Add sub */
 router.post('/addsub', function(req, res, next) {
   var sub = new Sub();
-    sub.subid = damm.append('45353');
+    var Random = randomInt(10000, 99999);
+    sub.subid = damm.append(Random);
     sub.name= req.body.name;
     sub.email= req.body.email;
     sub.password= passwordHash.generate(req.body.password);
