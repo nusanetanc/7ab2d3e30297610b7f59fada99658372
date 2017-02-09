df8729b916fad51e61288c37df15046e33f286bc
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-09 17:13:59 +0700

    perubahan subid

--- assets/app/is/app.component.ts
+++ assets/app/is/app.component.ts
@@ -1,10 +1,20 @@
-import {Component} from 'angular2/core';
+import { Component } from '@angular/core';
+import { Routes, ROUTER_DIRECTIVES } from '@angular/router';
+import {SigninComponent}  from "./user/signin.component";
+
 @Component({
     selector: 'my-app',
     template: `
         <h1>Hello World!</h1>
     `
 })
+@Routes([
+    { path: '/', component: MessagesComponent },
+    { path: '/signin', component: SigninComponent },
+    { path: '/signup', component: SignupComponent },
+    { path: '/profile', component: ProfileComponent }
+])
+
 export class AppComponentIs {
 
 }
---
+++ assets/app/is/user/signin.component.ts
@@ -0,0 +1,24 @@
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'my-app',
+    template: `
+    <div>
+      <div class="container container-auth">
+          <div class="top-margin text-center">
+              <img class="logo-size" src="images/groovy.png">
+              <div class="form">
+                  <div class="text">Sign in to continue to Information System</div>
+                  <div class="form-group">
+                      <input type="text" class="form-control" id="email" placeholder="Email">
+                      <input type="password" class="form-control" id="password" placeholder="Password">
+                  </div>
+                  <a href="#" class="btn button-submit">SIGN IN</a>
+                  <div class="text text-other"><a href="isforgot.html">I forgot password</a></div>
+              </div>
+          </div>
+      </div>
+    `
+})
+export class AppComponentIs {
+
+}
