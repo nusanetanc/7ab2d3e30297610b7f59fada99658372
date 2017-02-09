eb490f01ac3442645cb3244226fae323938d69eb
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-09 16:09:05 +0700

    perubahan subid

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
+import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>tes</h1>
+    template: `  
+        <h1>Hello World!</h1>
     `
 })
 export class AppComponent {
+
 }
--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -1,12 +1,5 @@
-///<reference path="../../typings.d.ts"/>
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { AppComponent } from "./app.component";
-//import { JobService } from "./messages/job.service";
-//import { AuthService } from "./user/auth.service";
-import {ErrorService} from "./errors/error.service";
-import { ROUTER_PROVIDERS,  } from '@angular/router'
-import { HTTP_PROVIDERS } from '@angular/http';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { provide } from '@angular/core';
+///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from "./app.component";
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy}), HTTP_PROVIDERS]);
+bootstrap(AppComponent);
