c0fe0031a2c7f1c59a40379f778068916060ec09
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-10 13:53:16 +0700

    perubahan subid

--- assets/app/is/app.component.ts
+++ assets/app/is/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from "angular2/core";
 import {ROUTER_DIRECTIVES, RouteConfig, Router} from 'angular2/router';
 import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import {Http} from 'angular2/http';
+import {SigninComponent} from "user/signin.component";
 
 @Component({
    selector: 'my-app',
