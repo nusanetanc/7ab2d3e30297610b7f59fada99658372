4924db527ffffe6853042a6ab25deb2acdf1a23d
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-20 19:43:40 +0700

    perbaikan background css

--- assets/app/is/dashboard/dashboard.component.ts
+++ assets/app/is/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, OnDestroy} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import {SidebarComponent} from "./sidebar.component";
+//import {SidebarComponent} from "./sidebar.component";
 import {ContentDashboardComponent} from "./content/dashboard.component";
 import {ContentAllSubsComponent} from "./content/allsubs.component";
 @Component({
@@ -8,11 +8,59 @@ import {ContentAllSubsComponent} from "./content/allsubs.component";
     template: `
     <!-- START CONTENT -->
     <div id="wrapper">
-      <sidebar></sidebar>
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+      <nav id="spy">
+        <ul class="sidebar-nav nav">
+          <li class="sidebar-brand">
+            <div class="title">
+              <img src="./images/ava.png" alt="ava">
+              <a href="" style="margin-top: 20px;"><strong>John Doe</strong></a>
+              <a href="" style="margin-top: -20px;">Acount Manager</a>
+            </div>
+          </li>
+
+          <li class="active" style="margin-top: 20px;">
+            <a routerLink="/" ><i class="material-icons">dashboard</i> <strong>DASHBOARD</strong></a>
+          </li>
+          <li>
+            <a role="button" data-toggle="collapse" [routerLink]="['AllSubs']" aria-expanded="false" aria-controls="collapseExample">
+              <i class="material-icons">supervisor_account</i> <strong>SUBSCRIBERS</strong>
+            </a>
+            <div class="collapse" id="subscribers">
+              <div class="listsub">
+                <a href="allsubscribers"><strong>ALL SUBSCRIBERS</strong></a>
+                <a href="newsubscribers"><strong>ADD NEW</strong></a>
+              </div>
+            </div>
+          </li>
+          <li>
+            <a href="reports"><i class="material-icons">announcement</i> <strong>REPORTS</strong></a>
+          </li>
+          <li>
+            <a [routerLink]="['AllSubs']" ><i class="material-icons">info</i> <strong>INFORMATION</strong></a>
+          </li>
+
+          <li class="sidebar-footer">
+            <div>
+              <img src="./images/groovy-grayscale.png" alt="ava">
+              <a href="">Privacy</a>
+              <a href="">Terms</a>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </div>
     </div><!-- END CONTENT -->
+    <main>
+    <router-outlet></router-outlet>
+    </main>
     `,
     directives: [ContentDashboardComponent,ContentAllSubsComponent, SidebarComponent,ROUTER_DIRECTIVES],
 })
+@RouteConfig([
+  { path: '/is/allsubs', component:ContentAllSubsComponent, name:'AllSubs'}
+])
 
 export class DashboardComponent {
 
