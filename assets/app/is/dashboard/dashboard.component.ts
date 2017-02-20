71f3e703d84bb3cd996d9324481903385dcc2539
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-20 14:13:51 +0700

    perbaikan background css

--- assets/app/is/dashboard/dashboard.component.ts
+++ assets/app/is/dashboard/dashboard.component.ts
@@ -1,55 +1,13 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
-//import {SidebarComponent} from "./sidebar.component";
+import {SidebarComponent} from "./sidebar.component";
 //import {ContentDashboardComponent} from "./content/content.component";
 @Component({
     selector: 'dashboard',
     template: `
-    <!-- Sidebar -->
-    <div id="sidebar-wrapper">
-      <nav id="spy">
-        <ul class="sidebar-nav nav">
-          <li class="sidebar-brand">
-            <div class="title">
-              <img src="ava.png" alt="ava">
-              <a href="" style="margin-top: 20px;"><strong>John Doe</strong></a>
-              <a href="" style="margin-top: -20px;">Acount Manager</a>
-            </div>
-          </li>
-
-          <li class="active" style="margin-top: 20px;">
-            <a href="index.html" ><i class="material-icons">dashboard</i> <strong>DASHBOARD</strong></a>
-          </li>
-          <li>
-            <a role="button" data-toggle="collapse" href="#subscribers" aria-expanded="false" aria-controls="collapseExample">
-              <i class="material-icons">supervisor_account</i> <strong>SUBSCRIBERS</strong>
-            </a>
-            <div class="collapse" id="subscribers">
-              <div class="listsub">
-                <a href="allsubscribers"><strong>ALL SUBSCRIBERS</strong></a>
-                <a href="newsubscribers"><strong>ADD NEW</strong></a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <a href="reports"><i class="material-icons">announcement</i> <strong>REPORTS</strong></a>
-          </li>
-          <li>
-            <a href="information"><i class="material-icons">info</i> <strong>INFORMATION</strong></a>
-          </li>
-
-          <li class="sidebar-footer">
-            <div>
-              <img src="groovy-grayscale.png" alt="ava">
-              <a href="">Privacy</a>
-              <a href="">Terms</a>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <sidebar></sidebar>
     `,
-    directives: [ROUTER_DIRECTIVES],
+    directives: [SidebarComponent,ROUTER_DIRECTIVES],
 })
 export class DashboardComponent {
 
