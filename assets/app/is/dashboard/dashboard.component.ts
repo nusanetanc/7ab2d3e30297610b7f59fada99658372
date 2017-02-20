1efce0e62c858c1fd48cc06efe78f2fe1c1a90b7
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-20 19:46:51 +0700

    perbaikan background css

--- assets/app/is/dashboard/dashboard.component.ts
+++ assets/app/is/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, OnDestroy} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
+import {Http} from 'angular2/http';
 //import {SidebarComponent} from "./sidebar.component";
 import {ContentDashboardComponent} from "./content/dashboard.component";
 import {ContentAllSubsComponent} from "./content/allsubs.component";
