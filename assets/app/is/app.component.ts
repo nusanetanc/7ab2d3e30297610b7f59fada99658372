f040ad051e0001c457251f047a434cde23f7e7cb
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-20 21:00:49 +0700

    perbaikan background css

--- assets/app/is/app.component.ts
+++ assets/app/is/app.component.ts
@@ -47,6 +47,7 @@ import {ContentAllSubsComponent} from "./dashboard/content/allsubs.component";
                         <div>
                           <img src="./images/groovy-grayscale.png" alt="ava">
                           <a href="">Privacy</a>
+                          <a [routerLink]="['All']">INFORMATION</a>
                           <a href="">Terms</a>
                         </div>
                       </li>
