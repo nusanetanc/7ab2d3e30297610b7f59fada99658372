230776801c7f408a1ad8c3dce9367dbd8454ab03
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-20 20:41:52 +0700

    perbaikan background css

--- assets/app/is/app.component.ts
+++ assets/app/is/app.component.ts
@@ -10,6 +10,49 @@ import {ContentAllSubsComponent} from "./dashboard/content/allsubs.component";
     template: `
             <header>
                 <a [routerLink]="['All']">Login</a>
+                <!-- Sidebar -->
+                <div id="sidebar-wrapper">
+                  <nav id="spy">
+                    <ul class="sidebar-nav nav">
+                      <li class="sidebar-brand">
+                        <div class="title">
+                          <img src="./images/ava.png" alt="ava">
+                          <a href="" style="margin-top: 20px;"><strong>John Doe</strong></a>
+                          <a href="" style="margin-top: -20px;">Acount Manager</a>
+                        </div>
+                      </li>
+
+                      <li class="active" style="margin-top: 20px;">
+                        <a routerLink="/" ><i class="material-icons">dashboard</i> <strong>DASHBOARD</strong></a>
+                      </li>
+                      <li>
+                        <a role="button" data-toggle="collapse" [routerLink]="['AllSubs']" aria-expanded="false" aria-controls="collapseExample">
+                          <i class="material-icons">supervisor_account</i> <strong>SUBSCRIBERS</strong>
+                        </a>
+                        <div class="collapse" id="subscribers">
+                          <div class="listsub">
+                            <a href="allsubscribers"><strong>ALL SUBSCRIBERS</strong></a>
+                            <a href="newsubscribers"><strong>ADD NEW</strong></a>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <a href="reports"><i class="material-icons">announcement</i> <strong>REPORTS</strong></a>
+                      </li>
+                      <li>
+                        <a href="information"><i class="material-icons">info</i> <strong>INFORMATION</strong></a>
+                      </li>
+
+                      <li class="sidebar-footer">
+                        <div>
+                          <img src="./images/groovy-grayscale.png" alt="ava">
+                          <a href="">Privacy</a>
+                          <a href="">Terms</a>
+                        </div>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
            </header>
             <main>
               <router-outlet></router-outlet>
