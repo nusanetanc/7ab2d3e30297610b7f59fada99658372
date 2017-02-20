652c451b0cfd8d493f04865d865367e7cf9fa439
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-20 14:56:47 +0700

    perbaikan background css

--- assets/app/is/dashboard/content/content.component.ts
+++ assets/app/is/dashboard/content/content.component.ts
@@ -4,28 +4,32 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 @Component({
     selector: 'form-dashboard',
     template: `
-    <div id="page-content-wrapper">
-      <div class="content-header">
-          <h3 id="home">
-            <a id="menu-toggle" href="#" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
-            </a>
-              &nbsp; Dashboard
-          </h3>
+    <!-- Page content -->
+<div id="page-content-wrapper">
+  <div class="content-header">
+      <h3 id="home">
+        <a id="menu-toggle" href="#" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
+        </a>
+          &nbsp; Dashboard
+      </h3>
 
-      </div>
+  </div>
 
-      <div class="page-content inset" data-spy="scroll" data-target="#spy">
-        <!-- <div class="row">
+  <div class="page-content inset" data-spy="scroll" data-target="#spy">
+    <!-- <div class="row">
 
-          <div class="jumbotron text-center" >
-            <h1>Hello Beautiful!</h1>
-            <p>This is a sidebar navigation responsive template built off of Bootstrap 3.0 and simple sidebar template. It includes anchors, scroll spy, smooth scroll, and Awesome icon fonts.</p>
+      <div class="jumbotron text-center" >
+        <h1>Hello Beautiful!</h1>
+        <p>This is a sidebar navigation responsive template built off of Bootstrap 3.0 and simple sidebar template. It includes anchors, scroll spy, smooth scroll, and Awesome icon fonts.</p>
 
-          </div> -->
+      </div> -->
 
     </div>
+  </div>
 
-    </div>
+</div>
+
+</div><!-- END CONTENT -->
     `,
     directives: [ROUTER_DIRECTIVES],
 })
