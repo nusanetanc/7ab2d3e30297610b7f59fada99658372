d4be944c64d003114ffb8deb6a23a4d6dc44b263
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-20 10:17:42 +0700

    perbaikan background css

--- assets/app/is/user/authentication/isforgot.component.ts
+++ assets/app/is/user/authentication/isforgot.component.ts
@@ -3,15 +3,17 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 @Component({
     selector: 'form-login',
     template: `
-                <div class="form">
-                    <div class="text">Sign in to continue to Information System</div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="email" placeholder="Email">
-                        <input type="password" class="form-control" id="password" placeholder="Password">
-                    </div>
-                    <a href="#" class="btn button-submit">SIGN IN</a>
-                    <div class="text text-other"><a href="isforgot">I forgot password</a></div>
+    <div class="container container-auth">
+        <div class="top-margin text-center">
+            <img class="logo-size" src="images/groovy.png">
+            <div class="form">
+                <div class="text">Help is on the way, reset password link has been sent to your email</div>
+                <div class="button-resend">
+                    <button type="submit" class="btn">RESEND</button>
                 </div>
+            </div>
+        </div>
+    </div>
     `,
     directives: [ROUTER_DIRECTIVES],
 })
