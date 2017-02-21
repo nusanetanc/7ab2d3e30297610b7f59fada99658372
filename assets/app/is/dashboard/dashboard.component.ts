fad1eabb41c222d302a68b4d5a00c48f255348eb
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-21 13:35:37 +0700

    perbaikan router

--- assets/app/is/dashboard/dashboard.component.ts
+++ assets/app/is/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, OnDestroy} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {RouteConfig, ROUTER_DIRECTIVES, FORM_PROVIDERS} from 'angular2/router';
 import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import {Http} from 'angular2/http';
 import {ContentDashboardComponent} from "./content/dashboard.component";
@@ -47,7 +47,7 @@ import {ContentAllSubsComponent} from "./content/allsubs.component";
                 <router-outlet></router-outlet>
         </div><!-- END CONTENT -->
     `,
-    directives: [ContentAllSubsComponent, ContentDashboardComponent, ROUTER_DIRECTIVES],
+    directives: [ContentAllSubsComponent, ContentDashboardComponent, ROUTER_DIRECTIVES, FORM_PROVIDERS],
 })
 @RouteConfig([
   { path: '/is/allsubs', component:ContentAllSubsComponent, name:'AllSubs'},
