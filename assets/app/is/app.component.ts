ae39af0b5fdb2099549bcb758adf7877dd825713
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-21 13:25:55 +0700

    perbaikan router

--- assets/app/is/app.component.ts
+++ assets/app/is/app.component.ts
@@ -4,13 +4,15 @@ import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import {Http} from 'angular2/http';
 import {AuthenticationComponent} from "./authentication/authentication.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {ContentDashboardComponent} from "./content/dashboard.component";
+import {ContentAllSubsComponent} from "./content/allsubs.component";
 
 @Component({
    selector: 'is-app',
    template: `
     <dashboard></dashboard>
 `,
-    directives: [DashboardComponent, ROUTER_DIRECTIVES]
+    directives: [DashboardComponent, ContentAllSubsComponent, ContentDashboardComponent, ROUTER_DIRECTIVES]
 })
 
 export class AppComponent {
