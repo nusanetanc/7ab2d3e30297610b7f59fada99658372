5d512153e514c18f0df9f30e5cb249b986f396a1
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-21 11:39:46 +0700

    perbaikan router

--- assets/app/is/dashboard/dashboard.component.ts
+++ assets/app/is/dashboard/dashboard.component.ts
@@ -3,67 +3,58 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import {Http} from 'angular2/http';
 //import {SidebarComponent} from "./sidebar.component";
-import {ContentDashboardComponent} from "./content/dashboard.component";
-import {ContentAllSubsComponent} from "./content/allsubs.component";
+import {ContentDashboardComponent} from "./dashboard/content/dashboard.component";
+import {ContentAllSubsComponent} from "./dashboard/content/allsubs.component";
 @Component({
-    selector: 'dashboard',
+    selector: 'is-app',
     template: `
     <!-- START CONTENT -->
-    <div id="wrapper">
-    <!-- Sidebar -->
-    <div id="sidebar-wrapper">
-      <nav id="spy">
-        <ul class="sidebar-nav nav">
-          <li class="sidebar-brand">
-            <div class="title">
-              <img src="./images/ava.png" alt="ava">
-              <a href="" style="margin-top: 20px;"><strong>John Doe</strong></a>
-              <a href="" style="margin-top: -20px;">Acount Manager</a>
-            </div>
-          </li>
+      <div id="wrapper">
+                <!-- Sidebar -->
+                <div id="sidebar-wrapper">
+                  <nav id="spy">
+                    <ul class="sidebar-nav nav">
+                      <li class="sidebar-brand">
+                        <div class="title">
+                          <img src="./images/ava.png" alt="ava">
+                          <a href="" style="margin-top: 20px;"><strong>John Doe</strong></a>
+                          <a href="" style="margin-top: -20px;">Acount Manager</a>
+                        </div>
+                      </li>
+                      <li style="margin-top: 10px;">
+                          <a [routerLink]="['Dashboard']" class="collapse"><i class="material-icons">dashboard</i><strong>DASHBOARD</strong></a>
+                      </li>
+                      <li  style="margin-top: 10px;">
+                          <a [routerLink]="['AllSubs']" class="collapse"><i class="material-icons">supervisor_account</i><strong>ALL SUBSCRIBERS</strong></a>
+                      </li>
+                      <li style="margin-top: 10px;">
+                          <a [routerLink]="['AddSubs']" class="collapse"><i class="material-icons">dashboard</i><strong>ADD SUBSCRIBERS</strong></a>
+                      </li>
+                      <li style="margin-top: 10px;">
+                        <a href="reports"><i class="material-icons">announcement</i> <strong>REPORTS</strong></a>
+                      </li>
 
-          <li class="active" style="margin-top: 20px;">
-            <a routerLink="/" ><i class="material-icons">dashboard</i> <strong>DASHBOARD</strong></a>
-          </li>
-          <li>
-            <a role="button" data-toggle="collapse" [routerLink]="['AllSubs']" aria-expanded="false" aria-controls="collapseExample">
-              <i class="material-icons">supervisor_account</i> <strong>SUBSCRIBERS</strong>
-            </a>
-            <div class="collapse" id="subscribers">
-              <div class="listsub">
-                <a href="allsubscribers"><strong>ALL SUBSCRIBERS</strong></a>
-                <a href="newsubscribers"><strong>ADD NEW</strong></a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <a href="reports"><i class="material-icons">announcement</i> <strong>REPORTS</strong></a>
-          </li>
-          <li>
-            <a [routerLink]="['AllSubs']" ><i class="material-icons">info</i> <strong>INFORMATION</strong></a>
-          </li>
+                      <li class="sidebar-footer">
+                        <div>
+                          <img src="./images/groovy-grayscale.png" alt="ava">
+                          <a href="">Privacy</a>
 
-          <li class="sidebar-footer">
-            <div>
-              <img src="./images/groovy-grayscale.png" alt="ava">
-              <a href="">Privacy</a>
-              <a href="">Terms</a>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    </div><!-- END CONTENT -->
-    <main>
-    <router-outlet></router-outlet>
-    </main>
+                          <a href="">Terms</a>
+                        </div>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              <router-outlet></router-outlet>
+        </div><!-- END CONTENT -->
     `,
-    directives: [ContentDashboardComponent,ContentAllSubsComponent, ROUTER_DIRECTIVES],
+    directives: [ContentAllSubsComponent, ContentDashboardComponent, ROUTER_DIRECTIVES],
 })
 @RouteConfig([
-  { path: '/is/allsubs', component:ContentAllSubsComponent, name:'AllSubs'}
+  { path: '/is/allsubs', component:ContentAllSubsComponent, name:'AllSubs'},
+  { path: '/is', component:ContentDashboardComponent, name:'Dashboard'}
 ])
 
-export class DashboardComponent {
+export class AppComponent {
 
 }
--- assets/app/is/dashboard/sidebar.component.ts
+++ assets/app/is/dashboard/sidebar.component.ts
@@ -1,60 +1,57 @@
-import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES, RouteConfig, Router} from 'angular2/router';
-import {ContentAllSubsComponent} from "./content/allsubs.component";
+import {Component, OnInit, OnDestroy} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
+import {Http} from 'angular2/http';
+//import {SidebarComponent} from "./sidebar.component";
+import {ContentDashboardComponent} from "./dashboard/content/dashboard.component";
+import {ContentAllSubsComponent} from "./dashboard/content/allsubs.component";
 @Component({
-    selector: 'sidebar',
+    selector: 'is-app',
     template: `
-      <!-- Sidebar -->
-      <div id="sidebar-wrapper">
-        <nav id="spy">
-          <ul class="sidebar-nav nav">
-            <li class="sidebar-brand">
-              <div class="title">
-                <img src="./images/ava.png" alt="ava">
-                <a href="" style="margin-top: 20px;"><strong>John Doe</strong></a>
-                <a href="" style="margin-top: -20px;">Acount Manager</a>
-              </div>
-            </li>
+                <!-- Sidebar -->
+                <div id="sidebar-wrapper">
+                  <nav id="spy">
+                    <ul class="sidebar-nav nav">
+                      <li class="sidebar-brand">
+                        <div class="title">
+                          <img src="./images/ava.png" alt="ava">
+                          <a href="" style="margin-top: 20px;"><strong>John Doe</strong></a>
+                          <a href="" style="margin-top: -20px;">Acount Manager</a>
+                        </div>
+                      </li>
+                      <li style="margin-top: 10px;">
+                          <a [routerLink]="['Dashboard']" class="collapse"><i class="material-icons">dashboard</i><strong>DASHBOARD</strong></a>
+                      </li>
+                      <li  style="margin-top: 10px;">
+                          <a [routerLink]="['AllSubs']" class="collapse"><i class="material-icons">supervisor_account</i><strong>ALL SUBSCRIBERS</strong></a>
+                      </li>
+                      <li style="margin-top: 10px;">
+                          <a [routerLink]="['AddSubs']" class="collapse"><i class="material-icons">dashboard</i><strong>ADD SUBSCRIBERS</strong></a>
+                      </li>
+                      <li style="margin-top: 10px;">
+                        <a href="reports"><i class="material-icons">announcement</i> <strong>REPORTS</strong></a>
+                      </li>
 
-            <li class="active" style="margin-top: 20px;">
-              <a routerLink="/" ><i class="material-icons">dashboard</i> <strong>DASHBOARD</strong></a>
-            </li>
-            <li>
-              <a role="button" data-toggle="collapse" [routerLink]="['AllSubs']" aria-expanded="false" aria-controls="collapseExample">
-                <i class="material-icons">supervisor_account</i> <strong>SUBSCRIBERS</strong>
-              </a>
-              <div class="collapse" id="subscribers">
-                <div class="listsub">
-                  <a href="allsubscribers"><strong>ALL SUBSCRIBERS</strong></a>
-                  <a href="newsubscribers"><strong>ADD NEW</strong></a>
+                      <li class="sidebar-footer">
+                        <div>
+                          <img src="./images/groovy-grayscale.png" alt="ava">
+                          <a href="">Privacy</a>
+
+                          <a href="">Terms</a>
+                        </div>
+                      </li>
+                    </ul>
+                  </nav>
                 </div>
-              </div>
-            </li>
-            <li>
-              <a href="reports"><i class="material-icons">announcement</i> <strong>REPORTS</strong></a>
-            </li>
-            <li>
-              <a href="information"><i class="material-icons">info</i> <strong>INFORMATION</strong></a>
-            </li>
-
-            <li class="sidebar-footer">
-              <div>
-                <img src="./images/groovy-grayscale.png" alt="ava">
-                <a href="">Privacy</a>
-                <a href="">Terms</a>
-              </div>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <router-outlet></router-outlet>
-
+              <router-outlet></router-outlet>
     `,
-    directives: [ROUTER_DIRECTIVES, ContentAllSubsComponent],
+    directives: [ContentAllSubsComponent, ContentDashboardComponent, ROUTER_DIRECTIVES],
 })
 @RouteConfig([
-  { path: '/is/allsubs', component:ContentAllSubsComponent, name:'AllSubs'}
+  { path: '/is/allsubs', component:ContentAllSubsComponent, name:'AllSubs'},
+  { path: '/is', component:ContentDashboardComponent, name:'Dashboard'}
 ])
-export class SidebarComponent {
+
+export class AppComponent {
 
 }
