19e284187bc920eeb7dfbf640953fa11747cee2a
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-21 13:38:49 +0700

    perbaikan router

--- assets/app/is/dashboard/dashboard.component.ts
+++ assets/app/is/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, OnDestroy} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES, FORM_PROVIDERS} from 'angular2/router';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import {Http} from 'angular2/http';
 import {ContentDashboardComponent} from "./content/dashboard.component";
