cd74e9ad8423e19bdeb509d7d1887c3f0986dfb4
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-24 09:38:58 +0700

    perbaikan background css0

--- assets/app/is/dashboard/content/information.component.ts
+++ assets/app/is/dashboard/content/information.component.ts
@@ -35,16 +35,6 @@ import 'rxjs/add/operator/map';
             <div class="row">
                 <div class="col-sm-12">
                   <div class="row subInfo">
-                      <div class="col-sm-2 invoiceId"><span><a href="information-detail.html" class="linkGrey">11 Feb 2017</a></span></div>
-                      <div class="col-sm-8 invoiceList"><span><a href="information-detail.html" class="linkGrey">Fiber Optic Network Disruption</a></span></div>
-                      <div class="col-sm-2 invoiceList"><span style="color: red;">On Progress</span></div>
-                  </div>
-                  <div class="row subInfo">
-                      <div class="col-sm-2 invoiceId"><span>09 Feb 2017</span></div>
-                      <div class="col-sm-8 invoiceList" style="padding: 20px 0px 20px 0px;"><span>Network Server Maintenance</span></div>
-                      <div class="col-sm-2 invoiceList" style="padding: 20px 0px 20px 0px;"><span style="color: green;">Solved</span></div>
-                  </div>
-                  <div class="row subInfo">
                       <div class="col-sm-2 invoiceId" style="padding: 20px 0px 20px 35px;"><span>21 Jan 2017</span></div>
                       <div class="col-sm-8 invoiceList" style="padding: 20px 0px 20px 0px;"><span>Welcome to Groovy</span></div>
                       <div class="col-sm-2 invoiceList" style="padding: 20px 0px 20px 0px;"><span></span></div>
