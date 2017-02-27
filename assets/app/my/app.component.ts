d81b5f767672363f502b859f46d7efb699e1c985
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-27 10:51:20 +0700

    perbaikan background css0

--- assets/app/my/app.component.ts
+++ assets/app/my/app.component.ts
@@ -2,15 +2,25 @@ import {Component, OnInit, OnDestroy} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import {Http} from 'angular2/http';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {ContentDashboardComponent} from "./dashboard/content/dashboard.component";
 //import {AuthenticationComponent} from "./authentication/authentication.component";
-//import {DashboardComponent} from "./dashboard/dashboard.component";
+/import {ContentDashboardComponent} from "./dashboard/dashboard.component";
 
 @Component({
    selector: 'my-app',
    template: `
-<h1>halo</h1>
+   <!-- START CONTENT -->
+     <div id="wrapper">
+    <dashboard></dashboard>
+    <router-outlet></router-outlet>
+  </div><!-- END CONTENT -->
 `,
+    directives: [DashboardComponent, ContentDashboardComponent,  ROUTER_DIRECTIVES],
 })
+@RouteConfig([
+  { path: '/my', component:ContentDashboardComponent, name:'Dashboard'}
+])
 
 export class AppComponent {
 
