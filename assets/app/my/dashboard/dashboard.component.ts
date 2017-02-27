7889ce89ad4023c330fedb217ee2f6a066d921be
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-27 11:38:25 +0700

    perbaikan background css0

--- assets/app/my/dashboard/dashboard.component.ts
+++ assets/app/my/dashboard/dashboard.component.ts
@@ -5,41 +5,42 @@ import {Http} from 'angular2/http';
 @Component({
     selector: 'dashboard',
     template: `
-                <!-- Sidebar -->
-                <div id="sidebar-wrapper">
-                  <nav id="spy">
-                    <ul class="sidebar-nav nav">
+    <!-- Sidebar -->
+          <div id="sidebar-wrapper">
+              <nav id="spy">
+                  <ul class="sidebar-nav nav">
                       <li class="sidebar-brand">
-                        <div class="title">
-                          <img src="./images/ava.png" alt="ava">
-                          <a href="" style="margin-top: 20px;"><strong>John Doe</strong></a>
-                          <a href="" style="margin-top: -20px;">Acount Manager</a>
-                        </div>
+                          <div class="title">
+                              <img src="ava.png" alt="ava">
+                              <a class="name" href="account.html"><strong>John Doe</strong></a>
+                              <a class="user" href="account.html">Subscriber - Level 1</a>
+                          </div>
                       </li>
-                      <li style="margin-top: 10px;">
-                          <a [routerLink]="['Dashboard']" class="collapse"><i class="material-icons">dashboard</i><strong>DASHBOARD</strong></a>
+
+                      <li class="active firstLiSidebar">
+                          <a [routerLink]="['Dashboard']" class="collapse" ><i class="material-icons">dashboard</i> <strong>DASHBOARD</strong></a>
                       </li>
-                      <li  style="margin-top: 10px;">
-                          <a [routerLink]="['AllSubs']" class="collapse"><i class="material-icons">supervisor_account</i><strong>ALL SUBSCRIBERS</strong></a>
+                      <li>
+                          <a [routerLink]="['Billing']" class="collapse"><i class="material-icons">supervisor_account</i> <strong>BILLING</strong></a>
                       </li>
-                      <li style="margin-top: 10px;">
-                          <a [routerLink]="['AddSubs']" class="collapse"><i class="material-icons">dashboard</i><strong>ADD SUBSCRIBERS</strong></a>
+                      <li>
+                          <a [routerLink]="['Reports']" class="collapse"><i class="material-icons">announcement</i> <strong>REPORTS</strong></a>
                       </li>
-                      <li style="margin-top: 10px;">
-                        <a href="reports"><i class="material-icons">announcement</i> <strong>REPORTS</strong></a>
+                      <li>
+                          <a [routerLink]="['Reports']" class="collapse"><i class="material-icons">info</i> <strong>INFORMATION</strong></a>
                       </li>
 
                       <li class="sidebar-footer">
-                        <div>
-                          <img src="./images/groovy-grayscale.png" alt="ava">
-                          <a href="">Privacy</a>
-
-                          <a href="">Terms</a>
-                        </div>
+                          <div>
+                              <img src="groovy-grayscale.png" alt="ava">
+                              <a href="">Privacy</a>
+                              <a href="">Terms</a>
+                          </div>
                       </li>
-                    </ul>
-                  </nav>
-                </div>
+                  </ul>
+              </nav>
+          </div>
+          <!-- /Sidebar -->
     `,
     directives: [ ROUTER_DIRECTIVES], providers: [],
 })
