d364aa5e781c8bc49381d6d8f9c61752c72edf48
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-27 09:53:08 +0700

    perbaikan background css0

--- assets/app/my/app.component.ts
+++ assets/app/my/app.component.ts
@@ -2,19 +2,14 @@ import {Component, OnInit, OnDestroy} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import {Http} from 'angular2/http';
-import {AuthenticationComponent} from "./authentication/authentication.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
+//import {AuthenticationComponent} from "./authentication/authentication.component";
+//import {DashboardComponent} from "./dashboard/dashboard.component";
 
 @Component({
    selector: 'my-app',
    template: `
-   <!-- START CONTENT -->
-     <div id="wrapper">
-    <dashboard></dashboard>
-
-  </div><!-- END CONTENT -->
+<h1>halo</h1>
 `,
-    directives: [DashboardComponent,ROUTER_DIRECTIVES],
 })
 
 export class AppComponent {
