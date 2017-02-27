72f679f860233e264d01cbdac9b57b34d2b8789d
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-27 10:59:10 +0700

    perbaikan background css0

--- assets/app/my/app.component.ts
+++ assets/app/my/app.component.ts
@@ -3,7 +3,7 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
 import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import {Http} from 'angular2/http';
 import {DashboardComponent} from "./dashboard/dashboard.component";
-import {ContentDashboardComponent} from "./dashboard/content/dashboard.component";
+//import {ContentDashboardComponent} from "./dashboard/content/dashboard.component";
 
 @Component({
    selector: 'my-app',
@@ -14,12 +14,8 @@ import {ContentDashboardComponent} from "./dashboard/content/dashboard.component
     <router-outlet></router-outlet>
   </div><!-- END CONTENT -->
 `,
-    directives: [DashboardComponent, ContentDashboardComponent,  ROUTER_DIRECTIVES],
+    directives: [DashboardComponent,  ROUTER_DIRECTIVES],
 })
-@RouteConfig([
-  { path: '/my', component:ContentDashboardComponent, name:'Dashboard'}
-])
-
 export class AppComponent {
 
 }
