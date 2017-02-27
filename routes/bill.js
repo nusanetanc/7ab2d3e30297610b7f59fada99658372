d232cd53dd5cd294ce96a14885fd132f9d59c13e
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-27 17:10:36 +0700

    perbaikan background css0

--- routes/bill.js
+++ routes/bill.js
@@ -21,7 +21,7 @@ Bill.findById(req.params.id, function(err, bills) {
 
 /* GET detail bill one account. */
 router.get('/subbill/:sub', function(req, res, next) {
-Bill.findBysub(req.params.sub, function(err, bills) {
+Bill.findSub(req.params.sub, function(err, bills) {
        console.log( bills );
        res.json(bills);
    });
