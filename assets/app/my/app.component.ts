ed8e8dd50a4597dd259d3256c1c7a5664b865542
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-02-27 14:52:58 +0700

    perbaikan background css0

--- assets/app/my/app.component.ts
+++ assets/app/my/app.component.ts
@@ -4,8 +4,6 @@ import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import {Http} from 'angular2/http';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {ContentDashboardComponent} from "./dashboard/content/dashboard.component";
-import 'rxjs/add/operator/map';
-import { Sub } from './dashboard/content/subs';
 
 @Component({
    selector: 'my-app',
@@ -23,23 +21,5 @@ import { Sub } from './dashboard/content/subs';
 ])
 
 export class AppComponent {
-// Link to our api, pointing to localhost
-  API = 'http://202.162.207.164:3000';
-  Session_ID = '58b3cdac45912d052e2c85a5';
-
-subs: any[] = [];
-  constructor(private http: Http) {}
-
-  ngOnInit() {
-    this.getAcountSub();
-  }
-
-getAcountSub() {
-  this.http.get(`${this.API}/subscribe/sub/${this.Session_ID}`)
-    .map(res => res.json())
-    .subscribe(subs => {
-      this.subs = subs
-    })
-}
 
 }
