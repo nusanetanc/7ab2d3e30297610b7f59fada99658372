e717c6db392d62ff52fe62d6119299fdb42f14c7
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-02 14:18:20 +0700

    perbaikan background css

--- assets/app/is/dashboard/content/addsubs.component.ts
+++ assets/app/is/dashboard/content/addsubs.component.ts
@@ -33,7 +33,7 @@ import { Sub } from './subs';
                             <div class="col-sm-12 paddingL35">
                                 <form class="paddingTB20 paddingR30">
                                     <div class="form-group">
-                                        <input ng-control="subname" type="text" class="form-control inputForm" placeholder="Full Name">
+                                        <input ng-control="{{subname}}" type="text" class="form-control inputForm" placeholder="Full Name">
                                         <input type="text" class="form-control inputForm" id="exampleInputHp" placeholder="Handphone">
                                         <input type="email" class="form-control inputForm" id="exampleInputEmail1" placeholder="Email">
                                         <p>Upload your National Identity Card</p>
@@ -183,11 +183,10 @@ export class ContentAddSubsComponent implements OnInit {
   // Angular 2 Life Cycle event when component has been initialized
   ngOnInit() {
     this.getAllSub();
-    this.addSub(myname);
   }
 
 // Add one person to the API
-  addSub(myname) {
+  addSub(subname : String) {
   var body = 'name=subname';
   var headers = new Headers();
   headers.append('Content-Type', 'application/x-www-form-urlencoded');
