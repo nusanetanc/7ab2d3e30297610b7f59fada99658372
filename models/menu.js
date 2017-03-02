8c750bd04dc72300b3a2973662bfec6f7110fc32
ferdianrb <frizki7@gmail.com>
2017-03-02 16:34:24 +0700

    dashboard menu api

--- models/menu.js
+++ models/menu.js
@@ -1,13 +1,11 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
-var Emp = require('../models/employee');
 
 var MenuSchema   = new Schema({
   title: String,
   file: String,
   icons: String,
   access: String,
-  emp_id: {type: Schema.Types.ObjectId, ref: 'Emp'}
 });
 
 module.exports = mongoose.model('Menu', MenuSchema);
