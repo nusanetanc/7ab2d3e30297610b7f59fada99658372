c85b9b273b5b359f70db765f36453e691d919a1b
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-02 11:11:10 +0700

    perbaikan background css

--- assets/app/is/dashboard/content/allsubs.component.ts
+++ assets/app/is/dashboard/content/allsubs.component.ts
@@ -39,7 +39,7 @@ import { Sub } from './subs';
                 <div class="row">
                     <div class="col-sm-12"  *ngFor="#sub of subs">
                         <div class="row subInfo">
-                            <div class="col-sm-2 invoiceId"><span>GR-123456</span></div>
+                            <div class="col-sm-2 invoiceId"><span>{{ sub.subid }}</span></div>
                             <div class="col-sm-8 invoiceList"><span><a class="grey333">{{ sub.name }}</a></span></div>
                             <div class="col-sm-1 invoiceList"><span class="green">{{ sub.status }}</span></div>
                             <div class="col-sm-1 invoiceList"><span class="red">Not Paid</span></div>
