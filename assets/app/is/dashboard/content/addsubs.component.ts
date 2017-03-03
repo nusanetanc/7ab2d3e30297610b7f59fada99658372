0b3440628746dc25854b26bb78dfb16bbfaf30b6
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-03 11:00:24 +0700

    perbaikan background css

--- assets/app/is/dashboard/content/addsubs.component.ts
+++ assets/app/is/dashboard/content/addsubs.component.ts
@@ -188,7 +188,7 @@ export class ContentAddSubsComponent implements OnInit {
 // Add one person to the API
   addSub(subname.value) {
 
-  var body = 'name='+this.subname;
+  var body = 'name='+${this.subname};
   var headers = new Headers();
   headers.append('Content-Type', 'application/x-www-form-urlencoded');
     this.http
