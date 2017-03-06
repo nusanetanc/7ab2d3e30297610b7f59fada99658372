4db7e2d8628274278a3189c6f444c0d2a91c62c2
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-06 14:51:38 +0700

    perbaikan background css

--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -157,7 +157,7 @@ export class ContentSubscribeComponent {
 
   // Get all users from the API
   getSub() {
-    this.http.get(`${this.API}/subscribe/sub/{req.params.id}`)
+    this.http.get(`${this.API}/subscribe/sub/{this.routeParam.params.id}`)
       .map(res => res.json())
       .subscribe(subs => {
         this.subs = subs
