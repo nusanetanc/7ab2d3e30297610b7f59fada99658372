51dbaf685ad9a374ba90a1b9ac4d5b1e3d658a05
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-06 14:53:24 +0700

    perbaikan background css

--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {ROUTER_DIRECTIVES, RouteParams} from 'angular2/router';
 import { Http } from 'angular2/http';
 import 'rxjs/add/operator/map';
 import { Sub } from './subs';
