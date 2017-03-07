1bece0deb9d32c06b4e0e87f9f6ce1c006a40ad4
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-07 16:31:47 +0700

    perbaikan background css

--- assets/app/my/auth/authentication.component.ts
+++ assets/app/my/auth/authentication.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {SigninComponent} from "./signin.component";
-import {ForgotComponent} from "./forgot.component";
+//import {ForgotComponent} from "./forgot.component";
 @Component({
     selector: 'authentication',
     template: `
