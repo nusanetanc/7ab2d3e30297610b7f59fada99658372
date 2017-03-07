fc7853ad06a6970f333e3bddc399b72954a65b96
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-07 15:49:41 +0700

    perbaikan background css

--- assets/app/my/app.component.ts
+++ assets/app/my/app.component.ts
@@ -13,7 +13,6 @@ import {ContentDetailInformationComponent} from "./dashboard/content/detailinfor
 import {ContentDetailReportComponent} from "./dashboard/content/detailreport.component";
 import {ContentAccountComponent} from "./dashboard/content/account.component";
 import {AuthenticationComponent} from "./auth/authentication.component";
-import {PrivateComponent} from './auth/private.component';
 
 @Component({
    selector: 'my-app',
@@ -35,11 +34,9 @@ import {PrivateComponent} from './auth/private.component';
                   ContentDetailReportComponent,
                   ContentAccountComponent,
                   AuthenticationComponent,
-                  PrivateComponent,
                   ROUTER_DIRECTIVES],
 })
 @RouteConfig([
-  { path: '/home', name: 'Home', component: PrivateComponent},
   { path: '/my/signin', component:AuthenticationComponent, name:'Signin'},
   { path: '/my', component:ContentDashboardComponent, name:'Dashboard'},
   { path: '/my/billing', component:ContentBillingComponent, name:'Billing'},
