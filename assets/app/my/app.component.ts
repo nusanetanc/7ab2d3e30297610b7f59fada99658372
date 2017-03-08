0b6f29a66df73ae8d31b43de21621f6db5be97e3
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-08 10:37:01 +0700

    perbaikan background css

--- assets/app/my/app.component.ts
+++ assets/app/my/app.component.ts
@@ -12,8 +12,6 @@ import {ContentDetailBillingComponent} from "./dashboard/content/detailbilling.c
 import {ContentDetailInformationComponent} from "./dashboard/content/detailinformation.component";
 import {ContentDetailReportComponent} from "./dashboard/content/detailreport.component";
 import {ContentAccountComponent} from "./dashboard/content/account.component";
-import {AuthenticationComponent} from "./auth/authentication.component";
-import {AuthenticationService} from "./auth/auth.service";
 
 @Component({
    selector: 'my-app',
@@ -33,12 +31,9 @@ import {AuthenticationService} from "./auth/auth.service";
                   ContentDetailBillingComponent,
                   ContentDetailInformationComponent,
                   ContentDetailReportComponent,
-                  ContentAccountComponent,
-                  AuthenticationComponent,
                   ROUTER_DIRECTIVES],
 })
 @RouteConfig([
-  { path: '/my/signin', component:AuthenticationComponent, name:'Signin'},
   { path: '/my', component:ContentDashboardComponent, name:'Dashboard'},
   { path: '/my/billing', component:ContentBillingComponent, name:'Billing'},
   { path: '/my/reports', component:ContentReportComponent, name:'Reports'},
