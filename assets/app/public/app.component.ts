4157d6c08027aca8955f82942a789d72fc776630
ferdianrb <frizki7@gmail.com>
2017-03-08 13:43:27 +0700

    public angular 2

--- assets/app/public/app.component.ts
+++ assets/app/public/app.component.ts
@@ -4,7 +4,6 @@ import {HeaderComponent} from "./header.component";
 import {FooterComponent} from "./footer.component";
 import {HomeComponent} from "./content/home.component";
 import {FeaturesComponent} from "./content/features.component";
-import {PackagesComponent} from "./content/packages.component";
 
 @Component({
     selector: 'my-app',
@@ -13,13 +12,12 @@ import {PackagesComponent} from "./content/packages.component";
             <router-outlet></router-outlet>
             <my-footer></my-footer>
 `,
-    directives: [ROUTER_DIRECTIVES, HeaderComponent, FooterComponent, HomeComponent, FeaturesComponent, PackagesComponent]
+    directives: [ROUTER_DIRECTIVES, HeaderComponent, FooterComponent, HomeComponent, FeaturesComponent]
 })
 
 @RouteConfig([
     {path: '/public', name: 'Home', component: HomeComponent, useAsDefault: true},
     {path: '/public/features', name: 'Features', component: FeaturesComponent},
-    {path: '/public/packages', name: 'Packages', component: PackagesComponent},
 ])
 export class AppComponent{
 
