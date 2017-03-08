5c4e064642d695ee3f4dac10f68cfbb1cc1efbf3
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-08 16:43:53 +0700

    perbaikan background css

--- assets/app/signin-my/signin.component.ts
+++ assets/app/signin-my/signin.component.ts
@@ -15,7 +15,7 @@ import { Sub } from './subs';
                             <input type="text" class="form-control" id="signEmail" #signEmail placeholder="Email">
                             <input type="text" class="form-control" id="signPassword" #signPassword placeholder="Password">
                         </div>
-                        <button type="submit" (click)="signSub(signEmail.value, signEmail.value)" class="btn button-submit">SIGN IN</button>
+                        <button type="submit" (click)="signSub(signEmail.value, signPassword.value)" class="btn button-submit">SIGN IN</button>
                         <div class="text text-other"><a href="isforgot.html">I forgot password</a></div>
                     </div>
                 </div>
