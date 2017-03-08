5e59da31d8049321b447db86857c4f7108b82d97
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-08 11:41:11 +0700

    perbaikan background css

--- assets/app/signin-my/app.component.ts
+++ assets/app/signin-my/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, OnDestroy} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
+import 'rxjs/add/operator/map';
 import { Http, Headers} from 'angular2/http';
 import { Sub } from './subs';
 import {HeaderComponent} from "./header.component";
@@ -23,12 +24,14 @@ import {FooterComponent} from "./footer.component";
             </div>
             <my-footer></my-footer>
 `,
-    directives: [HeaderComponent, FooterComponent]
+    directives: [HeaderComponent, FooterComponent, ROUTER_DIRECTIVES]
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
 
 // Link to our api, pointing to localhost
   API = 'http://202.162.207.164:3000';
+  
+constructor(private http: Http) {}
 
 // Declare empty list of people
 subs: any[] = [];
