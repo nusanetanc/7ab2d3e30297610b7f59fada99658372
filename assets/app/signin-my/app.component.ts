55bec0f32bdecf5f033cb0bb2a02d7c2121f15d0
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-08 11:04:29 +0700

    perbaikan background css

--- assets/app/signin-my/app.component.ts
+++ assets/app/signin-my/app.component.ts
@@ -8,8 +8,18 @@ import {FooterComponent} from "./footer.component";
 @Component({
     selector: 'my-signin',
     template: `
-            <my-header></my-header><br>
-            <router-outlet></router-outlet>
+            <my-header></my-header><div class="container container-auth-client">
+                <div class="top-margin text-center">
+                    <div class="form">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="email" placeholder="Email">
+                            <input type="password" class="form-control" id="password" placeholder="Password">
+                        </div>
+                        <a href="#" class="btn button-submit">SIGN IN</a>
+                        <div class="text text-other"><a href="isforgot.html">I forgot password</a></div>
+                    </div>
+                </div>
+            </div>
             <my-footer></my-footer>
 `,
     directives: [HeaderComponent, FooterComponent]
