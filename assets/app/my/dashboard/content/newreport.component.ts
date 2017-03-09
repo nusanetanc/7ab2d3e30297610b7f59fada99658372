6edc6d4d0d3f209bc98f0798d785840963820c18
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-09 17:26:36 +0700

    perbaikan background css

--- assets/app/my/dashboard/content/newreport.component.ts
+++ assets/app/my/dashboard/content/newreport.component.ts
@@ -57,7 +57,7 @@ import { Problem } from './problem';
                                     </div>
                                     <div class="col-sm-11" >
                                         Whats up..! What is going on ? <br> Please select the category of your problem
-                                         <div> {{ inputsubcategory.desc }} </div>
+                                         <div> {{ problem.desc }} </div>
                                     </div>
                                 </div>
                             </div>
