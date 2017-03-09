8fac6b96f4cde26af26e75715518b9803efaaa27
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-09 16:11:44 +0700

    perbaikan background css

--- assets/app/my/dashboard/content/newreport.component.ts
+++ assets/app/my/dashboard/content/newreport.component.ts
@@ -57,7 +57,7 @@ import { Problem } from './problem';
                                     </div>
                                     <div class="col-sm-11">
                                         Whats up..! What is going on ? <br> Please select the category of your problem
-                                        {{ descproblems.desc }}
+                                        *ngFor="#descproblem of descproblems" {{ descproblems.desc }}
                                     </div>
                                 </div>
                             </div>
