31f21d77eb80030676b4292bcf5dfdca6aee51b2
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2017-03-09 14:54:15 +0700

    update sidebar (rifki)

--- assets/app/is/app.component.ts
+++ assets/app/is/app.component.ts
@@ -15,10 +15,28 @@ import {SigninComponent} from "./authentication/signin.component";
    selector: 'is-app',
    template: `
    <!-- START CONTENT -->
-     <div id="wrapper">
-    <dashboard></dashboard>
-    <router-outlet></router-outlet>
-  </div><!-- END CONTENT -->
+   <div id="wrapper">
+   <dashboard></dashboard>
+   <router-outlet></router-outlet>
+        
+   <div id="failed" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="alert alert-danger alert-dismissible fade in" role=alert>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4>Gagal, coba lagi !!!</h4>
+            </div>
+        </div>
+    </div>
+    <div id="success" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="alert alert-success alert-dismissible fade in" role=alert>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4>Berhasil !!!</h4>
+            </div>
+        </div>
+    </div>
+        
+   </div><!-- END CONTENT -->
 `,
     directives: [DashboardComponent, ContentAllSubsComponent, ContentDashboardComponent, ContentInformationComponent, ContentSubscribeComponent, SigninComponent, ROUTER_DIRECTIVES],
 })
