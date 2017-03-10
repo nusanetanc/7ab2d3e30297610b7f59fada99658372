2e5068d2b31f1ea8b467d59d16ca08b2e529542c
ferdianrb <frizki7@gmail.com>
2017-03-10 09:09:58 +0700

    signin-my

---
+++ assets/app/public/content/signin.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit, OnDestroy} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
+
+@Component({
+    selector: 'form-signin',
+    template: `
+            <div class="container container-auth-client">
+                <div class="top-margin text-center">
+                    <div class="form">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="email" placeholder="Email">
+                            <input type="password" class="form-control" id="password" placeholder="Password">
+                        </div>
+                        <a href="#" class="btn button-submit">SIGN IN</a>
+                        <div class="text text-other"><a href="isforgot.html">I forgot password</a></div>
+                    </div>
+                </div>
+            </div>
+`,
+    directives: [ROUTER_DIRECTIVES]
+})
+export class SigninComponent implements OnInit {
+
+}
