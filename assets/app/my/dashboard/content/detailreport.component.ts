10e41c5344d2ded4499a8fcc3b2e2c5c68981212
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-10 14:30:49 +0700

    perbaikan background css

--- assets/app/my/dashboard/content/detailreport.component.ts
+++ assets/app/my/dashboard/content/detailreport.component.ts
@@ -32,13 +32,13 @@ import { Complaint } from './complaints';
                       <div class="col-sm-10 invoiceId"><span><b class="grey333">{{ complaints.subject }}</b><br>{{ complaints.category }} - {{ complaints.subcategory }}</span></div>
                       <div class="col-sm-2 invoiceList"><span class="grey333">Status : <span class="red">{{ complaints.status }}s</span></span></div>
                   </div>
-                  <div *ngFor="#chat of chatcomplaints">
+                  <div>
                       <div class="row">
                           <div class="col-sm-12 invoiceId"><span>Posted <b class="grey333">11 Feb 2017 - 11.00 PM</b> by <b class="grey333">Jhon Doe</b></span></div>
                       </div>
                       <div class="row">
                           <div class="col-sm-11 infoDetail">
-                              <span>{{ chat.message }}</span>
+                              <span *ngFor="#chat of chatcomplaints">{{ chat.message }}</span>
                           </div>
                       </div>
                     </div>
