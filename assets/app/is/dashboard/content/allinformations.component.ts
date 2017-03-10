eca8754e82ee0044a1550f160e6d4110d9553c1f
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2017-03-10 14:24:51 +0700

    add content (rifki)

--- assets/app/is/dashboard/content/allinformations.component.ts
+++ assets/app/is/dashboard/content/allinformations.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import { Http } from 'angular2/http';
 import 'rxjs/add/operator/map';
-import { Information } from './informations';
+import { Information } from './allinformation';
 
 @Component({
     selector: 'form-allinformations',
@@ -21,29 +21,42 @@ import { Information } from './informations';
         <div class="page-content inset" data-spy="scroll" data-target="#spy">
             <div class="row marginB20 marginR0">
                 <div class="col-sm-12">
-                  <a href="" class="glyphicon glyphicon-chevron-down sort-down"></a>
-                  <div class="dropdown right">
-                      <a class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" style="background-color: #E2E2E2; border: none; border-radius: 0px; color: #676767; padding: 12.5px 40px 12.5px 40px; text-decoration: none;">
-                          DATE
-                      </a>
-                      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                          <li><a href="#">NAME</a></li>
-                          <li><a href="#">ID</a></li>
-                      </ul>
-                  </div>
+                    <a href="newinformation.html" class="btn btn-default buttonOrange">
+                        NEW INFORMATION
+                    </a>
+                    <a href="" class="glyphicon glyphicon-chevron-down sort-down"></a>
+                    <div class="dropdown right">
+                        <a class="btn btn-default dropdown-toggle buttonSort" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            DATE
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                            <li><a href="#">NAME</a></li>
+                            <li><a href="#">ID</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-12" *ngFor="#information of informations">
-                  <div class="row subInfo">
-                      <div class="col-sm-2 invoiceId" style="padding: 20px 0px 20px 35px;"><span>{{ information.date }}</span></div>
-                      <div class="col-sm-8 invoiceList" style="padding: 20px 0px 20px 0px;"><span>{{ information.subject }}</span></div>
-                      <div class="col-sm-2 invoiceList" style="padding: 20px 0px 20px 0px;"><span>{{ information.status }}</span></div>
-                  </div>
+                <div class="col-sm-12">
+                    <div class="row subInfo">
+                        <div class="col-sm-2 invoiceId"><span><a href="" class="grey333">11 Feb 2017</a></span></div>
+                        <div class="col-sm-8 invoiceList"><span><a href="" class="grey333">Fiber Optic Network Disruption</a></span></div>
+                        <div class="col-sm-2 invoiceList"><span class="red">On Progress</span></div>
+                    </div>
+                    <div class="row subInfo">
+                        <div class="col-sm-2 invoiceId"><span>09 Feb 2017</span></div>
+                        <div class="col-sm-8 invoiceList"><span>Network Server Maintenance</span></div>
+                        <div class="col-sm-2 invoiceList"><span class="green">Solved</span></div>
+                    </div>
+                    <div class="row subInfo">
+                        <div class="col-sm-2 invoiceId"><span>21 Jan 2017</span></div>
+                        <div class="col-sm-8 invoiceList"><span>Welcome to Groovy</span></div>
+                        <div class="col-sm-2 invoiceList"><span></span></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div><!-- Page content -->
+    </div>
     `,
     directives: [ROUTER_DIRECTIVES],
 })
