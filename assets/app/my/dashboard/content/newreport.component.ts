7f4cbf1597c5408a9fd8e1c6942708e8e24186a9
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-10 07:10:41 +0700

    perbaikan background css

--- assets/app/my/dashboard/content/newreport.component.ts
+++ assets/app/my/dashboard/content/newreport.component.ts
@@ -40,7 +40,7 @@ import { Problem } from './problem';
                                 <form>
                                     <select  #inputsubcategory id="inputsubcategory"  (click)="getDescProblem(subcategory.value)">
                                         <option class="option" disabled="true" selected="true" value="">-- Select Internet Problem --</option>
-                                        <option *ngFor="#problem of problems" [value] = "problem.desc" >{{ problem.subcategory }}</option>
+                                        <option *ngFor="#problem of problems" [value] = {{problem.desc}} >{{ problem.subcategory }}</option>
                                     </select><br/>
                                 </form>
                                 <textarea id="message" class="input width100" name="message" rows="10" placeholder="*note"></textarea>
