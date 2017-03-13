1f356be26ecc849767b1f9c96c23626a850d53ab
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-13 16:19:07 +0700

    perbaikan background css

--- routes/finnet.js
+++ routes/finnet.js
@@ -2,10 +2,23 @@ var express = require('express');
 var router = express.Router();
 var Finnet = require('../models/finnet');
 var Sub = require('../models/subs');
+var Bill = require('../models/subs');
 
 /* GET detail bill one account. */
 router.post('/inqreq/', function(req, res, next) {
 Sub.findOne({subid: req.body.subid}, function(err, doc) {
+  if (err) {
+      return res.status(404).json({
+          title: 'An error occured',
+          error: err
+      });
+  }
+  if (!doc) {
+      return res.status(404).json({
+          title: 'No user found',
+          error: {message: 'User could not be found'}
+      });
+  }
        res.json({
          subid: doc.subid,
          subname: doc.name
