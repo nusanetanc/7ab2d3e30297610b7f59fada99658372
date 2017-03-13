68fe19e2880eaca01b9f8d473284508ebcef44b2
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-13 14:44:26 +0700

    perbaikan background css

--- routes/finnet.js
+++ routes/finnet.js
@@ -4,7 +4,7 @@ var Finnet = require('../models/finnet');
 var Sub = require('../models/subs');
 
 /* GET detail bill one account. */
-router.post('/inqreq/:subid', function(req, res, next) {
+router.get('/inqreq/:subid', function(req, res, next) {
 Sub.find({subid: req.params.subid}, function(err, subs) {
        console.log( subs );
        res.json(subs);
