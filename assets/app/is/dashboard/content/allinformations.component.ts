4176d5e99563e80786f1d82d96ff2543ffff9f53
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2017-03-14 07:57:28 +0700

    rifki

--- assets/app/is/dashboard/content/allinformations.component.ts
+++ assets/app/is/dashboard/content/allinformations.component.ts
@@ -50,7 +50,7 @@ import { Information } from './allinformation';
     `,
     directives: [ROUTER_DIRECTIVES],
 })
-export class ContentInformationComponent {
+export class ContentAllInformationComponent {
 // Link to our api, pointing to localhost
   API = 'http://202.162.207.164:3000';
 
