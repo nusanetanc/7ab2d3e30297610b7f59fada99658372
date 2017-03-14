70305e34a7bf0d10d26aec63cafc6bf0a0d2d22e
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2017-03-14 14:55:58 +0700

    rifki

--- assets/app/is/dashboard/content/allstock.component.ts
+++ assets/app/is/dashboard/content/allstock.component.ts
@@ -10,7 +10,7 @@ import { Http } from 'angular2/http';
     <div id="page-content-wrapper">
         <div class="content-header">
             <h3 id="home">
-                <a id="menu-toggle" href="#" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
+                <a id="menu-toggle" href="" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
                 </a>
                 &nbsp; All Stock
             </h3>
--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -10,7 +10,7 @@ import { Sub } from './subs';
     <div id="page-content-wrapper">
         <div class="content-header">
             <h3 id="home">
-                <a id="menu-toggle" href="#" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
+                <a id="menu-toggle" href="" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
                 </a>
                 &nbsp; Subscriber
             </h3>
