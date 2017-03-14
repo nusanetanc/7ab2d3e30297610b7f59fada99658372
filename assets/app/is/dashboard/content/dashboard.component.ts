2dc017ed1a0c347674a36b9fbe5fd4c92864013e
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2017-03-14 14:52:26 +0700

    rifki

--- assets/app/is/dashboard/content/dashboard.component.ts
+++ assets/app/is/dashboard/content/dashboard.component.ts
@@ -8,7 +8,7 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
           <div id="page-content-wrapper">
               <div class="content-header">
                   <h3 id="home">
-                      <a id="menu-toggle" href="#" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
+                      <a id="menu-toggle" href="" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
                       </a>
                       &nbsp; Dashboard
                   </h3>
