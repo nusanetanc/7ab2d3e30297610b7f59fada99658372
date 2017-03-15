80cc48ae1e493182bd9525f4bcc6586e152490f8
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-15 13:52:05 +0700

    update

--- assets/app/my/dashboard/dashboard.component.ts
+++ assets/app/my/dashboard/dashboard.component.ts
@@ -62,7 +62,7 @@ subs: any[] = [];
   }
 
 getAcountSub() {
-  this.http.get(`${this.API}/subscribe/sub/${this.sessionId}`)
+  this.http.get(`${this.API}/subscribe/sub/${this.Session_ID}`)
     .map(res => res.json())
     .subscribe(subs => {
       this.subs = subs
