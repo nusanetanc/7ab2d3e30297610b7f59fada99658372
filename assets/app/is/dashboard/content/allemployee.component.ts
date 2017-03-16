b7c9df6b6ed655c6c519b443858a3b4764441788
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2017-03-16 11:36:49 +0700

    creat list employee

--- assets/app/is/dashboard/content/allemployee.component.ts
+++ assets/app/is/dashboard/content/allemployee.component.ts
@@ -66,7 +66,7 @@ export class ContentAllEmployeeComponent {
 
     // Get all users from the API
     getAllSub() {
-        this.http.get(`${this.API}/is/allemployee`)
+        this.http.get(`${this.API}/employee/listemp`)
             .map(res => res.json())
             .subscribe(emp => {
                 this.subs = emp
