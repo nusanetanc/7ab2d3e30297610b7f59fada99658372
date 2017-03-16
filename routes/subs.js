0648ea0fd1bce982b8472b089037efe434baefc1
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-16 14:48:29 +0700

    update

--- routes/subs.js
+++ routes/subs.js
@@ -27,7 +27,7 @@ Sub.findById(req.params.id, function(err, subs) {
 /* GET detail sub. */
 router.get('/sub/detailsub', function(req, res, next) {
 var decoded = jwt.decode(req.query.token);
-Sub.findById(decoded.user._id, function(err, subs) {
+Sub.findById(decoded.sub._id, function(err, subs) {
        console.log( subs );
        res.json(subs);
    });
