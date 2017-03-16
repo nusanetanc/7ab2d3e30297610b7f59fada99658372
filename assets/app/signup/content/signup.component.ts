a4f782b67672dbd08d9e5e310ed2f5defcc3da53
ferdianrb <frizki7@gmail.com>
2017-03-16 11:19:48 +0700

    signup app

--- assets/app/signup/content/signup.component.ts
+++ assets/app/signup/content/signup.component.ts
@@ -19,10 +19,11 @@ import {City} from "./cities";
                     <div class="row">
                         <div class="col-md-4 col-md-offset-4">
                             <form>
-                                <select *ngIf="onChange" (change)="onChange($event.target.value)" style="" name="cars">
+                                <select style="" name="cars">
                                     <option disabled="true" selected="true" style="height: 30px;">Select your city</option>
-                                    <option *ngFor="#city of cities" value="{{ city._id }}">{{ city.name }}</option>
+                                    <option *ngFor="#city of cities" value="{{ city._id }}" (click)="onCityClicked(city)">{{ city.name }}</option>
                                 </select><br/><br/>
+                                <input type="text" [(ngModel)]="clickedCity.name">
                                 <select *ngIf="!onChange" style="" name="cars">
                                     <option disabled="true" selected="true" style="height: 30px;">Select your citys</option>
                                     <option *ngFor="#city of cities" value="{{ city._id }}" (click)="toggleHidden()">{{ city.name }}</option>
@@ -55,6 +56,10 @@ export class SignupComponent implements OnInit{
 
     onChange(deviceValue) {
         console.log(deviceValue);
+    }
+
+    onCityClicked(city) {
+        this.clickedCity = city;
     }
 // Link to our api, pointing to localhost
     API = 'http://202.162.207.164:3000';
