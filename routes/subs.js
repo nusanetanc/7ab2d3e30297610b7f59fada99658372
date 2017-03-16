2f4153190d143c8c442ce11ddc362b26fb50b792
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-16 19:15:44 +0700

    update

--- routes/subs.js
+++ routes/subs.js
@@ -22,22 +22,9 @@ Sub.findById(req.params.id, function(err, subs) {
    });
 });
 
-router.use('/sub/detailsub', function(req, res, next){
-  jwt.verify(req.query.token, 'secret', function(err, decoded) {
-    if (err) {
-      return res.status(401).json({
-        title: "Authentication Failed",
-        error: err
-      });
-    }
-    next();
-  })
-})
-
 /* GET detail sub. */
 router.get('/sub/detailsub', function(req, res, next) {
-  localStorage.getItem('email', email);
-  console.log( email );
+  console.log( localStorage.getItem('email', email) );
 var decoded = jwt.decode(req.query.token);
 Sub.findById(decoded.sub._id, function(err, subs) {
        console.log( subs );
