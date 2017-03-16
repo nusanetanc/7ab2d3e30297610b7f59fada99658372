ce0f7b6ee14116afbe1a8d35e588cb9f5136c69e
ferdianrb <frizki7@gmail.com>
2017-03-16 16:12:30 +0700

    signup app

--- assets/app/signup/content/signup.component.ts
+++ assets/app/signup/content/signup.component.ts
@@ -19,7 +19,7 @@ import {City} from "./cities";
                     <div class="row">
                         <div class="col-md-4 col-md-offset-4">
                             <form>
-                                <select *ngIf="!selectedCity" style="" name="cars" (change)="onChange($event.target.value)">
+                                <select [hidden]="hidden" *ngIf="!selectedCity" style="" name="cars" (change)="onChange($event.target.value)">
                                     <option disabled="true" selected="true" style="height: 30px;">Select your city</option>
                                     <option *ngFor="#city of cities" value="{{ city._id }}">{{ city.name }}</option>
                                 </select><br/><br/>
@@ -54,6 +54,7 @@ export class SignupComponent implements OnInit{
     onChange(deviceValue): void{
         console.log(deviceValue);
         this.selectedCity = deviceValue;
+        this.hidden = !this.hidden;
     }
 
 
