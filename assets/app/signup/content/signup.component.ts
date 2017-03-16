6d9ffbfa41323815a9e0efefa59e27b1a929a0cf
ferdianrb <frizki7@gmail.com>
2017-03-16 11:41:18 +0700

    signup app

--- assets/app/signup/content/signup.component.ts
+++ assets/app/signup/content/signup.component.ts
@@ -19,12 +19,12 @@ import {City} from "./cities";
                     <div class="row">
                         <div class="col-md-4 col-md-offset-4">
                             <form>
-                                <select style="" name="cars">
+                                <select *ngIf="show" [hidden]="hidden" style="" name="cars">
                                     <option disabled="true" selected="true" style="height: 30px;">Select your city</option>
-                                    <option *ngFor="#city of cities" value="{{ city._id }}" (click)="onCityClicked()">{{ city.name }}</option>
+                                    <option *ngFor="#city of cities" value="{{ city._id }}" (click)="selectCity()">{{ city.name }}</option>
                                 </select><br/><br/>
-                                <input type="text" [(ngModel)]="city.name">
-                                <select *ngIf="!onChange" style="" name="cars">
+                                <input type="text" [(ngModel)]="clickedCity.name">
+                                <select *ngIf="!show" style="" name="cars">
                                     <option disabled="true" selected="true" style="height: 30px;">Select your citys</option>
                                     <option *ngFor="#city of cities" value="{{ city._id }}" (click)="toggleHidden()">{{ city.name }}</option>
                                 </select><br/>
@@ -46,22 +46,14 @@ export class SignupComponent implements OnInit{
         this.show = !this.show;
     }
 
-    selectCity(city:any) {
-        console.log(city);
+    public clickedCity = {name: ""};
+
+    selectCity(city) {
+        this.clickedCity = city;
     }
 
     toggleHidden() {
         this.hidden = !this.hidden;
-    }
-
-    onChange(deviceValue) {
-        console.log(deviceValue);
-    }
-
-    public clickedCity = {name};
-
-    onCityClicked(city:any) {
-        this.clickedCity = city;
     }
 // Link to our api, pointing to localhost
     API = 'http://202.162.207.164:3000';
