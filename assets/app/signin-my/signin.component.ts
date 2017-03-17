8a94fd47f93fcdd26fd4708ecd7c9d8a8de282d6
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-17 07:08:40 +0700

    update

--- assets/app/signin-my/signin.component.ts
+++ assets/app/signin-my/signin.component.ts
@@ -17,14 +17,57 @@ import { Sub } from './subs';
                                 <input type="password" class="form-control" id="signPassword" #signPassword placeholder="Password">
                             </div>
                             <button type="submit" (click)="signSub(signEmail.value, signPassword.value)" class="btn button-submit">SIGN IN</button>
-                            <div class="text text-other"><a href="isforgot">I forgot password</a></div>
+                            <div class="text text-other"><a href="isforgot.html">I forgot password</a></div>
                     </div>
                 </div>
             </div>
 `,
     directives: [ROUTER_DIRECTIVES]
 })
-export class SigninComponent{
+export class SigninComponent implements OnInit {
+
+// Link to our api, pointing to localhost
+  API = 'http://202.162.207.164:3000';
+
+constructor(private http: Http) {}
 
 
+  // Angular 2 Life Cycle event when component has been initialized
+  ngOnInit() {
+    this.getAllSub();
+  }
+
+// Declare empty list of people
+subs: any[] = [];
+
+// Get all Sub from the API
+getAllSub() {
+  this.http.get(`${this.API}/subscribe/listsub`)
+    .map(res => res.json())
+    .subscribe(subs => {
+      this.subs = subs
+    })
+}
+
+// Add one person to the API
+  signSub(signEmail, signPassword) {
+
+  var body = `email=${signEmail}&password=${signPassword}`;
+  var headers = new Headers();
+  headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    this.http
+        .post(`${this.API}/subscribe/signin`,
+          body, {
+            headers: headers
+          })
+    .subscribe(
+            data => {
+              window.location.href = `/my`;
+            },
+            error => {
+              alert(error.text());
+              console.log(error.text());
+            }
+          );
+  }
 }
