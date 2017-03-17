46e0dc3626542e1c72159d71cefe68895a2986b9
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-17 07:27:53 +0700

    update

--- assets/app/signin-my/signin.component.ts
+++ assets/app/signin-my/signin.component.ts
@@ -2,14 +2,24 @@ import {Component, OnInit, OnDestroy} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
 import { Http, Headers} from 'angular2/http';
 import { Sub } from './subs';
 
 @Component({
     selector: 'form-signin',
     template: `
-            <h1>Heloo</h1>
+            <div class="container container-auth-client">
+                <div class="top-margin text-center">
+                    <div class="form">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="signEmail" #signEmail placeholder="Email">
+                                <input type="password" class="form-control" id="signPassword" #signPassword placeholder="Password">
+                            </div>
+                            <button type="submit" (click)="signSub(signEmail.value, signPassword.value)" class="btn button-submit">SIGN IN</button>
+                            <div class="text text-other"><a href="isforgot.html">I forgot password</a></div>
+                    </div>
+                </div>
+            </div>
 `,
     directives: [ROUTER_DIRECTIVES]
 })
@@ -38,5 +48,27 @@ getAllSub() {
     })
 }
 
+// Add one person to the API
+  signSub(signEmail, signPassword) {
 
+  var body = `email=${signEmail}&password=${signPassword}`;
+  var headers = new Headers();
+  headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    this.http
+        .post(`${this.API}/subscribe/signin`,
+          body, {
+            headers: headers
+          })
+    .subscribe(
+            data => {
+              window.location.href = `/my`;
+              localStorage.setItem('token', data.obj);
+              localStorage.setItem('MongoId', data._id);
+            },
+            error => {
+              alert(error.text());
+              console.log(error.text());
+            }
+          );
+  }
 }
