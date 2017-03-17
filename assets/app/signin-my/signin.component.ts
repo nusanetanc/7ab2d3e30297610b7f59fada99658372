b20c91068293cea94ae8d1b40e8f6dc5a3166f22
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-17 08:04:14 +0700

    update

--- assets/app/signin-my/signin.component.ts
+++ assets/app/signin-my/signin.component.ts
@@ -23,12 +23,29 @@ import { Sub } from './subs';
 `,
     directives: [ROUTER_DIRECTIVES]
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit {
 
 // Link to our api, pointing to localhost
   API = 'http://202.162.207.164:3000';
 
 constructor(private http: Http) {}
+
+// Angular 2 Life Cycle event when component has been initialized
+ngOnInit() {
+  this.getAllSub();
+}
+
+// Declare empty list of people
+subs: any[] = [];
+
+// Get all Sub from the API
+getAllSub() {
+this.http.get(`${this.API}/subscribe/listsub`)
+  .map(res => res.json())
+  .subscribe(subs => {
+    this.subs = subs
+  })
+}
 
 // Add one person to the API
   signSub(signEmail, signPassword) {
