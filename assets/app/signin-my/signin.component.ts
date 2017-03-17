76da1dff1fd2bd46ed1986e93141f0a0e4839c0a
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-17 09:46:13 +0700

    update

--- assets/app/signin-my/signin.component.ts
+++ assets/app/signin-my/signin.component.ts
@@ -4,6 +4,7 @@ import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
 import 'rxjs/add/operator/map';
 import { Http, Headers} from 'angular2/http';
 import { Sub } from './subs';
+import storage from 'local-storage-fallback'
 
 @Component({
     selector: 'form-signin',
@@ -61,12 +62,8 @@ getAllSub() {
           })
     .subscribe(
             data => {
-            import { jsonLocalStorage } from 'json-web-storage';
-            jsonLocalStorage.setItem('obj', { name: 'John' });
-            let obj = jsonLocalStorage.getItem('obj');
-            console.log(typeof obj) // => object
-            console.log(obj.name) // => John
               window.location.href = `/my`;
+
 
             },
             error => {
