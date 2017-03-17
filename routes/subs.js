d7a9a6be3090c0c046835d26a32e9295dd298e3f
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-17 14:19:19 +0700

    update

--- routes/subs.js
+++ routes/subs.js
@@ -38,6 +38,7 @@ router.use('/sub/detailsub', function(req, res, next){
 router.get('/sub/detailsub', function(req, res, next) {
   var localStorage = require('localStorage')
 myValue = localStorage.getItem('myKey');
+console.log( myValue );
 var decoded = jwt.decode(req.query.myValue);
 Sub.findById(decoded.sub._id, function(err, subs) {
        console.log( subs );
