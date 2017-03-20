52b0b76d86f3321e1648cd59264edbc4e8ab3466
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-21 02:52:00 +0700

    update

--- routes/bill.js
+++ routes/bill.js
@@ -18,37 +18,10 @@ var Bill = require('../models/bill');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
-router.use(upload.array());
+//router.use(upload.array());
 router.use(cookieParser());
 router.use(session({
   secret: 'Your secret key',
-  saveUninitialized: true,
-  resave: true,
-  maxAge: 200000000000000000000
-}));
-
-/* GET billloye listing. */var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
-var router = express.Router();
-var Sub = require('../models/subs');
-var randomInt = require('random-int');
-var damm = require('damm');
-var jwt = require('jsonwebtoken');
-var session = require('express-session');
-var localStorage = require('localStorage');
-var jwtDecode = require('jwt-decode');
-var Bill = require('../models/bill');
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(upload.array());
-router.use(cookieParser());
-router.use(session({
-  secret: 'Your secret key',
-  saveUninitialized: true,
-  resave: true,
-  maxAge: 200000000000000000000
 }));
 
 router.get('/listbill', function(req, res, next) {
