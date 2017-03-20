5aeadbffbcf61f8d1463a29d749e7bc34fadbb3b
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-20 19:54:44 +0700

    update

--- routes/menu.js
+++ routes/menu.js
@@ -2,10 +2,32 @@ var express = require('express');
 var router = express.Router();
 var Menu = require('../models/menu');
 
+var Emp = require('../models/employee');
+
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var router = express.Router();
+var randomInt = require('random-int');
+var damm = require('damm');
+var jwt = require('jsonwebtoken');
+var session = require('express-session');
+var localStorage = require('localStorage');
+var jwtDecode = require('jwt-decode');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(upload.array());
+router.use(cookieParser());
+router.use(session({secret: "Your secret key"}));
 
 /* GET menu listing. */
 router.get('/listmenu', function(req, res, next) {
-    Menu.find({access:'2'}, function(err, menus) {
+  if(req.session.emp){
+      sessionEmpAccess = req.session.accessrole;
+}
+    Menu.find({access:sessionEmpAccess}, function(err, menus) {
         console.log( menus );
         res.json(menus);
     });
