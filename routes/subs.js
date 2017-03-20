74032b26c382c62b99b08ebd3ad26a18671a5b5f
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-20 11:51:28 +0700

    update

--- routes/subs.js
+++ routes/subs.js
@@ -38,7 +38,7 @@ Sub.findById(req.params.id, function(err, subs) {
 /* GET detail sub. */
 router.get('/detailsub', function(req, res, next) {
   var mysubs = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsiX192IjowLCJfaWQiOiI1OGIzY2RhYzQ1OTEyZDA1MmUyYzg1YTUiLCJlbWFpbCI6ImRldmVsb3BlcnNAZ3Jvb3Z5LmlkIiwiZ3Jvb3Z5aWQiOiI1ODk4MzMwY2MwZDA5OTJhNDY0NjUxMDkiLCJuYW1lIjoiVGVzIEFkbWluIiwibm92YSI6NzIwMDIzMjEzNDIxNCwicGFja2xldiI6IjEiLCJwYXNzd29yZCI6InNoYTEkMTg2MWNjNTMkMSQ5YWQzZGY3OTU3NDM3YWZhZGNmYmI4MTNlNDMzZDlhNTQ0Y2UwMTE1IiwicGhvbmUiOiI4NTcyMzM5OTAzNCIsInN0YXR1cyI6IkFrdGlmIiwic3ViaWQiOiJHUjYxOTkxNiIsImhpc3RvcnkiOltdLCJiaWxsaW5nIjpbXX0sImlhdCI6MTQ4OTk4NDA0MiwiZXhwIjoxNDg5OTkxMjQyfQ.Ie4yiijpdGmWphaVvVmv9yLKXWw63SFT1jRT1fZkUfI";
-  var decoded = jwt_decode(mysubs);
+  var decoded = jwtDecode(mysubs);
   Sub.findOne({_id: decode._id}, function(err, subs) {
     console.log( subs );
     res.json(subs);
