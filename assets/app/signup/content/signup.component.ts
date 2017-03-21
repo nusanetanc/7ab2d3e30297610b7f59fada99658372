43235829cb00449cca0c87437d12fcd4736a6b48
ferdianrb <frizki7@gmail.com>
2017-03-21 14:15:38 +0700

    signup app

--- assets/app/signup/content/signup.component.ts
+++ assets/app/signup/content/signup.component.ts
@@ -25,23 +25,23 @@ import {Package} from "./package";
                     <div class="row">
                         <div class="col-md-4 col-md-offset-4">
                             <form>                            
-                                <select #subcity id="subcity" name="cars">
+                                <select name="cars">
                                     <option disabled="true" selected="true" style="height: 30px;">Select your city</option>
                                     <option *ngFor="#city of cities" value="{{ city.name }}">{{ city.name }}</option>
                                 </select>
-                                 <select #subproperty id="subproperty" name="property">
+                                 <select name="property">
                                     <option class="option" disabled="true" selected="true">-- Select Property Name --</option>
                                     <option *ngFor="#property of properties" value="{{ property.name }}">{{property.name}}</option>
                                 </select>
-                                <select #type id="subtype" name="type">
+                                <select name="type">
                                     <option class="option" disabled="true" selected="true">-- Select Type --</option>
                                     <option *ngFor="#typeproperty of typeproperties" value="{{ typeproperty.name }}">{{ typeproperty.name }}</option>      
                                 </select>
-                                <select #subcluster id="subcluster" name="cluster">
+                                <select name="cluster">
                                     <option class="option" disabled="true" selected="true">-- Select Cluster --</option>
                                     <option *ngFor="#cluster of clusters" value="{{ cluster.name }}">{{ cluster.name }}</option>
                                 </select>
-                                <select #subblok id="subblok" name="block">
+                                <select name="block">
                                     <option class="option" disabled="true" selected="true">-- Select Block --</option>
                                     <option *ngFor="#blokfloor of blokfloors" value="{{ blokfloor.name }}">{{ blokfloor.name }}</option>
                                 </select>
@@ -103,10 +103,10 @@ import {Package} from "./package";
                                         </div>
                                     </div>
                                 </div>
+                            </form>
                                 <button (click)="addSub(subname.value, subphone.value, subemail.value, subdateinst.value, subtimeinst.value, subpacklev.value, subgroovyid.value)" class="next btn btn-default dropdown-toggle" style="">
                                     NEXT
                                 </button>
-                            </form>
                         </div>
                     </div>
                 </div>
