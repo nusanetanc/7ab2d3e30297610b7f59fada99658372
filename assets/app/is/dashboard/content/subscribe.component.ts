96ada1031d10d48e0668b28ae278a4b84dfee5ff
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-22 17:19:48 +0700

    perbaikan

--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -164,38 +164,5 @@ import { Sub } from './subs';
     directives: [ROUTER_DIRECTIVES],
 })
 export class ContentSubscribeComponent implements  OnInit, OnDestroy {
-  // Link to our api, pointing to localhost
-    API = 'http://202.162.207.164:3000';
-    subid = '58b3cdac45912d052e2c85a5';
 
-    // Declare empty list of people
-    subs: any[] = [];
-
-    constructor(private http: Http, private activatedRoute: ActivatedRoute) {}
-
-    // Angular 2 Life Cycle event when component has been initialized
-    ngOnInit() {
-      this.getSub();
-      this.idSubscription = this.activatedRoute.params.subscribe(params => {
-            this.id = params['id']
-            this.load()
-        })
-    }
-    ngOnDestroy() {
-        this.idSubscription.unsubscribe()
-    }
-    public id: string
-    private idSubscription: Subscription
-
-  // Get all users from the API
-  getSub() {
-    this.http.get(`${this.API}/subscribe/sub`)
-      .map(res => res.json())
-      .subscribe(subs => {
-        this.subs = subs
-      })
-  }
-  load() {
-        console.log("Load Id " + this.id);
-    }
 }
