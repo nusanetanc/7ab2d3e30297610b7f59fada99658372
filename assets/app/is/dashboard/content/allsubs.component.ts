6849e4d37648b74cec1714715afcd58e63dbf2b9
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-23 18:58:29 +0700

    perbaikan

--- assets/app/is/dashboard/content/allsubs.component.ts
+++ assets/app/is/dashboard/content/allsubs.component.ts
@@ -7,31 +7,11 @@ import { Sub } from './subs';
 @Component({
     selector: 'form-allsubs',
     template: `
-
+    <h1>hai</h1>
     `,
     directives: [ROUTER_DIRECTIVES],
 })
 export class ContentAllSubsComponent {
 
-  // Link to our api, pointing to localhost
-    API = 'http://202.162.207.164:3000';
 
-    // Declare empty list of people
-      subs: any[] = [];
-
-    constructor(private http: Http) {}
-
-    // Angular 2 Life Cycle event when component has been initialized
-    ngOnInit() {
-      this.getAllSub();
-    }
-
-  // Get all users from the API
-  getAllSub() {
-    this.http.get(`${this.API}/subscribe/listsub`)
-      .map(res => res.json())
-      .subscribe(subs => {
-        this.subs = subs
-      })
-  }
 }
