8e47d9f07167083c9a4f54d2383b62ce190c8aa3
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-23 19:06:41 +0700

    perbaikan

--- assets/app/is/dashboard/content/allsubs.component.ts
+++ assets/app/is/dashboard/content/allsubs.component.ts
@@ -7,7 +7,47 @@ import { Sub } from './subs';
 @Component({
     selector: 'form-allsubs',
     template: `
-    <h1>tuy</h1>
+    <!-- Page content -->
+  <div id="page-content-wrapper">
+      <div class="content-header">
+          <h3 id="home">
+              <a id="menu-toggle" href="#" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
+              </a>
+              &nbsp; Subscribers
+          </h3>
+
+      </div>
+
+      <div class="page-content inset" data-spy="scroll" data-target="#spy">
+          <div class="row marginB20 marginR0">
+              <div class="col-sm-12">
+                  <a href="newsubscriber.html" class="btn btn-default buttonOrange">
+                      NEW SUBSCRIBER
+                  </a>
+                  <a href="" class="glyphicon glyphicon-chevron-down sort-down"></a>
+                  <div class="dropdown right">
+                      <a class="btn btn-default dropdown-toggle buttonSort" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                          NAME
+                      </a>
+                      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                          <li><a href="#">NAME</a></li>
+                          <li><a href="#">ID</a></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-sm-12">
+                  <div class="row subInfo">
+                      <div class="col-sm-2 invoiceId"><span>GR-123456</span></div>
+                      <div class="col-sm-8 invoiceList"><span><a href="account.html" class="grey333">Yudi Nurhandi</a></span></div>
+                      <div class="col-sm-1 invoiceList"><span class="green">Active</span></div>
+                      <div class="col-sm-1 invoiceList"><span class="red">Not Paid</span></div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div><!-- END CONTENT -->
     `,
     directives: [ROUTER_DIRECTIVES],
 })
