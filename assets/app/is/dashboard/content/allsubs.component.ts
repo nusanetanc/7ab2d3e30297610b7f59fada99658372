45a267b4cf3b283ef61eddd2b60900812c9708d5
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-23 18:55:42 +0700

    perbaikan

--- assets/app/is/dashboard/content/allsubs.component.ts
+++ assets/app/is/dashboard/content/allsubs.component.ts
@@ -7,50 +7,7 @@ import { Sub } from './subs';
 @Component({
     selector: 'form-allsubs',
     template: `
-    <!-- Page content -->
-    <div id="page-content-wrapper">
-        <div class="content-header">
-            <h3 id="home">
-                <a id="menu-toggle" href="" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
-                </a>
-                &nbsp; All Subscribers
-            </h3>
 
-        </div>
-
-        <div class="page-content inset" data-spy="scroll" data-target="#spy">
-            <div class="row marginB20 marginR0">
-                <div class="col-sm-12">
-                    <a [routerLink]="['AddSubs']" class="btn btn-default buttonOrange">
-                        NEW SUBSCRIBER
-                    </a>
-                    <a href="" class="glyphicon glyphicon-chevron-down sort-down"></a>
-                    <div class="dropdown right">
-                        <button ng-click="sortBy('name')" class="btn btn-default dropdown-toggle buttonSort" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                            NAME
-                        </button>
-                        <span class="sortorder" ng-show="propertyName === 'name'" ng-class="{reverse: reverse}"></span>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                            <li><a href="#">PAID</a></li>
-                            <li><a href="#">ID</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-12" *ngFor="#sub of subs | orderBy:propertyName:reverse">
-                    <a [routerLink]="['Subscribe', {id: sub._id}]">
-                        <div class="row subInfo">
-                            <div class="col-sm-2 invoiceId"><span>{{ sub.subid }}</span></div>
-                            <div class="col-sm-8 invoiceList"><span><a class="grey333">{{ sub.name | uppercase }}</a></span></div>
-                            <div class="col-sm-1 invoiceList"><span class="green">{{ sub.status }}</span></div>
-                            <div class="col-sm-1 invoiceList"><span class="red">Not Paid</span></div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
     `,
     directives: [ROUTER_DIRECTIVES],
 })
