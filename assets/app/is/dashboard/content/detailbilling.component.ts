4f97312b994b8d098b2aeaae99b92a286d9a63e0
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-24 13:21:56 +0700

    perbaikan

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {ROUTER_DIRECTIVES, RouteParams} from 'angular2/router';
 import { Http } from 'angular2/http';
 import 'rxjs/add/operator/map';
 import { Billing } from './billing';
@@ -243,6 +243,9 @@ export class ContentDetailBillingComponent {
   bills: any[] = [];
 
   constructor(private http: Http) {}
+  constructor(params: RouteParams) {
+      var paramId = params.get("id");
+  }
 
   ngOnInit() {
     this.getBills();
