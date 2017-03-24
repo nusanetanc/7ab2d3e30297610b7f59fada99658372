ad2b080063f93d3090ae148541d63b5446ca842b
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-24 07:20:37 +0700

    perbaikan

--- assets/app/is/dashboard/content/coveragecluster.component.ts
+++ assets/app/is/dashboard/content/coveragecluster.component.ts
@@ -35,25 +35,21 @@ import { Http } from 'angular2/http';
                                   <div class="col-sm-6">
                                       <div class="formNewReport marginLR20">
                                           <form>
-                                              <select name="problemCatagory">
+                                              <select #clustercity id="clustercity">
                                                   <option class="option" disabled="true" selected="true">-- Select City Name --</option>
-                                                  <option value="volvo">Jakarta</option>
-                                                  <option value="saab">Bandung</option>
-                                                  <option value="fiat">All Region</option>
+                                                  <option *ngFor="#city of cities" value={{city.name}}>{{ city.name }}</option>
                                               </select><br/>
                                           </form>
                                           <form>
-                                              <select name="problemCatagory">
+                                              <select #clusterproperty id="clusterproperty">
                                                   <option class="option" disabled="true" selected="true">-- Select Property Name --</option>
-                                                  <option value="volvo">Jakarta</option>
-                                                  <option value="saab">Bandung</option>
-                                                  <option value="fiat">All Region</option>
+                                                  <option *ngFor="#property of propertys" value={{property.name}}>{{ property.name }}</option>
                                               </select><br/>
                                           </form>
-                                          <input type="text" class="form-control inputForm" id="exampleInputName" placeholder="Cluster Name">
-                                          <a href="coverage4.html" class="btn btn-default buttonOrange">
+                                          <input type="text" class="form-control inputForm" #clustername id="clustername" placeholder="Cluster Name">
+                                          <button type="submit" (click)="addCluster(clustername.value, clustercity.value, clusterproperty.value)" class="btn btn-default buttonOrange">
                                               SEND
-                                          </a>
+                                          </button>
                                       </div>
                                   </div>
                               </div>
@@ -68,6 +64,61 @@ import { Http } from 'angular2/http';
     `,
     directives: [ROUTER_DIRECTIVES],
 })
-export class ContentCoverageClusterComponent {
+export class ContentCoverageClusterComponent implements OnInit {
+API = 'http://202.162.207.164:3000';
 
+// Declare empty list of people
+cities: any[] = [];
+propertys: any[] = [];
+clusters: any[] = [];
+
+constructor(private http: Http) {}
+
+// Angular 2 Life Cycle event when component has been initialized
+ngOnInit() {
+    this.getAllCity();
+    this.getAllProperty();
+    this.getAllCluster();
+}
+// Get all City from the API
+    getAllCity() {
+        this.http.get(`${this.API}/city/listcity`)
+            .map(res => res.json())
+            .subscribe(cities => {
+                this.cities = cities
+            })
+    }
+    // Get all Property from the API
+        getAllProperty() {
+            this.http.get(`${this.API}/property/listproperty`)
+                .map(res => res.json())
+                .subscribe(propertys => {
+                    this.propertys = propertys
+                })
+        }
+    // Get all Property from the API
+        getAllCluster() {
+            this.http.get(`${this.API}/cluster/listcluster`)
+                .map(res => res.json())
+                .subscribe(clusters => {
+                    this.clusters = clusters
+                })
+        }
+    addCluster(clustername, clustercity, clusterproperty) {
+
+        var body = `name=${clustername}&city=${clustercity}&property=${clusterproperty}`;
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        this.http
+            .post(`${this.API}/cluster/addcluster`,
+                body, {
+                    headers: headers
+                })
+            .subscribe(data => {
+                alert('Add Cluster Success');
+                this.getAllCluster();
+            }, error => {
+                console.log(JSON.stringify(error.json()));
+            });
+    }
 }
