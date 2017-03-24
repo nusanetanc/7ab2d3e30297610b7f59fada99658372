ead60be3cbea1b4002dc796769ae27726a5de571
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-24 08:28:21 +0700

    perbaikan

--- assets/app/is/dashboard/content/coveragestreet.component.ts
+++ assets/app/is/dashboard/content/coveragestreet.component.ts
@@ -1,6 +1,12 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
-import { Http } from 'angular2/http';
+import { Http, Headers} from 'angular2/http';
+import 'rxjs/add/operator/map';
+import { City } from './cities';
+import { Property } from './property';
+import { Cluster } from './cluster';
+import { Blokfloor } from './blokfloor';
+import { Street } from './street';
 
 @Component({
     selector: 'form-coveragestreet',
@@ -34,42 +40,34 @@ import { Http } from 'angular2/http';
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="formNewReport marginLR20">
-                                        <form>
-                                            <select name="problemCatagory">
-                                                <option class="option" disabled="true" selected="true">-- Select City Name --</option>
-                                                <option value="volvo">Jakarta</option>
-                                                <option value="saab">Bandung</option>
-                                                <option value="fiat">All Region</option>
-                                            </select><br/>
-                                        </form>
-                                        <form>
-                                            <select name="problemCatagory">
-                                                <option class="option" disabled="true" selected="true">-- Select Property Name --</option>
-                                                <option value="volvo">Jakarta</option>
-                                                <option value="saab">Bandung</option>
-                                                <option value="fiat">All Region</option>
-                                            </select><br/>
-                                        </form>
-                                        <form>
-                                            <select name="problemCatagory">
-                                                <option class="option" disabled="true" selected="true">-- Select Cluster Name --</option>
-                                                <option value="volvo">Jakarta</option>
-                                                <option value="saab">Bandung</option>
-                                                <option value="fiat">All Region</option>
-                                            </select><br/>
-                                        </form>
-                                        <form>
-                                            <select name="problemCatagory">
-                                                <option class="option" disabled="true" selected="true">-- Select Block / Floor Name --</option>
-                                                <option value="volvo">Jakarta</option>
-                                                <option value="saab">Bandung</option>
-                                                <option value="fiat">All Region</option>
-                                            </select><br/>
-                                        </form>
-                                        <input type="text" class="form-control inputForm" id="exampleInputName" placeholder="Street Name">
-                                        <a href="coverage6.html" class="btn btn-default buttonOrange">
+                                    <form>
+                                        <select #streetcity id="streetcity">
+                                            <option class="option" disabled="true" selected="true">-- Select City Name --</option>
+                                            <option *ngFor="#city of cities">{{ city._id }}</option>
+                                        </select><br/>
+                                    </form>
+                                    <form>
+                                        <select #streetproperty id="streetproperty">
+                                            <option class="option" disabled="true" selected="true">-- Select Property Name --</option>
+                                            <option *ngFor="#property of propertys" >{{ property._id }}</option>
+                                        </select><br/>
+                                    </form>
+                                    <form>
+                                        <select #streetcluster id="streetcluster">
+                                            <option class="option" disabled="true" selected="true">-- Select Property Name --</option>
+                                            <option *ngFor="#cluster of clusters" >{{ cluster._id }}</option>
+                                        </select><br/>
+                                    </form>
+                                    <form>
+                                        <select #streetblok id="streetblok">
+                                            <option class="option" disabled="true" selected="true">-- Select Block Name --</option>
+                                            <option *ngFor="#blokfloor of blokfloors" >{{ blokfloor._id }}</option>
+                                        </select><br/>
+                                    </form>
+                                        <input type="text" class="form-control inputForm" #streetname id="streetname" placeholder="Street Name">
+                                        <button type="submit" (click)="addBlock(streetname.value, streetblok.value)" class="btn btn-default buttonOrange">
                                             SEND
-                                        </a>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
@@ -82,6 +80,82 @@ import { Http } from 'angular2/http';
     `,
     directives: [ROUTER_DIRECTIVES],
 })
-export class ContentCoverageStreetComponent {
+export class ContentCoverageStreetComponent implements OnInit {
+API = 'http://202.162.207.164:3000';
 
+// Declare empty list of people
+cities: any[] = [];
+propertys: any[] = [];
+clusters: any[] = [];
+blokfloors: any[] = [];
+blokfloors: any[] = [];
+streetnames: any[] = [];
+
+constructor(private http: Http) {}
+
+// Angular 2 Life Cycle event when component has been initialized
+ngOnInit() {
+    this.getAllCity();
+    this.getAllProperty();
+    this.getAllCluster();
+    this.getAllBlock();
+    this.getAllStreet();
+}
+// Get all City from the API
+    getAllCity() {
+        this.http.get(`${this.API}/city/listcity`)
+            .map(res => res.json())
+            .subscribe(cities => {
+                this.cities = cities
+            })
+    }
+    // Get all Property from the API
+        getAllProperty() {
+            this.http.get(`${this.API}/property/listproperty`)
+                .map(res => res.json())
+                .subscribe(propertys => {
+                    this.propertys = propertys
+                })
+        }
+    // Get all Property from the API
+        getAllCluster() {
+            this.http.get(`${this.API}/cluster/listcluster`)
+                .map(res => res.json())
+                .subscribe(clusters => {
+                    this.clusters = clusters
+                })
+        }
+      // Get all BLock from the API
+          getAllBlock() {
+              this.http.get(`${this.API}/blokfloor/listblokfloor`)
+                  .map(res => res.json())
+                  .subscribe(blokfloors => {
+                      this.blokfloors = blokfloors
+                  })
+          }
+      // Get all BLock from the API
+          getAllStreet() {
+              this.http.get(`${this.API}/streetname/liststreetname`)
+                  .map(res => res.json())
+                  .subscribe(streetnames => {
+                      this.streetnames = streetnames
+                  })
+          }
+    addBlock(streetname, blokfloor) {
+
+        var body = `name=${streetname}&blokfloor=${streetblok}`;
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        this.http
+            .post(`${this.API}/streetname/addstreetname`,
+                body, {
+                    headers: headers
+                })
+            .subscribe(data => {
+                alert('Add Street Success');
+                this.getAllStreet();
+            }, error => {
+                console.log(JSON.stringify(error.json()));
+            });
+    }
 }
