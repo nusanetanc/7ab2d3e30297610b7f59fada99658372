39fb628aa6cd7ee638c617f52d2dd9eb3f6da97a
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-24 07:54:36 +0700

    perbaikan

--- assets/app/is/dashboard/content/coverageblock.component.ts
+++ assets/app/is/dashboard/content/coverageblock.component.ts
@@ -1,6 +1,10 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
-import { Http } from 'angular2/http';
+import { Http, Headers} from 'angular2/http';
+import 'rxjs/add/operator/map';
+import { City } from './cities';
+import { Property } from './property';
+import { Cluster } from './cluster';
 
 @Component({
     selector: 'form-coverageblock',
@@ -34,28 +38,22 @@ import { Http } from 'angular2/http';
                         <div class="row">
                             <div class="col-sm-6">
                                 <div class="formNewReport marginLR20">
-                                    <form>
-                                        <select name="problemCatagory">
+                                    <<form>
+                                        <select #blockcity id="blockcity">
                                             <option class="option" disabled="true" selected="true">-- Select City Name --</option>
-                                            <option value="volvo">Jakarta</option>
-                                            <option value="saab">Bandung</option>
-                                            <option value="fiat">All Region</option>
+                                            <option *ngFor="#city of cities">{{ city._id }}</option>
                                         </select><br/>
                                     </form>
                                     <form>
-                                        <select name="problemCatagory">
+                                        <select #blockproperty id="blockproperty">
                                             <option class="option" disabled="true" selected="true">-- Select Property Name --</option>
-                                            <option value="volvo">Jakarta</option>
-                                            <option value="saab">Bandung</option>
-                                            <option value="fiat">All Region</option>
+                                            <option *ngFor="#property of propertys" >{{ property._id }}</option>
                                         </select><br/>
                                     </form>
                                     <form>
-                                        <select name="problemCatagory">
-                                            <option class="option" disabled="true" selected="true">-- Select Cluster Name --</option>
-                                            <option value="volvo">Jakarta</option>
-                                            <option value="saab">Bandung</option>
-                                            <option value="fiat">All Region</option>
+                                        <select #blockcluster id="blockcluster">
+                                            <option class="option" disabled="true" selected="true">-- Select Property Name --</option>
+                                            <option *ngFor="#cluster of clusters" >{{ cluster._id }}</option>
                                         </select><br/>
                                     </form>
                                     <input type="text" class="form-control inputForm" id="exampleInputName" placeholder="Block / Floor Name">
@@ -75,6 +73,61 @@ import { Http } from 'angular2/http';
     `,
     directives: [ROUTER_DIRECTIVES],
 })
-export class ContentCoverageBlockComponent {
+export class ContentCoverageBlockComponent implements OnInit {
+API = 'http://202.162.207.164:3000';
 
+// Declare empty list of people
+cities: any[] = [];
+propertys: any[] = [];
+clusters: any[] = [];
+
+constructor(private http: Http) {}
+
+// Angular 2 Life Cycle event when component has been initialized
+ngOnInit() {
+    this.getAllCity();
+    this.getAllProperty();
+    this.getAllCluster();
+}
+// Get all City from the API
+    getAllCity() {
+        this.http.get(`${this.API}/city/listcity`)
+            .map(res => res.json())
+            .subscribe(cities => {
+                this.cities = cities
+            })
+    }
+    // Get all Property from the API
+        getAllProperty() {
+            this.http.get(`${this.API}/property/listproperty`)
+                .map(res => res.json())
+                .subscribe(propertys => {
+                    this.propertys = propertys
+                })
+        }
+    // Get all Property from the API
+        getAllCluster() {
+            this.http.get(`${this.API}/cluster/listcluster`)
+                .map(res => res.json())
+                .subscribe(clusters => {
+                    this.clusters = clusters
+                })
+        }
+    addCluster(clustername, clusterproperty) {
+
+        var body = `name=${clustername}&property=${clusterproperty}`;
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        this.http
+            .post(`${this.API}/cluster/addcluster`,
+                body, {
+                    headers: headers
+                })
+            .subscribe(data => {
+                alert('Add Cluster Success');
+                this.getAllCluster();
+            }, error => {
+                console.log(JSON.stringify(error.json()));
+            });
+    }
 }
