c0e123c1aa084658f8ad44e91e9b8e790a17754d
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-29 15:28:19 +0700

    perbaikan

--- assets/app/is/app.component.ts
+++ assets/app/is/app.component.ts
@@ -25,7 +25,7 @@ import {ContentProfileEngineerComponent} from "./dashboard/content/profileengine
 import {ContentReplayReportComponent} from "./dashboard/content/replayreports.component";
 import {ContentAllBillsComponent} from "./dashboard/content/allbills.component";
 import {ContentCreateInvoiceComponent} from "./dashboard/content/createinvoice.component";
-import {ContentCreateInvoiceComponent1} from "./dashboard/content/invoice1.component";
+//import {ContentCreateInvoiceComponent1} from "./dashboard/content/invoice1.component";
 import {ContentDetailBillingComponent} from "./dashboard/content/detailbilling.component";
 import {ContentTestComponent} from "./dashboard/content/testsort.component";
 import {ContentCoverageComponent} from "./dashboard/content/coverage.component";
@@ -85,7 +85,7 @@ import {ContentCoverageHomeComponent} from "./dashboard/content/coveragehome.com
         ContentAllBillsComponent,
         AuthenticationComponent,
         ContentCreateInvoiceComponent,
-        ContentCreateInvoiceComponent1,
+        //ContentCreateInvoiceComponent1,
         ContentDetailBillingComponent,
         ContentTestComponent,
         ContentAddCityComponent,
@@ -102,7 +102,7 @@ import {ContentCoverageHomeComponent} from "./dashboard/content/coveragehome.com
     { path: '/is/allsubs', component:ContentAllSubsComponent, name:'AllSubs'},
     { path: '/is/addproperty', component:ContentCoveragePropertyComponent, name:'AddProperty'},
     { path: '/is/createinvoice', component:ContentCreateInvoiceComponent, name:'Createinvoice'},
-    { path: '/is/createinvoice1/:id', component:ContentCreateInvoiceComponent1, name:'Createinvoice1'},
+  //  { path: '/is/createinvoice1/:id', component:ContentCreateInvoiceComponent1, name:'Createinvoice1'},
     { path: '/is/detailbilling/:id', component:ContentDetailBillingComponent, name:'Detailbilling'},
     { path: '/is/addsubs', component:ContentAddSubsComponent, name:'AddSubs'},
     { path: '/is', component:ContentDashboardComponent, name:'Dashboard'},
