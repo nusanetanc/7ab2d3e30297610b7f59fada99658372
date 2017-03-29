07f7e38af07b5b143611d6617b1429c6d689f511
ferdianrb <frizki7@gmail.com>
2017-03-29 10:24:40 +0700

    addsubs

--- assets/app/is/dashboard/content/addsubs.component.ts
+++ assets/app/is/dashboard/content/addsubs.component.ts
@@ -43,11 +43,13 @@ import {Streetname} from "./street_name";
                                                 <input #subemail id="subemail" type="email" class="form-control inputForm" id="exampleInputEmail1" placeholder="Email">
                                                 <input #subdatebirth id="subdatebirth" type="text" class="form-control inputForm" id="exampleInputEmail1" placeholder="Date of Birth (yyyy/mm/dd)">
                                                 <input #subidnumber id="subidnumber" type="text" class="form-control inputForm" id="exampleInputEmail1" placeholder="ID Number">
-                                                /*<p>Upload your National Identity Card</p>
+                                                
+                                                <!--<p>Upload your National Identity Card</p>
                                                 <div class="form-control inputForm">
                                                     <button class="left" type="button" disabled="true">choose file</button>
                                                     <p class="left marginL5">No choose file</p>
-                                                </div>*/
+                                                </div>-->
+                                                
                                             </div>
                                         </form>
                                     </div>
