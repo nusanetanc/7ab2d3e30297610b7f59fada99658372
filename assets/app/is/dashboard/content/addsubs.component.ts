092472896c079184d465716331df317acdd1510f
ferdianrb <frizki7@gmail.com>
2017-03-30 17:18:54 +0700

    addsub

--- assets/app/is/dashboard/content/addsubs.component.ts
+++ assets/app/is/dashboard/content/addsubs.component.ts
@@ -172,7 +172,7 @@ export class ContentAddSubsComponent implements OnInit {
     onSelectCity(_id) {
         console.log(_id);
         this.properties = this.getAllPropertyByCity(){
-            this.http.get(`${this.API}/property/propertybycity/${_id}`)
+            this.http.get(`${this.API}/property/propertybycity/58d3af8b84a03c36aef03a33`)
                 .map(res => res.json())
                 .subscribe(properties => {
                     this.properties = properties
