901e8abab82dde65a229822ef54dcd5470ac2e7a
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-03-30 11:00:35 +0700

    perbaikan

--- assets/app/is/dashboard/content/createinvoice.component.ts
+++ assets/app/is/dashboard/content/createinvoice.component.ts
@@ -186,8 +186,8 @@ import { Sub } from './subs';
                                               </div><!-- /input-group -->
                                             <input type="text" class="form-control inputForm" #routerprice id="routerprice" placeholder="Router Rent Fee" value="40000">
                                             <div class="row">
-                                                <div class="col-sm-12 paddingL35">
-                                                    <form class="paddingTB20 paddingR30">
+                                                <div class="col-sm-12">
+                                                    <form class="paddingTB20">
                                                         <div class="input-group">
                                                             <span class="input-group-addon">
                                                                 <input type="checkbox" aria-label="...">
