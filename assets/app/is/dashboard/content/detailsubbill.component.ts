9c5f76e3a3f94f9eaa0375317bb73a78d37bd71c
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2017-03-31 14:53:35 +0700

    test bill

--- assets/app/is/dashboard/content/detailsubbill.component.ts
+++ assets/app/is/dashboard/content/detailsubbill.component.ts
@@ -165,10 +165,10 @@ import { Sub } from './subs';
             </div>
             
             <!-- Content List -->
-            <div class="row paddingLR15Margin20 paddingLR30">
+            <div class="row paddingLR15Margin20 paddingLR30 marginT20">
                 <div class="col-sm-12">
                     <div class="row headerList">
-                        <div class="col-sm-12 paddingT20 paddingL35 headerSubList"><strong>Billing Information</strong></div>
+                        <div class="col-sm-12 paddingL35 headerSubList"><strong>Billing Information</strong></div>
                     </div>
                     <div class="row subInfo" *ngFor="#bill of bills">
                         <a [routerLink]="['Detailbilling', {id: bill._id}]">
