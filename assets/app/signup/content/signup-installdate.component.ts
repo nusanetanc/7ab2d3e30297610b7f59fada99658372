0b776e426cfe34ead96318479a7a05c5192c7214
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2017-03-31 15:14:52 +0700

    test bill

--- assets/app/signup/content/signup-installdate.component.ts
+++ assets/app/signup/content/signup-installdate.component.ts
@@ -24,7 +24,7 @@ import { Sub } from '../subs';
                                         <div class='col-sm-6'>
                                             <div class="form-group">
                                                 <div class='input-group date' id='datetimepicker1'>
-                                                    <input type='text' class="form-control" />
+                                                    <input type='date' class="form-control" />
                                                     <span class="input-group-addon">
                                                         <span class="glyphicon glyphicon-calendar"></span>
                                                     </span>
