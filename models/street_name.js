80cb30496d56ac5f604b799361b6706a6a915530
ferdianrb <frizki7@gmail.com>
2017-03-31 11:24:53 +0700

    addsubs

--- models/street_name.js
+++ models/street_name.js
@@ -4,6 +4,6 @@ var BlokfloorSchema = require('../models/blokfloor');
 
 var StreetnameSchema   = new Schema({
     name: String,
-    blok: {type: Schema.Types.ObjectId, ref: 'BlokfloorSchema'}
+    blokfloor: {type: Schema.Types.ObjectId, ref: 'BlokfloorSchema'}
 });
 module.exports = mongoose.model('Streetname', StreetnameSchema)
