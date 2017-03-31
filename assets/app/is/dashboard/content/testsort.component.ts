de8e1ef793d7b01dc299e5622ad4995c85881394
Ahmad Rifki Fachrudin <fahmadrifki@gmail.com>
2017-03-31 13:48:35 +0700

    test bill

--- assets/app/is/dashboard/content/testsort.component.ts
+++ assets/app/is/dashboard/content/testsort.component.ts
@@ -8,43 +8,45 @@ import 'rxjs/add/operator/map';
     selector: 'form-test',
     template: `
     <div id="page-content-wrapper">
-          <div class="content-header">
-              <h3 id="home">
-                  <a id="menu-toggle" href="" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
-                  </a>
-                  &nbsp; Test
-              </h3>
+        <div class="content-header">
+            <h3 id="home">
+                <a id="menu-toggle" href="" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
+                </a>
+                Test
+            </h3>
+        </div>
     
-          </div>
-    
-          <div class="page-content inset" data-spy="scroll" data-target="#spy">
-              <div class="row marginB20 marginR0">
-                  <div class="col-sm-12">
-                      <a href="" class="glyphicon glyphicon-chevron-down sort-down"></a>
-                      <div class="dropdown right">
-                          <button (click)="byId()" class="btn btn-default dropdown-toggle buttonSort" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                              ID
-                          </button>
-                          <button (click)="byName()" class="btn btn-default dropdown-toggle buttonSort" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                              NAME
-                          </button>
-                          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                              <li><a href="#">NAME</a></li>
-                              <li><a href="#">ID</a></li>
-                          </ul>
-                      </div>
-                  </div>
-              </div>
-              <div class="row">
-                  <div class="col-sm-12" *ngFor="#names of names">
-                      <div class="row subInfo">
-                          <div class="col-sm-2 invoiceId"><span>{{ names.id }}</span></div>
-                          <div class="col-sm-5 invoiceList"><span>{{ names.first_name }}</span></div>
-                          <div class="col-sm-5 invoiceList"><span>{{ names.last_name }}</span></div>
-                      </div>
-                  </div>
-              </div>
-          </div>
+        <div class="page-content inset" data-spy="scroll" data-target="#spy">
+            <div class="row marginB20 marginR0">
+                <div class="col-sm-12">
+                    <a [routerLink]="['AddSubs']" class="btn btn-default buttonOrange">
+                        NEW SUBSCRIBER
+                    </a>
+                    <a href="" class="glyphicon glyphicon-chevron-down sort-down"></a>
+                    <div class="dropdown right">
+                        <button (click)="sortByName()" class="btn btn-default dropdown-toggle buttonSort" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            NAME
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                            <li><a href="#">NAME</a></li>
+                            <li><a href="#">ID</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12" *ngFor="#sub of subs">
+                    <a [routerLink]="['Subscribe', {id: sub._id}]">
+                        <div class="row subInfo">
+                            <div class="col-sm-2 invoiceId"><span>{{ sub.subid }}</span></div>
+                            <div class="col-sm-8 invoiceList"><span><a href="account.html" class="grey333">{{ sub.name }}</a></span></div>
+                            <div class="col-sm-1 invoiceList"><span class="green">{{ sub.status }}</span></div>
+                            <div class="col-sm-1 invoiceList"><span class="red">Not Paid</span></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div><!-- END CONTENT -->
     `,
     directives: [ROUTER_DIRECTIVES],
@@ -56,7 +58,42 @@ import 'rxjs/add/operator/map';
 
 export class ContentTestComponent {
 
-    names = [
+    // Sort By
+    sortByName(){
+        this.subs.sort( function(a, b) {
+            if ( a.name < b.name ){
+                return -1;
+            }else if( a.name > b.name ){
+                return 1;
+            }else{
+                return 0;
+            }
+        });
+    }
+
+    // Link to our api, pointing to localhost
+    API = 'http://202.162.207.164:3000';
+
+    // Declare empty list of people
+    subs: any[] = [];
+
+    constructor(private http: Http) {}
+
+    // Angular 2 Life Cycle event when component has been initialized
+    ngOnInit() {
+        this.getAllSub();
+    }
+
+    // Get all users from the API
+    getAllSub() {
+        this.http.get(`${this.API}/subscribe/listsub`)
+            .map(res => res.json())
+            .subscribe(subs => {
+                this.subs = subs
+            })
+    }
+
+    /*names = [
         {"id": 1,"first_name": "Jason","last_name": "Martin"},
         {"id": 2,"first_name": "Douglas","last_name": "Holmes"},
         {"id": 3,"first_name": "Randy","last_name": "Woods"},
@@ -104,7 +141,7 @@ export class ContentTestComponent {
                 return 0;
             }
         });
-    }
+    }*/
 
     /*transform(array: Array<string>, args: string): Array<string> {
         array.sort((a: any, b: any) => {
