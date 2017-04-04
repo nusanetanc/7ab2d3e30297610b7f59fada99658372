a7542311a8dccc54584e98e8f00c875f46441e5c
ferdianrb <frizki7@gmail.com>
2017-04-04 17:21:12 +0700

    update

--- assets/app/is/dashboard/content/addsubs.component.ts
+++ assets/app/is/dashboard/content/addsubs.component.ts
@@ -43,12 +43,10 @@ import {Streetname} from "./street_name";
                                                 <input #subemail id="subemail" type="email" class="form-control inputForm" id="exampleInputEmail1" placeholder="Email">
                                                 <input #subdatebirth id="subdatebirth" type="text" class="form-control inputForm" id="exampleInputEmail1" placeholder="Date of Birth (yyyy/mm/dd)">
                                                 <input #subidnumber id="subidnumber" type="text" class="form-control inputForm" id="exampleInputEmail1" placeholder="NIK">
-                                                <input type="file" (change)="fileChangeEvent($event)" placeholder="Upload file..." />
-                                                <!--<p>Upload your National Identity Card</p>
+                                                <p>Upload your National Identity Card</p>
                                                 <div class="form-control inputForm">
-                                                    <button class="left" type="button" disabled="true">choose file</button>
-                                                    <p class="left marginL5">No choose file</p>
-                                                </div>-->
+                                                    <input type="file" (change)="fileChangeEvent($event)" placeholder="Upload file..." />                                                    
+                                                </div>
                                                 
                                                 <!-- komen -->
                                                 
