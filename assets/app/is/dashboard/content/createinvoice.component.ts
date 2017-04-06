53366f26dcf1a9e749f7daa2e177474e8df2a434
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-06 11:11:46 +0700

    perbaikan

--- assets/app/is/dashboard/content/createinvoice.component.ts
+++ assets/app/is/dashboard/content/createinvoice.component.ts
@@ -286,7 +286,7 @@ bills: any[] = [];
 subs: any[] = [];
 totalharga: number;
       constructor(private http: Http, private _routeParams: RouteParams) {
-        this.totalharga = subs['packprice'];
+        this.totalharga = this.subs['packprice'];
       }
   onKey(event:KeyboardEvent) {
     this.val = (event.target).value;
