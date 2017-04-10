1e284b86b5af600e7f855b14d7a6c09abc9679e4
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-10 16:26:48 +0700

    perbaikan

--- assets/app/is/dashboard/content/createinvoice.component.ts
+++ assets/app/is/dashboard/content/createinvoice.component.ts
@@ -184,7 +184,7 @@ import { Sub } from './subs';
                                     <span>:</span>
                                 </div>
                                 <div class="col-xs-12 col-md-7">
-                                    <span (updateTime)="updateMyTime()">{{date | date: format}}</span>
+                                    <span></span>
                                 </div>
                             </div>
                             <div class="row marginTB10 marginL5">
