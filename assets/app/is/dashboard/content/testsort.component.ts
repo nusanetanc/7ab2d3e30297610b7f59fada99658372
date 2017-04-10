f6f9331da34b527b63f931eb9d39a005135047fc
fahmadrifki <fahmadrifki@gmail.com>
2017-04-10 11:40:41 +0700

    update

--- assets/app/is/dashboard/content/testsort.component.ts
+++ assets/app/is/dashboard/content/testsort.component.ts
@@ -247,6 +247,16 @@ export class ContentTestComponent {
         var doc = new jsPDF();
         doc.text(20, 20, 'Hello world!');
         doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+
+        // Optional - set properties on the document
+        doc.setProperties({
+            title: 'Title',
+            subject: 'This is the subject',
+            author: 'Author Name',
+            keywords: 'generated, javascript, web 2.0, ajax',
+            creator: 'Creator Name'
+        });
+
         doc.addPage();
         doc.text(20, 20, 'Do you like that?');
 
