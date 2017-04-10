3c69a513b6b96c6c3ac48a58b1be65ce16ee31bd
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-10 11:33:42 +0700

    perbaikan

--- assets/app/is/dashboard/content/createinvoice.component.ts
+++ assets/app/is/dashboard/content/createinvoice.component.ts
@@ -331,9 +331,9 @@ totalbayar: number;
             this.getAllBill();
             this.getSubs();
           }
-          if(${this.paketlev} == "1"){
+          //if(${this.paketlev} == "1"){
                   public prices = {hargastb: 45000, hargarouter: 40000, hargains: 40000};
-          }
+          //}
 
 // Add one person to the API
   createInvoice(billingdate, billingduedate, subsid, namepackage, packageprice, routerprice, stbprice, cablej45price, instalationprice, subtotal, promoname, promoprice, taxprice, totalprice) {
