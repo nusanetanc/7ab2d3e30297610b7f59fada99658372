41ba2e0d2bf69303994d61b77d1e268de611b247
fahmadrifki <fahmadrifki@gmail.com>
2017-04-10 11:50:02 +0700

    update

--- assets/app/is/dashboard/content/testsort.component.ts
+++ assets/app/is/dashboard/content/testsort.component.ts
@@ -247,6 +247,7 @@ export class ContentTestComponent {
         var doc = new jsPDF();
         doc.text(20, 20, 'Hello world!');
         doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+        doc.text(50, 60, 30, 30, 'This is client-side Javascript, pumping out a PDF.');
 
         doc.addPage();
         doc.text(20, 20, 'Do you like that?');
