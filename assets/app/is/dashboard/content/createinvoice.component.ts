c825703ec79b73fbba976844e8d606bc93cef283
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-11 09:09:08 +0700

    perbaikan

--- assets/app/is/dashboard/content/createinvoice.component.ts
+++ assets/app/is/dashboard/content/createinvoice.component.ts
@@ -196,7 +196,7 @@ import { Sub } from './subs';
                                     <span>:</span>
                                 </div>
                                 <div class="col-xs-12 col-md-7">
-                                    <input value="{{today | date: 'yyyy/MM/'}}{{today.getDate()+3}}" class="form-control inputForm" #duedate id="duedate" placeholder="Due Date" disabled/>
+                                    <input value="{{today | date: 'MM/yyyy/'}}{{today.getDate()+3}}" class="form-control inputForm" #duedate id="duedate" placeholder="Due Date" disabled/>
                                 </div>
                             </div>
                             <div class="row marginTB10 marginL5">
