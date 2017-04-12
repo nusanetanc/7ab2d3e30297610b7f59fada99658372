e793b3ec9d9661c6f2d59f0d8b87d054eccf98ba
fahmadrifki <fahmadrifki@gmail.com>
2017-04-12 14:40:25 +0700

    update

--- assets/app/is/dashboard/content/testsort.component.ts
+++ assets/app/is/dashboard/content/testsort.component.ts
@@ -3,6 +3,7 @@ import {Pipe} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import { Http } from 'angular2/http';
 import 'rxjs/add/operator/map';
+import {ModalComponent} from "./dashboard/content/testmodal.component";
 
 declare let jsPDF;
 declare let kendo;
@@ -32,42 +33,7 @@ export class ContentTestComponent {
     public readonly modal: ModalComponent;
 }
 
-@Component({
-    selector: 'app-modal',
-    template: `
-  <div (click)="hide()" class="modal fade" tabindex="-1" [ngClass]="{'in': visibleAnimate}"
-       [ngStyle]="{'display': visible ? 'block' : 'none', 'opacity': visibleAnimate ? 1 : 0}">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <ng-content select=".app-modal-header"></ng-content>
-        </div>
-        <div class="modal-body">
-          <ng-content select=".app-modal-body"></ng-content>
-        </div>
-        <div class="modal-footer">
-          <ng-content select=".app-modal-footer"></ng-content>
-        </div>
-      </div>
-    </div>
-  </div>
-  `
-})
-export class ModalComponent {
 
-    public visible = false;
-    private visibleAnimate = false;
-
-    public show(): void {
-        this.visible = true;
-        setTimeout(() => this.visibleAnimate = true);
-    }
-
-    public hide(): void {
-        this.visibleAnimate = false;
-        setTimeout(() => this.visible = false, 300);
-    }
-}
 
 /* @Component({
     selector: 'form-test',
