41e77d60921f6096a9c909f571aac96782e51120
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-12 10:26:42 +0700

    perbaikan

--- assets/app/is/dashboard/content/createinvoice.component.ts
+++ assets/app/is/dashboard/content/createinvoice.component.ts
@@ -276,10 +276,10 @@ import { Sub } from './subs';
                                 </div>
                                 <div class="col-xs-12 col-md-7">
                                     <input *ngIf="subs.status == 'registrasi'" value="0" type="number" class="form-control inputForm" #cablerj45price id="cablerj45price" placeholder="Cable/Rj45 Price"/>
-                                    <input #key_val (keyup)="0" id="cablerj45price" *ngIf="subs.status != 'registrasi'" value="" type="number" class="form-control inputForm" placeholder="Cable/Rj45 Price"/>
+                                    <input #cablerj45price id="cablerj45price" (keyup)="0" *ngIf="subs.status != 'registrasi'" type="number" class="form-control inputForm" placeholder="Cable/Rj45 Price"/>
                                 </div>
                             </div>
-                            <p>{{key_val.value}}</p>
+                            {{cablerj45price.value}}
                             <div class="row marginTB10 marginL5">
                                 <div class="col-xs-6 col-sm-4">
                                     <span>Promo Name</span>
