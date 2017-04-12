1f817efbcc8a32da501d13b4cfa1a5072fb868a7
fahmadrifki <fahmadrifki@gmail.com>
2017-04-12 11:17:53 +0700

    update

--- assets/app/is/dashboard/content/testsort.component.ts
+++ assets/app/is/dashboard/content/testsort.component.ts
@@ -37,7 +37,7 @@ declare let kendo;
                         <!-- /Row Button -->
 
                         <!-- Content List -->
-                        <div style="zoom: 80%; -moz-transform: scale(0.8); -moz-transform-origin: 0 0;">
+                        <div style="zoom: 80%; -moz-transform: scale(0.9); -moz-transform-origin: 0 0;">
                         
                         <div id="formConfirmation">
 
