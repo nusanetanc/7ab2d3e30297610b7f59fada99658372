63c78c009fd21f6d3c35b0fc55efa02f01333f49
fahmadrifki <fahmadrifki@gmail.com>
2017-04-12 10:30:14 +0700

    update

--- assets/app/is/dashboard/content/testsort.component.ts
+++ assets/app/is/dashboard/content/testsort.component.ts
@@ -37,7 +37,8 @@ declare let kendo;
                         <!-- /Row Button -->
 
                         <!-- Content List -->
-                        <div class="container" width="50%">
+                        <div class="row rowBillInfoContList">
+                        
                         <div id="formConfirmation">
 
                             <!-- Content List -->
@@ -249,6 +250,7 @@ declare let kendo;
                             <!-- /Content List -->
                         
                         </div>
+                        
                         </div>
                         <!-- /Content List -->
                         
