b6868904fc148ebcd63c193b5735642194205c9b
fahmadrifki <fahmadrifki@gmail.com>
2017-04-12 15:48:09 +0700

    update

--- assets/app/is/dashboard/content/modal.module.ts
+++ assets/app/is/dashboard/content/modal.module.ts
@@ -2,14 +2,13 @@ import { NgModule} from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { BrowserModule } from '@angular/platform-browser';
 import { BootstrapModalModule } from 'ng2-bootstrap-modal';
+import {ConfirmModalComponent} from "./dashboard/content/confirmmodal.component";
 import { AppComponent } from './app.component';
 
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import { Http } from 'angular2/http';
 import 'rxjs/add/operator/map';
-
-import {ConfirmModalComponent} from "./dashboard/content/confirmmodal.component";
 
 @NgModule({
     declarations: [
