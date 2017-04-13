4274a8dd05d123cfdda04b2ef332218ae870eaa5
ferdianrb <frizki7@gmail.com>
2017-04-13 10:27:13 +0700

    new report

--- assets/app/my/dashboard/content/newreport.component.ts
+++ assets/app/my/dashboard/content/newreport.component.ts
@@ -49,7 +49,7 @@ import { Problem } from './problem';
                                 </a>
                             </div>
                         </div>
-                        <div *ngIf="!selectedProblem" class="col-sm-6">
+                        <div *ngIf="!selectedDescProblem" class="col-sm-6">
                             <div class="alertNewReports">
                                 <div class="row">
                                     <div class="col-sm-1">
@@ -61,7 +61,7 @@ import { Problem } from './problem';
                                 </div>
                             </div>
                         </div>
-                        <div *ngIf="selectedProblem" class="col-sm-6">
+                        <div *ngIf="selectedDescProblem" class="col-sm-6">
                             <div class="alertNewReports">
                                 <div class="row">
                                     <div class="col-sm-1">
