64d17b65bf0aa33fa1bb93edc89c57e80c6a8f36
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-13 14:07:58 +0700

    perbaikan

--- assets/app/is/dashboard/content/createinvoice.component.ts
+++ assets/app/is/dashboard/content/createinvoice.component.ts
@@ -350,15 +350,9 @@ import { Sub } from './subs';
                     </div>
                 </div>
                 <div class="row" style="margin-left: 3px !important;">
-                    <div class="col-sm-12" *ngIf="subs.status == 'registrasi'">
+                    <div class="col-sm-12">
                         <!-- Small modal -->
-                        <button *ngIf="subs.packlev == '4' || subs.packlev == '5' || subs.packlev == '6'" type="submit" (click)="createInvoice1(invoicedate.value, duedate.value, namepackage.value, packageprice.value, routerprice.value, stbprice1.value, cablerj45price1.value, instalationprice1.value, subtotal.value, promoname.value, promoprice.value, taxprice.value, totalprice.value)" class="btn btn-default buttonOrange marginT20 marginL20 paddingL10">CONFIRM1</button>
-                        <button *ngIf="subs.packlev == '1' || subs.packlev == '2' || subs.packlev == '3'" type="submit" (click)="createInvoice2(invoicedate.value, duedate.value, namepackage.value, packageprice.value, routerprice.value, stbprice2.value, cablerj45price1.value, instalationprice1.value, subtotal.value, promoname.value, promoprice.value, taxprice.value, totalprice.value)" class="btn btn-default buttonOrange marginT20 marginL20 paddingL10">CONFIRM2</button>
-                    </div>
-                    <div class="col-sm-12" *ngIf="subs.status != 'registrasi'">
-                        <!-- Small modal -->
-                        <button *ngIf="subs.packlev == '4' || subs.packlev == '5' || subs.packlev == '6'" type="submit" (click)="createInvoice3(invoicedate.value, duedate.value, namepackage.value, packageprice.value, routerprice.value, stbprice1.value, cablerj45price2.value, instalationprice2.value, subtotal.value, promoname.value, promoprice.value, taxprice.value, totalprice.value)" class="btn btn-default buttonOrange marginT20 marginL20 paddingL10">CONFIRM3</button>
-                        <button *ngIf="subs.packlev == '1' || subs.packlev == '2' || subs.packlev == '3'" type="submit" (click)="createInvoice4(invoicedate.value, duedate.value, namepackage.value, packageprice.value, routerprice.value, stbprice2.value, cablerj45price2.value, instalationprice2.value, subtotal.value, promoname.value, promoprice.value, taxprice.value, totalprice.value)" class="btn btn-default buttonOrange marginT20 marginL20 paddingL10">CONFIRM4</button>
+                        <button type="submit" (click)="createInvoice1(invoicedate.value, duedate.value, namepackage.value, packageprice.value, routerprice.value, subtotal.value, promoname.value, promoprice.value, taxprice.value, totalprice.value)" class="btn btn-default buttonOrange marginT20 marginL20 paddingL10">CONFIRM1</button>
                     </div>
                 </div>
             </div>
@@ -399,11 +393,10 @@ total:number;
 
 
 // Add one person to the API
-  createInvoice4(invoicedate, duedate, namepackage, packageprice, routerprice, stbprice2, cablerj45price2, instalationprice2, subtotal, promoname, promoprice, taxprice, totalprice) {
+  createInvoice4(invoicedate, duedate, namepackage, packageprice, routerprice, subtotal, promoname, promoprice, taxprice, totalprice) {
 alert('test');
-  var body = `namepack=${namepackage}&pricepack=${packageprice}&priceinstal=${instalationprice2}&pricerouter=${routerprice}&
-  pricestb=${stbprice2}&pricerj45cable=${cablerj45price2}&promoname=${promoname}&pricepromo=${promoprice}
-  &changetax=${taxprice}&totalprice=${subtotal}&totalpay=${totalprice}&billdate=${billingdate}&duedate=${billingduedate}
+  var body = `namepack=${namepackage}&pricepack=${packageprice}&pricerouter=${routerprice}&
+  promoname=${promoname}&pricepromo=${promoprice}&changetax=${taxprice}&totalprice=${subtotal}&totalpay=${totalprice}&billdate=${billingdate}&duedate=${billingduedate}
   &status='Waiting For Payment'&sub=${this._routeParams.get('id')}`;
   var headers = new Headers();
   headers.append('Content-Type', 'application/x-www-form-urlencoded');
