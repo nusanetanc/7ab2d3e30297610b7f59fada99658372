bf68d42080a4811e81de1bae5c608691f626724f
fahmadrifki <fahmadrifki@gmail.com>
2017-04-13 10:58:16 +0700

    update

--- assets/app/is/dashboard/content/testsort.component.ts
+++ assets/app/is/dashboard/content/testsort.component.ts
@@ -1,6 +1,8 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import { Http } from 'angular2/http';
+import { Overlay, overlayConfigFactory } from 'angular2-modal';
+import { Modal, BSModalContext } from 'angular2-modal/plugins/bootstrap';
 import 'rxjs/add/operator/map';
 
 declare let jsPDF;
@@ -265,9 +267,31 @@ declare let kendo;
 
 export class ContentTestComponent {
 
-    constructor() {
+    constructor(public modal: Modal) {
     }
 
+    get version(): string {
+        return window.angular2ModalVer;
+    }
+
+    onClick() {
+        this.modal.alert()
+            .size('lg')
+            .showClose(true)
+            .title('A simple Alert style modal window')
+            .body(`
+            <h4>Alert is a classic (title/body/footer) 1 button modal window that 
+            does not block.</h4>
+            <b>Configuration:</b>
+            <ul>
+                <li>Non blocking (click anywhere outside to dismiss)</li>
+                <li>Size large</li>
+                <li>Dismissed with default keyboard key (ESC)</li>
+                <li>Close wth button click</li>
+                <li>HTML content</li>
+            </ul>`)
+            .open();
+    }
 
     public print() {
 
