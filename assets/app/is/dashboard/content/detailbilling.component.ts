d9c4a435a94aa42203501f7deca735cba1304792
fahmadrifki <fahmadrifki@gmail.com>
2017-04-13 09:47:48 +0700

    update

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -236,7 +236,7 @@ declare let kendo;
         <!-- Page Content -->
         
         <!-- Content Print -->
-        <div class="print" style="padding: 0px 20px;">
+        <div class="print" style="padding: 0px 100px;">
            <div id="formConfirmation">
               <!-- Content List -->
               <div id="body" class="container" style="background-color: #ffffff;padding: 40px 40px; color: #000; font-weight:300;">
