69cc51900ceee33497a26c60b28d5d741621f794
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-17 17:12:42 +0700

    upload

--- assets/app/signup/content/signup.component.ts
+++ assets/app/signup/content/signup.component.ts
@@ -142,7 +142,7 @@ import {Streetname} from "./street_name";
                      </div>
                  </div>
                  <div class="row" *ngIf="clickedItem.name == 'regDone'">
-                    <div class="col-md-12 col-md-offset-6">
+                    <div class="col-md-12">
                         <div class="row" style="margin-bottom: 10px; text-align: center;">
                         All Set! We Will See You At The Date And Time You Chose
                         And A Confirmation Email Was Sent With Details Below.
