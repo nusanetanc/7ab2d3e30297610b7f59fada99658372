89f3ef68e509727326ddf090039a2c826a4e0bc0
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-17 15:00:30 +0700

    upload

--- assets/app/signup/content/signup.component.ts
+++ assets/app/signup/content/signup.component.ts
@@ -74,7 +74,7 @@ import {Streetname} from "./street_name";
                             </form>
                         </div>
                       </div>
-                      <div class="row"
+                      <div class="row">
                         <div class="col-md-4 col-md-offset-4">
                             <button *ngFor="#Item of Items" (click)="onItemClicked(Item)" class="back btn btn-default dropdown-toggle">
                                 {{ Item.name }}
