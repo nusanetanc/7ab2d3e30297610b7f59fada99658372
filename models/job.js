ea8df26a003b536dc85c9b4733313243f131544b
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-18 14:44:24 +0700

    upload

--- models/job.js
+++ models/job.js
@@ -1,12 +1,14 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
+var Emp = require('../models/employee');
+var Sub = require('../models/subs');
 
 var JobSchema   = new Schema({
   name: String,
   status: String,
-  emp1: String,
-  emp2: String,
-  subs: String
+  emp1: {type: Schema.Types.ObjectId, ref: 'Emp'},
+  emp2: {type: Schema.Types.ObjectId, ref: 'Emp'},
+  subs: {type: Schema.Types.ObjectId, ref: 'Sub'}
 });
 
 module.exports = mongoose.model('Job', JobSchema);
