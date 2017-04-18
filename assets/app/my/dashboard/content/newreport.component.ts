10904fb9740ff69098262005b21e8550b03d6d14
ferdianrb <frizki7@gmail.com>
2017-04-18 09:43:44 +0700

    new report

--- assets/app/my/dashboard/content/newreport.component.ts
+++ assets/app/my/dashboard/content/newreport.component.ts
@@ -44,7 +44,7 @@ import { Problem } from './problem';
                                     </select><br/>
                                 </form>
                                 <textarea id="message" class="input width100" name="message" rows="10" placeholder="*note"></textarea>
-                                <a (click)="addReport(category.value, subcategory.value, subs._id, today.{{today|date:'medium'}} )" class="btn btn-default">
+                                <a (click)="addReport(category.value, subcategory.value, subs._id, {{today|date:'medium'}} )" class="btn btn-default">
                                     SEND
                                 </a>
                             </div>
