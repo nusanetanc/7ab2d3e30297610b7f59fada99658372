b71a44da97ddb3e9d2d02884980c08ffd4c393fd
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-20 08:23:25 +0700

    package

--- assets/app/is/dashboard/content/allemployee.component.ts
+++ assets/app/is/dashboard/content/allemployee.component.ts
@@ -38,7 +38,7 @@ import {Employee} from './employee';
             </div>
             <div class="row">
               <div class="col-sm-12" *ngFor="#emp of emps">
-                <a [routerLink]="['ProfileEmployee', {id: emp._id}]">
+                <a [routerLink]="['ProfileEngineer', {id: emp._id}]">
                   <div class="row subInfo fontWeight300">
                       <div class="col-sm-2 invoiceId"><span>{{emp.idemployee}}</span></div>
                       <div class="col-sm-4 invoiceList"><span>{{emp.name}}</span></div>
@@ -56,7 +56,6 @@ import {Employee} from './employee';
 export class ContentAllEmployeeComponent {
   // Link to our api, pointing to localhost
   API = 'http://202.162.207.164:3000';
-  departement = 'technical';
 
   // Declare empty list of people
   emps: any[] = [];
