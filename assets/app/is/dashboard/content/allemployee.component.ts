22382a864463d1cd0dc3f6574a7b56395ff57c7b
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-20 08:05:00 +0700

    package

--- assets/app/is/dashboard/content/allemployee.component.ts
+++ assets/app/is/dashboard/content/allemployee.component.ts
@@ -1,6 +1,6 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
-import { Http } from 'angular2/http';
+import { Http, Response, Headers, Request, RequestOptions, RequestMethod, URLSearchParams } from 'angular2/http';
 import {Employee} from './employee';
 
 
@@ -51,25 +51,26 @@ import {Employee} from './employee';
     directives: [ROUTER_DIRECTIVES],
 })
 export class ContentAllEmployeeComponent {
-    // Link to our api, pointing to localhost
-    API = 'http://202.162.207.164:3000';
+  // Link to our api, pointing to localhost
+  API = 'http://202.162.207.164:3000';
+  departement = 'technical';
 
-    // Declare empty list of people
-    emps: any[] = [];
+  // Declare empty list of people
+  emps: any[] = [];
 
-    constructor(private http: Http) {}
+  constructor(private http: Http) {}
 
-    // Angular 2 Life Cycle event when component has been initialized
-    ngOnInit() {
-        this.getAllEmployee();
-    }
+  // Angular 2 Life Cycle event when component has been initialized
+  ngOnInit() {
+      this.getAllEmployee();
+  }
 
-    // Get all users from the API
-    getAllEmployee() {
-        this.http.get(`${this.API}/employee/listemp`)
-            .map(res => res.json())
-            .subscribe(emps => {
-                this.emps = emps
-            })
-    }
+  // Get all users from the API
+  getAllEmployee() {
+      this.http.get(`${this.API}/employee/listemp`)
+          .map(res => res.json())
+          .subscribe(emps => {
+              this.emps = emps
+          })
+  }
 }
