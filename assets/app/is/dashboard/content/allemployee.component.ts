36d336bd2048cd35f7cc38acd87602cf3bfa10c4
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-20 08:13:18 +0700

    package

--- assets/app/is/dashboard/content/allemployee.component.ts
+++ assets/app/is/dashboard/content/allemployee.component.ts
@@ -41,8 +41,9 @@ import {Employee} from './employee';
                 <a [routerLink]="['ProfileEngineer', {id: emp._id}]">
                   <div class="row subInfo fontWeight300">
                       <div class="col-sm-2 invoiceId"><span>{{emp.idemployee}}</span></div>
-                      <div class="col-sm-8 invoiceList"><span>{{emp.name}}</span></div>
-                      <div class="col-sm-2 invoiceList"><span>{{emp.titlejob}}</span></div>
+                      <div class="col-sm-4 invoiceList"><span>{{emp.name}}</span></div>
+                      <div class="col-sm-3 invoiceList"><span>{{emp.departement}}</span></div>
+                      <div class="col-sm-3 invoiceList"><span>{{emp.titlejob}}</span></div>
                   </div>
                 </a>
               </div>
