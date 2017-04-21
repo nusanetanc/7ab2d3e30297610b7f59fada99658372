1f2d187ab214f79eb6eac5c4d25cf7a9ba753ca6
fahmadrifki <fahmadrifki@gmail.com>
2017-04-21 09:30:56 +0700

    update

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -243,7 +243,7 @@ declare let kendo;
                         
                         
                         <!-- Content Print -->
-                        <div class="print" style="width: 50%;">
+                        <div class="print" style="width: 10%;">
                            <div id="printPenagihan">
                               <!-- Content List -->
                               <div id="body" class="container" style="background-color: #ffffff;padding: 40px 40px; color: #000; font-weight:300;">
