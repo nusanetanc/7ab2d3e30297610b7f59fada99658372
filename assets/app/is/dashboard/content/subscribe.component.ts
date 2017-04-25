91362ecc54d84f8a1caa0f9cfa0c15d94fd13426
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-25 08:46:41 +0700

    package

--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -1,11 +1,10 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component, OnInit, OnDestroy} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteParams} from 'angular2/router';
 import { Http } from 'angular2/http';
 import 'rxjs/add/operator/map';
 import {Subscription} from "rxjs/Rx";
 import { Sub } from './subs';
 import { Job } from './job';
-import { City } from './cities';
 
 @Component({
     selector: 'form-subscriber',
@@ -172,35 +171,7 @@ import { City } from './cities';
                         <div class="col-sm-6">
                             <div class="formNewReport marginLR20">
                                 <input #datejob type="date" class="form-control inputForm" id="datejob" placeholder="Date Job">
-                                <form>
-                                    <select #typejob id="typejob">
-                                        <option class="option" disabled="true" selected="true" value="0">-- Select Type Job --</option>
-                                        <option class="option" value="Installation">Installation</option>
-                                        <option class="option" value="Maintenance">Maintenance</option>
-                                        <option class="option" value="Switch Devices">Switch Devices</option>
-                                        <option class="option" value="Take Device">Take Device</option>
-                                    </select><br/><br/>
-                                </form>
-                                <textarea #detailjob id="detailjob" placeholder="Input Detail Job" class="form-control inputForm" rows="4" cols="50"></textarea>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                      <form>
-                                          <select #empjob1 id="empjob1" class="form-control inputForm">
-                                              <option class="option" disabled="true" value="0" selected="true">-- Select Field Engineer --</option>
-                                              <option *ngFor="#emp of emps" class="option">{{ emp._id }}</option>
-                                          </select><br/>
-                                      </form>
-                                    </div>
-                                    <div class="col-sm-6">
-                                      <form>
-                                          <select #empjob2 id="empjob2" class="form-control inputForm">
-                                              <option class="option" disabled="true" value="0" selected="true">-- Select Field Engineer --</option>
-                                              <option *ngFor="#emp of emps" class="option">{{ emp._id }}</option>
-                                          </select><br/><br/>
-                                      </form>
-                                    </div>
-                                </div>
-                                <button type="submit" (click)="addJob(detailjob.value)" class="btn btn-default buttonOrange">
+                                <button type="submit" (click)="addJob(datejob.value)" class="btn btn-default buttonOrange">
                                     SEND
                                 </button>
                             </div>
@@ -220,35 +191,60 @@ import { City } from './cities';
     directives: [ROUTER_DIRECTIVES],
 })
 export class ContentSubscribeComponent implements OnInit {
-API = 'http://202.162.207.164:3000';
-cities: City[];
-  ngOnInit() {
-      this.getAllCity();
-  }
-  constructor(private http: Http) {}
-  // Get all City from the API
-      getAllCity() {
-          this.http.get(`${this.API}/city/listcity`)
-              .map(res => res.json())
-              .subscribe(cities => {
-                  this.cities = cities
-              })
-      }
-  addJob(detailjob) {
+  // Link to our api, pointing to localhost
+    API = 'http://202.162.207.164:3000';
 
-      var body = `name=${detailjob}`;
-      var headers = new Headers();
-      headers.append('Content-Type', 'application/x-www-form-urlencoded');
-      this.http
-          .post(`${this.API}/city/addcity`,
-              body, {
-                  headers: headers
-              })
-          .subscribe(data => {
-              alert('Add City Success');
-              this.getAllCity();
-          }, error => {
-              console.log(JSON.stringify(error.json()));
-          });
+    // Declare empty list of people
+    subs: any[] = [];
+    emps: any[] = [];
+    jobs: any[] = [];
+
+    constructor(private http: Http, private _routeParams: RouteParams) {}
+
+    ngOnInit() {
+      this.getSubs();
+      this.getAllJob();
+    }
+
+    addJob(datejob) {
+        var body = `date=${datejob}`;
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        this.http
+            .post(`${this.API}/job/addjob`,
+                body, {
+                    headers: headers
+                })
+            .subscribe(data => {
+                alert('Add Job Success');
+                this.getAllJob();
+            }, error => {
+                console.log(JSON.stringify(error.json()));
+            });
+    }
+
+  getSubs() {
+    this.http.get(`${this.API}/subscribe/subs/${this._routeParams.get('id')}`)
+      .map(res => res.json())
+      .subscribe(subs => {
+        this.subs = subs
+        this.getAllEmployee();
+      })
+    }
+  // Get all users from the API
+  getAllEmployee() {
+      this.http.get(`${this.API}/employee/list/technical`)
+          .map(res => res.json())
+          .subscribe(emps => {
+              this.emps = emps
+          })
+  }
+  // Get all users from the API
+  getAllJob() {
+      this.http.get(`${this.API}/job/listjob`)
+          .map(res => res.json())
+          .subscribe(emps => {
+              this.emps = emps
+          })
   }
 }
