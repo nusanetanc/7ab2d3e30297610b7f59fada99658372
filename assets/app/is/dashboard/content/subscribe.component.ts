bbea20e5692e71aa1d7bb6656864b47ef5b24887
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-25 08:48:41 +0700

    package

--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit, OnDestroy} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteParams} from 'angular2/router';
-import { Http } from 'angular2/http';
+import { Http, Headers} from 'angular2/http';
 import 'rxjs/add/operator/map';
 import {Subscription} from "rxjs/Rx";
 import { Sub } from './subs';
