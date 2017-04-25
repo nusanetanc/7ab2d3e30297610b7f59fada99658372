9f301a32edede59e4183e5f72b7c4a081a5413b2
fahmadrifki <fahmadrifki@gmail.com>
2017-04-25 10:33:31 +0700

    update

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -241,6 +241,8 @@ declare let kendo;
                         <!-- /Content List -->
                         
                         <br>
+                            <h3 class="text-center">PRINT INVOICE</h3>
+                        <br>
                         
                         <!-- Content Print -->
                         <div class="print" style="width: 100%;">
@@ -475,7 +477,7 @@ declare let kendo;
                                        <div class="row" style="margin-bottom: 30px;">
                                           <div class="col-sm-12" >
                                              <div class="col-sm-12 text-center" style="padding: 5px;background: linear-gradient(to right, #ed4224 , #f8d143); background: -webkit-linear-gradient(left, #ed4224 , #f8d143); background: -linear-gradient(right, #ed4224 , #f8d143); background: -moz-linear-gradient(right, #ed4224 , #f8d143);">
-                                                <span style="color: #FFF; font-size: 22px; font-weight: 700;">INFORMASI TAGIHAN <i style="opacity: 0.5;"> &nbsp; / &nbsp; BILLING INFORMATION</i></span>
+                                                <span style="color: #FFF; font-size: 22px; font-weight: 700;">INFORMASI PEMBAYARAN <i style="opacity: 0.5;"> &nbsp; / &nbsp; BILLING INFORMATION</i></span>
                                              </div>
                                           </div>
                                        </div>
