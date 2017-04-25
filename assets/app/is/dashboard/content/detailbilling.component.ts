f06eb03fe89d2378d48ba5f5001cf03b70b47ba2
fahmadrifki <fahmadrifki@gmail.com>
2017-04-25 16:03:56 +0700

    update

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -29,13 +29,13 @@ declare let kendo;
                                 <a [routerLink]="['AllBill']" class="btn btn-default billInfoBack" type="button">
                                     BACK
                                 </a>
-                                <button (click)="onItemClicked2(Pack)" onclick="printPenagihan()" class="btn btn-default buttonOrange" type="button" style="float:right;" *ngIf="clickedItem.name == 'regArea'">
+                                <button class="btn btn-default buttonOrange" type="button" style="float:right;" >
                                     Print
                                 </button>
-                                <button (click)="onItemClicked3(Inst)" onclick="printPembayaran()" class="btn btn-default buttonOrange" type="button" style="float:right;" *ngIf="clickedItem.name == 'regPack'">
+                                <button onclick="printPenagihan()" class="btn btn-default buttonOrange" type="button" style="float:right;" >
                                     Print Penagihan
                                 </button>
-                                <button class="btn btn-default buttonOrange" type="button" style="float:right;" *ngIf="clickedItem.name == 'regInst'">
+                                <button onclick="printPembayaran()" class="btn btn-default buttonOrange" type="button" style="float:right;" >
                                     Print Pembayaran
                                 </button>
                             </div>
