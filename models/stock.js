c0a9c14b27510ad37d7269af682344087b253ec8
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-26 14:39:39 +0700

    package

--- models/stock.js
+++ models/stock.js
@@ -4,7 +4,7 @@ var Sub = require('../models/subs');
 var Goods = require('../models/goods');
 
 var StockSchema   = new Schema({
-  goods: {type: Schema.Types.ObjectId, ref: 'Goods'},
+  goods: String,
   barcode: String,
   status: String,
   subs: {type: Schema.Types.ObjectId, ref: 'Sub'}
