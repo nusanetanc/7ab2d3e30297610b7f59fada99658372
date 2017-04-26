9edca327701ccc417db665cae2e5206ed0a5d664
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-26 09:42:06 +0700

    package

--- assets/app/is/dashboard/content/addinformation.component.ts
+++ assets/app/is/dashboard/content/addinformation.component.ts
@@ -113,6 +113,7 @@ export class ContentAddInformationComponent implements OnInit {
     }
 
     onSelectProperty(_id) {
+      this.toInfo = _id;
         this.clusters = this.getAllClusterByProperty(){
             this.http.get(`${this.API}/cluster/clusterbyproperty/${_id}`)
                 .map(res => res.json())
