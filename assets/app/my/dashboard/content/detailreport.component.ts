b39f1d1337dffaba38f7db74f06b81b5a466cffd
ferdianrb <frizki7@gmail.com>
2017-04-26 12:03:19 +0700

    signup

--- assets/app/my/dashboard/content/detailreport.component.ts
+++ assets/app/my/dashboard/content/detailreport.component.ts
@@ -1,5 +1,5 @@
-import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {Component, OnInit, OnDestroy} from 'angular2/core';
+import {ROUTER_DIRECTIVES, RouteParam} from 'angular2/router';
 import { Http } from 'angular2/http';
 import 'rxjs/add/operator/map';
 import { Complaint } from './complaints';
@@ -52,12 +52,11 @@ import { Complaint } from './complaints';
 export class ContentDetailReportComponent {
 // Link to our api, pointing to localhost
   API = 'http://202.162.207.164:3000';
-  Report_ID = '58c24c1948bfc66fd001dbb8';
 
 complaints: any[] = [];
 chatcomplaints: any[] = [];
 
-  constructor(private http: Http) {}
+  constructor(private http: Http, private _routeParams: RouteParams) {}
 
   ngOnInit() {
     this.getDetailReport();
@@ -65,14 +64,14 @@ chatcomplaints: any[] = [];
   }
 
 getDetailReport() {
-  this.http.get(`${this.API}/complaint/complaint/${this.Report_ID}`)
+  this.http.get(`${this.API}/complaint/complaint/${this._routeParams.get('id')}`)
     .map(res => res.json())
     .subscribe(complaints => {
       this.complaints = complaints
     })
 }
 getChatReport() {
-  this.http.get(`${this.API}/chatcomplaint/chat/${this.Report_ID}`)
+  this.http.get(`${this.API}/chatcomplaint/chat/${this._routeParams.get('id')}`)
     .map(res => res.json())
     .subscribe(chatcomplaints => {
       this.chatcomplaints = complaints
