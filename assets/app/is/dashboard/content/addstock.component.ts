4221f976c36bd55f3397f0a13c3bc44a50fb6eac
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-27 08:16:42 +0700

    package

--- assets/app/is/dashboard/content/addstock.component.ts
+++ assets/app/is/dashboard/content/addstock.component.ts
@@ -18,23 +18,6 @@ import { Http } from 'angular2/http';
         </div>
 
         <div class="page-content inset" data-spy="scroll" data-target="#spy">
-        <div class="row marginB20 marginR0">
-            <div class="col-sm-12">
-               <a [routerLink]="['AddStock']" class="btn btn-default buttonOrange">
-                    ADD NEW STOCK
-                </a>
-                <a href="" class="glyphicon glyphicon-chevron-down sort-down"></a>
-                <div class="dropdown right">
-                    <a class="btn btn-default dropdown-toggle buttonSort" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                        DATE
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                        <li><a href="#">NAME</a></li>
-                        <li><a href="#">ID</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
             <div class="row">
                 <div class="col-sm-12" *ngFor="#good of goods">
                     <a [routerLink]="['InfoStock', {id: good._id}]">
