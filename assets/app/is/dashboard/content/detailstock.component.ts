8e151714c2eef6d1d7b1d3a0f7ffb5ecb251eebe
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-27 10:57:14 +0700

    package

--- assets/app/is/dashboard/content/detailstock.component.ts
+++ assets/app/is/dashboard/content/detailstock.component.ts
@@ -1,6 +1,9 @@
-import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {Component, OnInit, OnDestroy} from 'angular2/core';
+import {ROUTER_DIRECTIVES, RouteParams} from 'angular2/router';
 import { Http } from 'angular2/http';
+import 'rxjs/add/operator/map';
+import {Subscription} from "rxjs/Rx";
+
 
 
 @Component({
