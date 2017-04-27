88cfd7329364bbb10deebce674bbae9fe64cd2f2
ferdianrb <frizki7@gmail.com>
2017-04-27 10:54:28 +0700

    report

--- assets/app/my/dashboard/content/detailreport.component.ts
+++ assets/app/my/dashboard/content/detailreport.component.ts
@@ -34,7 +34,7 @@ import { Complaint } from './complaints';
                   </div>
                   <div>
                       <div class="row">
-                          <div class="col-sm-12 invoiceId"><span>Posted <b class="grey333">{{ complaints.dateopen }}</b> by <b class="grey333">{{ complaints.subname }}</b></span></div>
+                          <div class="col-sm-12 invoiceId"><span>Posted <b class="grey333">{{ complaints.dateopen }}</b> by <b class="grey333">aaa</b></span></div>
                       </div>
                       <div class="row">
                           <div class="col-sm-11 infoDetail">
