9a8ac904f9124fe51cd69671e532e454768a110b
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-27 10:55:08 +0700

    package

--- assets/app/is/dashboard/content/detailstock.component.ts
+++ assets/app/is/dashboard/content/detailstock.component.ts
@@ -28,11 +28,11 @@ import { Http } from 'angular2/http';
             <div class="row">
                 <div class="col-sm-12">
                     <div class="row headerList paddingLR30">
-                        <div class="col-sm-12 paddingT20 paddingL35 headerSubList"><strong>Bullet M5 (120 pcs)</strong></div>
+                        <div class="col-sm-12 paddingT20 paddingL35 headerSubList"><strong>{{stocks.goodsname}}</strong></div>
                     </div>
                     <div class="row subInfo">
-                        <div class="col-sm-11 invoiceId"><span>1109482746</span></div>
-                        <div class="col-sm-1 invoiceList"><span class="red">In Use</span></div>
+                        <div class="col-sm-11 invoiceId"><span>{{stocks.barcode}}</span></div>
+                        <div class="col-sm-1 invoiceList"><span class="red">{{stocks.status}}</span></div>
                     </div>
                     <div class="row subInfo">
                         <div class="col-sm-12 invoiceId roboto">
@@ -49,7 +49,7 @@ import { Http } from 'angular2/http';
                                     <span>:</span>
                                 </div>
                                 <div class="col-xs-12 col-md-7">
-                                    <span>Erwando</span>
+                                    <span>{{stocks.name}}</span>
                                 </div>
                             </div>
                             <div class="row marginTB10 marginL5">
@@ -60,40 +60,7 @@ import { Http } from 'angular2/http';
                                     <span>:</span>
                                 </div>
                                 <div class="col-xs-12 col-md-7">
-                                    <span>GR-231242</span>
-                                </div>
-                            </div>
-                            <div class="row marginTB10 marginL5">
-                                <div class="col-xs-6 col-sm-4">
-                                    <span>Groovy ID</span>
-                                </div>
-                                <div class="col-xs-6 col-sm-1">
-                                    <span>:</span>
-                                </div>
-                                <div class="col-xs-12 col-md-7">
-                                    <span>234234234</span>
-                                </div>
-                            </div>
-                            <div class="row marginTB10 marginL5">
-                                <div class="col-xs-6 col-sm-4">
-                                    <span>Email</span>
-                                </div>
-                                <div class="col-xs-6 col-sm-1">
-                                    <span>:</span>
-                                </div>
-                                <div class="col-xs-12 col-md-7">
-                                    <span>johndoe@example.com</span>
-                                </div>
-                            </div>
-                            <div class="row marginTB10 marginL5">
-                                <div class="col-xs-6 col-sm-4">
-                                    <span>Handphone</span>
-                                </div>
-                                <div class="col-xs-6 col-sm-1">
-                                    <span>:</span>
-                                </div>
-                                <div class="col-xs-12 col-md-7">
-                                    <span>+62 812 1234 2222</span>
+                                    <span>{{stocks.subid}}</span>
                                 </div>
                             </div>
                             <div class="row marginTB10 marginL5">
@@ -104,7 +71,7 @@ import { Http } from 'angular2/http';
                                     <span>:</span>
                                 </div>
                                 <div class="col-xs-12 col-md-7">
-                                    <span>Jln. Media No. 14,<br>Komplek Indah, Bandung</span>
+                                    <span>{{stocks.address}} No.{{stocks.nohome}} ,<br>{{stocks.cluster}}, {{stocks.city}}</span>
                                 </div>
                             </div>
                         </div>
@@ -117,5 +84,23 @@ import { Http } from 'angular2/http';
     directives: [ROUTER_DIRECTIVES],
 })
 export class ContentDetailStockComponent {
+  // Link to our api, pointing to localhost
+    API = 'http://202.162.207.164:3000';
 
+    stocks: any[] = [];
+
+    constructor(private http: Http, private _routeParams: RouteParams) {}
+
+    ngOnInit() {
+      this.getAllStocks();
+    }
+
+  // Get all users from the API
+  getAllStocks() {
+    this.http.get(`${this.API}/stock/detail/${this._routeParams.get('id')}`)
+      .map(res => res.json())
+      .subscribe(stocks => {
+        this.stocks = stocks
+      })
+  }
 }
