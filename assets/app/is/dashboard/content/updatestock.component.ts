21f1e55719997774f7ccc758b7c0b97d03d3e359
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-28 10:43:05 +0700

    package

--- assets/app/is/dashboard/content/updatestock.component.ts
+++ assets/app/is/dashboard/content/updatestock.component.ts
@@ -83,7 +83,7 @@ export class ContentUpdateStocksComponent implements OnInit {
         })
     }
     UpdateStock(inputSub, idgoods, idbarcode) {
-
+alert('Tes1');
         var body = `goods=${inputSub}&goods=${inputSub}&goods=${inputSub}`;
         var headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded');
