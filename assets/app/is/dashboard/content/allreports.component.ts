f95bd2876cfab9ef4ea9223e3c5068c1536c2c5a
fahmadrifki <fahmadrifki@gmail.com>
2017-05-02 16:29:55 +0700

    update

--- assets/app/is/dashboard/content/allreports.component.ts
+++ assets/app/is/dashboard/content/allreports.component.ts
@@ -25,15 +25,39 @@ import {Report} from './allreports';
                     <div class="dropdown right">
                       <button class="buttonDrop buttonSort">REPORT &nbsp; <span href="" class="glyphicon glyphicon-chevron-down"></span></button>
                       <div class="dropdown-content">
-                        <a href="#">OPEN</a>
-                        <a href="#">SOLVED</a>
-                        <a href="#">DONE</a>
+                        <a href="">OPEN</a>
+                        <a (click)="onItemClicked1(Area)">SOLVED</a>
+                        <a (click)="onItemClicked2(Pack)">DONE</a>
                       </div>
                     </div>
                 </div>
             </div>
-            <div class="row">
+            <div class="row" *ngIf="clickedItem.name == 'regArea'">
                 <div class="col-sm-12">
+                    <div class="row subInfo fontWeight300" *ngFor="#complaint of complaints">
+                        <a class="grey333" [routerLink]="['ReplyReport', {id: complaint.complaintId}]">
+                            <div class="col-sm-3 invoiceId"><span>{{complaint.dateopen}}</span></div>
+                            <div class="col-sm-7 invoiceList"><span>{{complaint.subcategory}}</span></div>
+                            <div class="col-sm-2 invoiceList"><span class="red">{{complaint.status}}</span></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row" *ngIf="clickedItem.name == 'regPack'">
+                <div class="col-sm-12">
+                    SOLVED
+                    <div class="row subInfo fontWeight300" *ngFor="#complaint of complaints">
+                        <a class="grey333" [routerLink]="['ReplyReport', {id: complaint.complaintId}]">
+                            <div class="col-sm-3 invoiceId"><span>{{complaint.dateopen}}</span></div>
+                            <div class="col-sm-7 invoiceList"><span>{{complaint.subcategory}}</span></div>
+                            <div class="col-sm-2 invoiceList"><span class="red">{{complaint.status}}</span></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row" *ngIf="clickedItem.name == 'regInst'">
+                <div class="col-sm-12">
+                    DONE
                     <div class="row subInfo fontWeight300" *ngFor="#complaint of complaints">
                         <a class="grey333" [routerLink]="['ReplyReport', {id: complaint.complaintId}]">
                             <div class="col-sm-3 invoiceId"><span>{{complaint.dateopen}}</span></div>
@@ -49,6 +73,17 @@ import {Report} from './allreports';
     directives: [ROUTER_DIRECTIVES],
 })
 export class ContentAllReportsComponent {
+
+    public clickedItem = {name: "regArea"};
+
+    onItemClicked1(Area) {
+        this.clickedItem = {name: "regPack"};
+    }
+
+    onItemClicked2(Pack) {
+        this.clickedItem = {name: "regInst"};
+    }
+
     // Link to our api, pointing to localhost
     API = 'http://202.162.207.164:3000';
 
