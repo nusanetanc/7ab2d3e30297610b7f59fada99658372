0344b67eb0f727a21e3c2108a09232481fd11e9f
ferdianrb <frizki7@gmail.com>
2017-05-02 14:30:12 +0700

    report

--- assets/app/my/dashboard/content/report.component.ts
+++ assets/app/my/dashboard/content/report.component.ts
@@ -38,10 +38,10 @@ import { Complaint } from './complaints';
             </div>
             <div class="row">
                 <div class="col-sm-12" *ngFor="#complaint of complaints">
-                <a [routerLink]="['Detailreport', {id: complaint.complaintId}]">
+                <a class="grey333" [routerLink]="['Detailreport', {id: complaint.complaintId}]">
                     <div class="row subInfo">
-                        <div class="col-sm-3 invoiceId"><span><a class="grey333">{{ complaint.dateopen }}</a></span></div>
-                        <div class="col-sm-7 invoiceList"><span><a class="grey333">{{ complaint.subcategory }} </a></span></div>
+                        <div class="col-sm-3 invoiceId"><span>{{ complaint.dateopen }}</span></div>
+                        <div class="col-sm-7 invoiceList"><span>{{ complaint.subcategory }}</span></div>
                         <div class="col-sm-2 invoiceList"><span class="red">{{ complaint.status }}</span></div>
                     </div>
                 </a>
