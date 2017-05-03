a812101a10249c10326f9e36085eead3768c9753
fahmadrifki <fahmadrifki@gmail.com>
2017-05-03 10:22:59 +0700

    update

--- assets/app/is/dashboard/dashboard.component.ts
+++ assets/app/is/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+
 import {Component, OnInit, OnDestroy} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {FORM_PROVIDERS, FORM_DIRECTIVES, Control} from 'angular2/common';
