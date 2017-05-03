7dbbbb66f64bb1502faff5472ea75fbdc8548c5f
ferdianrb <frizki7@gmail.com>
2017-05-03 11:30:17 +0700

    report

--- routes/chatcomplaint.js
+++ routes/chatcomplaint.js
@@ -13,7 +13,7 @@ router.get('/listchat', function(req, res, next) {
 });
 
 
-outer.get('/chat/:complaint', function(req, res, next) {
+router.get('/chat/:complaint', function(req, res, next) {
     Chat.findOne({complaintId: req.params.complaint}, function(err, chats) {
         Sub.findById(chats.sub, function(err, subs) {
             res.json({
