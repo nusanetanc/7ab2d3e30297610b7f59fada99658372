2c0eff670cc4ba57c33b312fc1456684edb18a98
fahmadrifki <fahmadrifki@gmail.com>
2017-05-03 08:09:52 +0700

    update

--- assets/app/is/dashboard/content/allstock.component.ts
+++ assets/app/is/dashboard/content/allstock.component.ts
@@ -26,10 +26,10 @@ import { Http } from 'angular2/http';
                 <a href="" class="glyphicon glyphicon-chevron-down sort-down"></a>
                 <div class="dropdown right">
                     <button (click)="sortByName()" class="btn btn-default dropdown-toggle buttonSort" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                        DATE
+                        NAME
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                        <li><a href="#">NAME</a></li>
+                        <li><a href="#">DATE</a></li>
                         <li><a href="#">ID</a></li>
                     </ul>
                 </div>
