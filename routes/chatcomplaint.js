df2d192490de794824d7276e6651e57d0fba2c4b
ferdianrb <frizki7@gmail.com>
2017-05-03 14:07:46 +0700

    report

--- routes/chatcomplaint.js
+++ routes/chatcomplaint.js
@@ -15,14 +15,8 @@ router.get('/listchat', function(req, res, next) {
 
 router.get('/chat/:complaint', function(req, res, next) {
     Chat.find({complaintId: req.params.complaint}, function(err, chats) {
-        Sub.findOne({_id: chats.sub}, function(err, subs) {
-            res.json({
-                _id: chats._id,
-                message: chats.message,
-                date: chats.date,
-                subname: subs.name,
-                complaintId: chats.complaintId,
-            });
+        Sub.findById(chats.sub, function(err, subs) {
+            res.json({chats, subname: subs.name});
         });
     });
 });
