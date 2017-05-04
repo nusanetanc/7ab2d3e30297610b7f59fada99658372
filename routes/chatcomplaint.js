8dad2cc09cf3e15d766667d88460f22cf720faca
ferdianrb <frizki7@gmail.com>
2017-05-04 10:10:13 +0700

    report

--- routes/chatcomplaint.js
+++ routes/chatcomplaint.js
@@ -15,7 +15,7 @@ router.get('/listchat', function(req, res, next) {
 
 
 router.get('/chat/:complaint', function(req, res, next) {
-    Chat.find({$or [{complaintId: req.params.complaint}, {sub: chats.sub}, {emp: chats.emp} ]}, function(err, chats) {
+    Chat.find({$or: [{complaintId: req.params.complaint}, {sub: chats.sub}, {emp: chats.emp} ]}, function(err, chats) {
         Sub.findById(chats.sub, function(err, sub) {
         Emp.findById(chats.emp, function(err, emp) {
             res.json({
