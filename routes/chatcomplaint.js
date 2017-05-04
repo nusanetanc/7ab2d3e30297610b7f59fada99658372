92764911fb0a87f9aae32772fff4033a043edf54
ferdianrb <frizki7@gmail.com>
2017-05-04 11:38:32 +0700

    report

--- routes/chatcomplaint.js
+++ routes/chatcomplaint.js
@@ -15,12 +15,14 @@ router.get('/listchat', function(req, res, next) {
 
 
 router.get('/chat/:complaint', function(req, res, next) {
-    Chat.find({complaintId: req.params.complaint}, function(err, chats) {
+    Chat.findOne({complaintId: req.params.complaint}, function(err, chats) {
+        Emp.findById(chats.emp, function(err, emps) {
             res.json({
                 message: chats.message,
                 date: chats.date,
                 complaintId: chats.complaintId,
             });
+        });
     });
 });
 
