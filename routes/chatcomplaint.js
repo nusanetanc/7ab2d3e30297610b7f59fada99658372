2bd63c84538e4328ef99cc4d2278ae3de9ce7aec
ferdianrb <frizki7@gmail.com>
2017-05-04 09:58:07 +0700

    report

--- routes/chatcomplaint.js
+++ routes/chatcomplaint.js
@@ -22,8 +22,7 @@ router.get('/chat/:complaint', function(req, res, next) {
                 message: chats.message,
                 date: chats.date,
                 complaintId: chats.complaintId,
-                sub: sub.name,
-                emp: emp.name,
+                name: sub.name && emp.name,
             });
         });
         });
