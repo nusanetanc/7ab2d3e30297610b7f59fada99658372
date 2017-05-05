703cfe14b0dec32128c20164772003ac3b95b320
ferdianrb <frizki7@gmail.com>
2017-05-05 09:46:36 +0700

    report

--- assets/app/my/dashboard/content/detailreport.component.ts
+++ assets/app/my/dashboard/content/detailreport.component.ts
@@ -32,9 +32,9 @@ import { Complaint } from './complaints';
                       <div class="col-sm-10 invoiceId"><span><b class="grey333">{{ complaints.subcategory }}</b><br>{{ complaints.category }}</span></div>
                       <div class="col-sm-2 invoiceList"><span class="grey333">Status : <span class="red">{{ complaints.status }}</span></span></div>
                   </div>
-                  <div>
+                  <div *ngFor="#chat of chats">
                       <div class="row">
-                          <div class="col-sm-12 invoiceId"><span>Posted <b class="grey333">{{ chats.date }}</b> by <b class="grey333">{{ chats.subname }}{{ chats.empname }}</b></span></div>
+                          <div class="col-sm-12 invoiceId"><span>Posted <b class="grey333">{{ chat.date }}</b> by <b class="grey333">{{ chat.subname }}{{ chat.empname }}</b></span></div>
                       </div>
                       <div class="row">
                           <div class="col-sm-11 infoDetail">
