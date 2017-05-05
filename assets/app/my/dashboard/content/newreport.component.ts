6ccdf4cf4351de92bf3172c2314d800a110bda95
ferdianrb <frizki7@gmail.com>
2017-05-05 09:07:31 +0700

    report

--- assets/app/my/dashboard/content/newreport.component.ts
+++ assets/app/my/dashboard/content/newreport.component.ts
@@ -86,8 +86,8 @@ export class ContentNewReportComponent implements OnInit {
     today : Date = new Date();
     // Add one person to the API
     addReport(category, subcategory, subs, date, message) {
-        var body = `category=${category}&subcategory=${subcategory}&sub=${subs._id}&dateopen=${date}`;
-        var body2 = `sub=${subs._id}&date=${date}&message=${message}&subname${subs.name}`;
+        var body = `category=${category}&subcategory=${subcategory}&sub=${subs}&dateopen=${date}`;
+        var body2 = `sub=${subs}&date=${date}&message=${message}&subname${subs}`;
         var headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded');
         this.http
