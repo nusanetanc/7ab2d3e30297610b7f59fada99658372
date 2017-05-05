4228afc21e2f61d9e9e2b4430cc3527ff464f387
fahmadrifki <fahmadrifki@gmail.com>
2017-05-05 08:30:47 +0700

    update

--- assets/app/is/dashboard/dashboard.component.ts
+++ assets/app/is/dashboard/dashboard.component.ts
@@ -72,9 +72,9 @@ import {Menu} from "./menu";
                     </div>
                     <div class="row sidebar-footer" style="margin-right: 0px; bottom: 0px; position: absolute;">
                         <div class="col-sm-12">
-                            <div class="row" style="border-top: 0.3px solid #B3B3B3;margin-right:-15px;">
+                            <div class="row" style="border-top: 0.3px solid #B3B3B3;margin-right:15px;">
                                 <div class="col-sm-6 col-xs-6" style="padding: 15px 25px;">
-                                    <img src="groovy-grayscale.png" alt="ava" style="width: 100%;">
+                                    <img src="images/groovy-grayscale.png" alt="ava" style="width: 100%;">
                                 </div>
                                 <div class="col-sm-6 col-xs-6" style="padding: 15px 25px 15px 0px;text-decoration: none;">
                                     <div class="row">
