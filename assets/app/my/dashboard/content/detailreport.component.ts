262df7b4703d6120cb8b953e5074d2feaa630e2d
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-08 16:36:07 +0700

    update

--- assets/app/my/dashboard/content/detailreport.component.ts
+++ assets/app/my/dashboard/content/detailreport.component.ts
@@ -35,11 +35,7 @@ import { Complaint } from './complaints';
                     <div class="row" *ngFor="#chat of chats">
                         <div class="col-sm-1 col-xs-12"><img class="ava" src="./images/ava.png" alt="ava"></div>
                         <div class="col-sm-11 col-xs-12 postBy marginT10"><span>Posted <b class="grey333">{{ chat.date }}</b> by <b class="grey333">Tes Admin</b><br><br>{{ chat.message }}</span></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-10 col-sm-offset-1">
-                            <hr  />
-                        </div>
+                        <hr  />
                     </div>
                     <div class="row">
                         <div class="col-sm-1 col-xs-12"><img class="ava marginB10" src="./images/ava.png" alt="ava"></div>
