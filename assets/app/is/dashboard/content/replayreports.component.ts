746fa59f73c4ca63bb51628064351fe4f7752030
fahmadrifki <fahmadrifki@gmail.com>
2017-05-08 17:09:13 +0700

    update

--- assets/app/is/dashboard/content/replayreports.component.ts
+++ assets/app/is/dashboard/content/replayreports.component.ts
@@ -35,7 +35,7 @@ import { Complaint } from './complaints';
                     <div class="row" *ngFor="#chat of chats">
                         <div class="col-sm-1 col-xs-12"><img class="ava" src="./images/ava.png" alt="ava"></div>
                         <div class="col-sm-11 col-xs-12 postBy marginT10"><span>Posted <b class="grey333">{{ chat.date }}</b> by <b class="grey333">Tes Admin</b><br>{{ chat.message }}</span></div>
-                        <div class="col-sm-11 col-xs-12 col-sm-offset-1">
+                        <div class="col-sm-10 col-xs-12 col-sm-offset-1">
                             <hr class="hr-report">
                         </div>
                     </div>
