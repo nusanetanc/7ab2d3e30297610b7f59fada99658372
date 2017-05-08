3f24dc792954d50324ffff4af5369250e7983d57
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-08 11:47:49 +0700

    update

--- assets/app/my/dashboard/content/detailreport.component.ts
+++ assets/app/my/dashboard/content/detailreport.component.ts
@@ -29,12 +29,12 @@ import { Complaint } from './complaints';
             <div class="row subInfo roboto grey8b reportDetail">
                 <div class="col-sm-12">
                     <div class="row marginB20">
-                        <div class="col-sm-10 "><span><b class="grey333">Account can't login in other devices</b></span></div>
-                        <div class="col-sm-2 "><span class="grey333">Status : <span class="red">On progress</span></span></div>
+                        <div class="col-sm-10 "><span><b class="grey333">{{ complaints.subcategory }}</b></span></div>
+                        <div class="col-sm-2 "><span class="grey333">Status : <span class="red">{{ complaints.status }}</span></span></div>
                     </div>
-                    <div class="row">
-                        <div class="col-sm-1 col-xs-12"><img class="ava" src="ava.png" alt="ava"></div>
-                        <div class="col-sm-11 col-xs-12 postBy marginT10"><span>Posted <b class="grey333">11 Feb 2017 - 11.00 PM</b> by <b class="grey333">Jhon Doe</b><br>Internet Problem > Unstable Internet Connection</span></div>
+                    <div class="row" *ngFor="#chat of chats">
+                        <div class="col-sm-1 col-xs-12"><img class="ava" src="./images/ava.png" alt="ava"></div>
+                        <div class="col-sm-11 col-xs-12 postBy marginT10"><span>Posted <b class="grey333">{{ chat.date }}</b> by <b class="grey333">Tes Admin</b><br>{{ chat.message }}</span></div>
                     </div>
                     <div class="row">
                         <div class="col-sm-10 col-sm-offset-1">
