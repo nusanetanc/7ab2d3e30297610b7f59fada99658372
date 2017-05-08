5fe8f0a9f844908664eee74b1ae71a688d02a429
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-08 16:23:37 +0700

    update

--- assets/app/is/dashboard/content/replayreports.component.ts
+++ assets/app/is/dashboard/content/replayreports.component.ts
@@ -61,7 +61,7 @@ import { Complaint } from './complaints';
     `,
     directives: [ROUTER_DIRECTIVES],
 })
-export class ContentDetailReportComponent implements OnInit {
+export class ContentReplayReportComponent implements OnInit {
 // Link to our api, pointing to localhost
   API = 'http://202.162.207.164:3000';
 
