bc4f1656658b2ff7ebd4b10d6633a9c50b0471a0
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-08 11:36:11 +0700

    update

--- assets/app/my/dashboard/content/detailreport.component.ts
+++ assets/app/my/dashboard/content/detailreport.component.ts
@@ -26,24 +26,40 @@ import { Complaint } from './complaints';
                       </a>
                 </div>
             </div>
-            <div class="row subInfo roboto grey8b">
+            <div class="row subInfo roboto grey8b reportDetail">
                 <div class="col-sm-12">
-                  <div class="row">
-                      <div class="col-sm-10 invoiceId"><span><b class="grey333">{{ complaints.subcategory }}</b><br>{{ complaints.category }}</span></div>
-                      <div class="col-sm-2 invoiceList"><span class="grey333">Status : <span class="red">{{ complaints.status }}</span></span></div>
-                  </div>
-                  <div *ngFor="#chat of chats">
-                      <div class="row">
-                          <div class="col-sm-12 invoiceId"><span>Posted <b class="grey333">{{ chat.date }}</b> by <b class="grey333">{{ chat.subname || chat.empname }}</b></span></div>
-                      </div>
-                      <div class="row">
-                          <div class="col-sm-11 infoDetail">
-                              <span>{{ chat.message }}</span>
-                          </div>
-                      </div>
+                    <div class="row marginB20">
+                        <div class="col-sm-10 "><span><b class="grey333">Account can't login in other devices</b></span></div>
+                        <div class="col-sm-2 "><span class="grey333">Status : <span class="red">On progress</span></span></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-1 col-xs-12"><img class="ava" src="ava.png" alt="ava"></div>
+                        <div class="col-sm-11 col-xs-12 postBy marginT10"><span>Posted <b class="grey333">11 Feb 2017 - 11.00 PM</b> by <b class="grey333">Jhon Doe</b><br>Internet Problem > Unstable Internet Connection</span></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-10 col-sm-offset-1">
+                            <hr  />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-1 col-xs-12"><img class="ava marginB10" src="ava.png" alt="ava"></div>
+                         <div class="col-sm-10 col-xs-12">
+                            <textarea id="message" class="input width100" name="message" rows="10" placeholder="*Type message here"></textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-10 col-sm-offset-1 marginB20">
+                            <button class="btn btn-default buttonOrange">
+                                SEND
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+
+</div>
         </div>
     </div>
     `,
