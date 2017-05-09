946ecb24665e28fab9dcb56fda9c5907467af113
ferdianrb <frizki7@gmail.com>
2017-05-09 14:53:42 +0700

    information

--- assets/app/login-is/login.component.ts
+++ assets/app/login-is/login.component.ts
@@ -13,7 +13,7 @@ import { Sub } from './subs';
                 <div class="form">
                     <center><img src="images/logo-groovy.png" alt="logo-groovy" width="30%"/></center>
                     <div class="form-group">
-                        <h3 class="text-center" style="color: #FFFFFF; font-family: 'Open Sans', sans-serif; font-weight: 700;">INFORMATION SYSTEM</h3><br>
+                        <h3 class="text-center" style="color: #FFFFFF; font-family: 'Open Sans', sans-serif; font-weight: 700;">INFORMATION sSYSTEM</h3><br>
                         <input type="text" class="form-control" id="signEmail" placeholder="Email">
                         <input type="password" class="form-control" id="signPassword" placeholder="Password">
                     </div>
