24739ed750dc4a39a176e16878b8e8bd3ea9508c
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-09 16:43:34 +0700

    update

--- assets/app/is/dashboard/content/subs.component.ts
+++ assets/app/is/dashboard/content/subs.component.ts
@@ -7,7 +7,7 @@ import { Sub } from './subs';
 @Component({
     selector: 'form-subs',
     template: `
-      <span>{{idsubs}}</span>
+      <span>{{subs.name}}</span>
     `,
     directives: [ROUTER_DIRECTIVES],
 })
@@ -21,7 +21,7 @@ subs: any[] = [];
   constructor(private http: Http) {}
   // Get all Subs from the API
       getSubs() {
-          this.http.get(`${this.API}/subscribe/subs/58b3cdac45912d052e2c85a5`)
+          this.http.get(`${this.API}/subscribe/subs/${this.idsubs}`)
               .map(res => res.json())
               .subscribe(subs => {
                   this.subs = subs
