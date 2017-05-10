080d90c3ecc442f8bb132407f27530e8279b4575
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-10 09:28:07 +0700

    update

--- assets/app/my/dashboard/content/detailreport.component.ts
+++ assets/app/my/dashboard/content/detailreport.component.ts
@@ -8,6 +8,7 @@ import {ContentEmpsNameComponent} from './empsname.component';
 import {AccordionModule} from 'primeng/primeng';
 import {MenuItem} from 'primeng/primeng';
 import {Message} from 'primeng/primeng';
+import {GrowlModule} from 'primeng/primeng';
 
 @Component({
     selector: 'form-dashboard',
@@ -64,22 +65,17 @@ import {Message} from 'primeng/primeng';
             </div>
         </div>
     </div>
-    <p-growl [value]="msgs" sticky="sticky"></p-growl>
-    <div>
-        <button type="button" pButton (click)="showSuccess()" label="Success" class="ui-button-success"></button>
-        <button type="button" pButton (click)="showInfo()" label="Info" class="ui-button-info"></button>
-        <button type="button" pButton (click)="showWarn()" label="Warn" class="ui-button-warning"></button>
-        <button type="button" pButton (click)="showError()" label="Error" class="ui-button-error"></button>
-        <button type="button" pButton (click)="showMultiple()" label="Multiple"></button>
-        <button type="button" pButton (click)="clear()" icon="fa-close" style="float:right" label="Clear"></button>
-    </div>
+    <p-growl [value]="msgs"></p-growl>
+
+    <button type="button" (click)="show()">Show</button>
+    <button type="button" (click)="clear()">Hide</button>
     `,
     directives: [ContentSubsNameComponent, ContentEmpsNameComponent, ROUTER_DIRECTIVES],
 })
 export class ContentDetailReportComponent implements OnInit {
 // Link to our api, pointing to localhost
   API = 'http://202.162.207.164:3000';
-
+msgs: Message[] = [];
 complaints: any[] = [];
 chats: any[] = [];
 
@@ -119,33 +115,11 @@ addReport(message) {
             console.log(JSON.stringify(error.json()));
         });
 }
+show() {
+    this.msgs.push({severity:'info', summary:'Info Message', detail:'PrimeNG rocks'});
+}
 
-
-msgs: Message[] = [];
-
-    showInfo() {
-        this.msgs = [];
-        this.msgs.push({severity:'info', summary:'Info Message', detail:'PrimeNG rocks'});
-    }
-
-    showWarn() {
-        this.msgs = [];
-        this.msgs.push({severity:'warn', summary:'Warn Message', detail:'There are unsaved changes'});
-    }
-
-    showError() {
-        this.msgs = [];
-        this.msgs.push({severity:'error', summary:'Error Message', detail:'Validation failed'});
-    }
-
-    showMultiple() {
-        this.msgs = [];
-        this.msgs.push({severity:'info', summary:'Message 1', detail:'PrimeNG rocks'});
-        this.msgs.push({severity:'info', summary:'Message 2', detail:'PrimeUI rocks'});
-        this.msgs.push({severity:'info', summary:'Message 3', detail:'PrimeFaces rocks'});
-    }
-
-    clear() {
-        this.msgs = [];
-    }
+hide() {
+    this.msgs = [];
+}
 }
