45443f21ee4ca4d5195d5e959dacf7e3111f715e
ferdianrb <frizki7@gmail.com>
2017-05-10 09:50:54 +0700

    count subscriber

--- assets/app/is/dashboard/content/dashboard.component.ts
+++ assets/app/is/dashboard/content/dashboard.component.ts
@@ -23,8 +23,8 @@ import {Report} from './allreports';
                               <div class="col-sm-4">
                                   <div class="cardDashboardSub">
                                       <div class="row">
-                                          <div class="col-sm-12" *ngFor="#sub of subs">
-                                              <p class="text-center font70MarginT35"><b>{{sub.length}}</b></p>
+                                          <div class="col-sm-12">
+                                              <p class="text-center font70MarginT35" *ngFor="#sub of subs"><b>{{sub.length}}</b></p>
                                               <h4 class="text-center fontWeight300">SUBSCRIBERS</h4>
                                           </div>
                                       </div>
