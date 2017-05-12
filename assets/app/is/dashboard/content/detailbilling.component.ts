5563a919f904e06d1e196a62e33a86facabac7c9
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-12 14:56:25 +0700

    update

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -162,7 +162,7 @@ declare let kendo;
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div *ngIf="bills.pricestb == '0' || bills.pricestb == '' || bills.pricestb == null" class="row">
+                                                <div *ngIf="bills.pricestb != '0' || bills.pricestb != '' || bills.pricestb != null" class="row">
                                                     <div class="col-sm-12 listWhite">
                                                         <div class="col-sm-10">
                                                             <span>STB Rent</span>
