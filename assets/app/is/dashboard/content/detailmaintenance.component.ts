56b59b37796f0beb9eb13fe7c2fad7bbbebc4268
fahmadrifki <fahmadrifki@gmail.com>
2017-05-12 14:55:49 +0700

    update

--- assets/app/is/dashboard/content/detailmaintenance.component.ts
+++ assets/app/is/dashboard/content/detailmaintenance.component.ts
@@ -90,52 +90,7 @@ import 'rxjs/add/operator/map';
                 <div class="col-sm-12">
                     <div class="row">
                         <div class="col-sm-12">
-                            <h4>BILLING INFORMATION</h4>
-                        </div>
-                    </div>
-    
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="row marginTB10 marginL5">
-                                <div class="col-xs-6 col-sm-4">
-                                    <span>Current Package</span>
-                                </div>
-                                <div class="col-xs-6 col-sm-1">
-                                    <span>:</span>
-                                </div>
-                                <div class="col-xs-12 col-md-7">
-                                    <span>Level {{ subs.packlev }}</span>
-                                </div>
-                            </div>
-                            <div class="row marginTB10 marginL5">
-                                <div class="col-xs-6 col-sm-4">
-                                    <span>Status</span>
-                                </div>
-                                <div class="col-xs-6 col-sm-1">
-                                    <span>:</span>
-                                </div>
-                                <div class="col-xs-12 col-md-7">
-                                    <span class="green">{{ subs.status }}</span>
-                                </div>
-                            </div>
-                            <div class="row marginTB10 marginL5">
-                                <div class="col-xs-6 col-sm-4">
-                                    <span>Virtual Account No.</span>
-                                </div>
-                                <div class="col-xs-6 col-sm-1">
-                                    <span>:</span>
-                                </div>
-                                <div class="col-xs-12 col-md-7">
-                                    <span>{{ subs.nova }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <h4>EDIT STATUS</h4>
+                            <h4>ADD BARANG</h4>
                         </div>
                     </div>
                     <div class="row">
@@ -143,14 +98,22 @@ import 'rxjs/add/operator/map';
                             <div class="formNewReport marginLR20">
                                 <form>
                                     <select #typestatus id="typestatus">
-                                        <option class="option" disabled="true" selected="true" value="0">-- Select Status --</option>
+                                        <option class="option" disabled="true" selected="true" value="0">-- Select Goods Name --</option>
+                                        <option class="option" value="Account Active">Account Active</option>>
+                                        <option class="option" value="Account and Service Active">Account and Service Active</option>
+                                        <option class="option" value="Account Unactive">Account Unactive</option>
+                                    </select><br/><br/>
+                                </form>
+                                <form>
+                                    <select #typestatus id="typestatus">
+                                        <option class="option" disabled="true" selected="true" value="0">-- Select Barcode --</option>
                                         <option class="option" value="Account Active">Account Active</option>>
                                         <option class="option" value="Account and Service Active">Account and Service Active</option>
                                         <option class="option" value="Account Unactive">Account Unactive</option>
                                     </select><br/><br/>
                                 </form>
                                 <button type="submit" (click)="editStatus(typestatus.value)" class="btn btn-default buttonOrange">
-                                    EDIT
+                                    ADD
                                 </button>
                             </div>
                         </div>
