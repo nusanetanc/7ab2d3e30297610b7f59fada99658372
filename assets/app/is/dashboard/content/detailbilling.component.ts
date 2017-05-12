e87d0085a1da5f17c9bebb3ddb38974cd5f026c6
ferdianrb <frizki7@gmail.com>
2017-05-12 10:03:34 +0700

    detail billing

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -148,7 +148,7 @@ declare let kendo;
                                                             <span>Level {{ bills.namepack }} Package</span>
                                                         </div>
                                                         <div class="col-sm-2">
-                                                            <span class="right">{{ bills.pricepack | currency:'RP '}}</span>
+                                                            <span class="right">{{ bills.pricepack | currency:'USD$'}}</span>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -681,13 +681,6 @@ export class ContentDetailBillingComponent implements OnInit {
         .subscribe(bills => {
             this.bills = bills
         })
-    }
-
-    "NUMBER_FORMATS": {
-        "CURRENCY_SYM": "Rp",
-        "DECIMAL_SEP": ",",
-        "GROUP_SEP": "."
-        ...
     }
 
 }
