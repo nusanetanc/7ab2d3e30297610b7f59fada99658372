cc2f542a6aa15fc13af2400511176d0c1a3fd47c
fahmadrifki <fahmadrifki@gmail.com>
2017-05-12 09:06:21 +0700

    update

--- assets/app/login-is/login.component.ts
+++ assets/app/login-is/login.component.ts
@@ -15,7 +15,7 @@ import { Sub } from './subs';
                         <div class="col-sm-5 card-login">
                             <form class="form-horizontal">
                                 <center><img src="images/logo-groovy.png" alt="Logo Groovy" width="40%">
-                                <h5><b class="grey-text">INFORMATION SYSTEM</b></h5></center><br><br>
+                                <h5 class="grey-text">INFORMATION SYSTEM</h5></center><br><br>
                                 <div class="form-group">
                                     <label for="inputEmail3" class="control-label orange-text" style="font-size: 11px; font-weight: 300;">Email</label><br>
                                     <input type="email" class="form-login" id="signEmail" #signEmail placeholder="Type your mail"><br>
@@ -23,7 +23,7 @@ import { Sub } from './subs';
                                 <div class="form-group">
                                     <label for="inputPassword3" class="control-label orange-text" style="font-size: 11px; font-weight: 300;">Password</label><br>
                                     <input type="password" class="form-login" id="signPassword" #signPassword placeholder="Type your password"><br>
-                                </div>
+                                </div><br>
                                 <div class="form-group form-forgot">
                                     <a href="" class="orange-text">I forgot password</a>
                                     <button type="submit" class="btn btn-login" (click)="signEmp(signEmail.value, signPassword.value)">LOGIN</button>
