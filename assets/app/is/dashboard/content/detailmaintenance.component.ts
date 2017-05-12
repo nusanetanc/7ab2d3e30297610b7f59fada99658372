3f1ba550719a609ff12f3ce9ed0b48e8028bfdba
fahmadrifki <fahmadrifki@gmail.com>
2017-05-12 14:21:58 +0700

    update

--- assets/app/is/dashboard/content/detailmaintenance.component.ts
+++ assets/app/is/dashboard/content/detailmaintenance.component.ts
@@ -38,16 +38,16 @@ import 'rxjs/add/operator/map';
                     <div class="row">
                         <div class="col-sm-6">
                             <div class="row marginTB10 marginL5">
-                                <div class="col-xs-6 col-sm-4">
+                                <div class="col-xs-6 col-sm-6">
                                     <span>Subscriber</span>
                                 </div>
-                                <div class="col-xs-6 col-sm-1">
+                                <div class="col-xs-6 col-sm-6">
                                     <span>:</span>
                                 </div>
-                                <div class="col-xs-12 col-md-7">
+                                <div class="col-xs-12 col-md-12">
                                     <span>IBNU ZULKIVLI</span>
                                 </div>
-                                <div class="col-xs-12 col-md-7">
+                                <div class="col-xs-12 col-md-12">
                                     <span>AHMAD CAHYADI</span>
                                 </div>
                             </div>
