b0b02720901adea18c8d3b2ae44d2050d827295b
ferdianrb <frizki7@gmail.com>
2017-05-12 10:25:28 +0700

    detail billing

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -148,7 +148,7 @@ declare let kendo;
                                                             <span>Level {{ bills.namepack }} Package</span>
                                                         </div>
                                                         <div class="col-sm-2">
-                                                            <span class="right">RP.{{ bills.pricepack | currency:'IDR':true:'2.2-4'}}</span>
+                                                            <span class="right">RP.{{ bills.pricepack | currency}}</span>
                                                         </div>
                                                     </div>
                                                 </div>
