ab542515ba37ea275167fbed149fa88ceecd0172
fahmadrifki <fahmadrifki@gmail.com>
2017-05-15 10:43:30 +0700

    update

--- assets/app/is/dashboard/content/profileengineer.component.ts
+++ assets/app/is/dashboard/content/profileengineer.component.ts
@@ -110,12 +110,12 @@ import 'rxjs/add/operator/map';
                          <div class="col-sm-6">
                             <div class="row">
                                <div class="col-sm-12" *ngFor="#job of jobs">
-                               <a [routerLink]="['Detailbilling', {id: bill._id}]">
                                   <div class="row subInfo">
+                                   <a [routerLink]="['Detailbilling', {id: bill._id}]">
                                      <div class="col-sm-8 invoiceList"><span>{{job.name}}</span></div>
                                      <div class="col-sm-4 invoiceList"><span>{{job.status}}</span></div>
+                                   </a>
                                   </div>
-                               </a>
                                </div>
                             </div>
                          </div>
