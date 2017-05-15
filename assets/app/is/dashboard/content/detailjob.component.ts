605da267039bb1ae8627e720af58517f27abc1c0
ferdianrb <frizki7@gmail.com>
2017-05-15 11:28:47 +0700

    jobs

--- assets/app/is/dashboard/content/detailjob.component.ts
+++ assets/app/is/dashboard/content/detailjob.component.ts
@@ -45,7 +45,7 @@ import 'rxjs/add/operator/map';
                                     <span>:</span>
                                 </div>
                                 <div class="col-xs-12 col-md-7">
-                                    <span>sadasd{{ jobs.subname }}</span>
+                                    <span>sadsakd{{ jobs.subname }}</span>
                                 </div>
                             </div>
                             <div class="row marginTB10 marginL5">
