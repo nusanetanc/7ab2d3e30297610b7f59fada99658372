f3cc78ceeaaa85caade6766b68a3d3cb11298160
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-16 08:18:10 +0700

    update

--- assets/app/is/dashboard/content/listpackage.component.ts
+++ assets/app/is/dashboard/content/listpackage.component.ts
@@ -7,11 +7,19 @@ import { Package } from './package';
 @Component({
     selector: 'form-subs',
     template: `
-      <span *ngFor="#package of packages">{{package.level}}</span>
+    <select [(ngModel)]="selectedPackage.level" (change)="onSelectPackage($event.target.value)" #subpacklev id="subpacklev" name="package" class="inputForm">
+        <option value="0">-- Select Package --</option>
+        <option *ngFor="#package of packages" value="{{ package.level }}">Level {{package.level}} - Monthly - {{package.price | currency:'IDR':true}}</option>
+    </select><br/>
     `,
     directives: [ROUTER_DIRECTIVES],
 })
 export class ContentListPackageComponent implements OnInit {
+selectedPackage: Package = new Package(0, 'dummy');
+
+onSelectPackage(level) {
+    console.log(level)
+}
 //@Input() idsubs: string;
 API = 'http://202.162.207.164:3000';
 packages: any[] = [];
