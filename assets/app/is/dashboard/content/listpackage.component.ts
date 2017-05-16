7af91fd2c750c5ed0cc2628a306422059aff5c4a
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-16 08:03:09 +0700

    update

--- assets/app/is/dashboard/content/listpackage.component.ts
+++ assets/app/is/dashboard/content/listpackage.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import { Http, Headers} from 'angular2/http';
 import 'rxjs/add/operator/map';
-import { Sub } from './subs';
+import { Package } from './package';
 
 @Component({
     selector: 'form-subs',
@@ -14,17 +14,17 @@ import { Sub } from './subs';
 export class ContentListPackageComponent implements OnInit {
 //@Input() idsubs: string;
 API = 'http://202.162.207.164:3000';
-subs: any[] = [];
+packages: any[] = [];
   ngOnInit() {
       this.getSubs();
   }
   constructor(private http: Http) {}
   // Get all Subs from the API
       getSubs() {
-          this.http.get(`${this.API}/subscribe/subs/58b3cdac45912d052e2c85a5`)
+          this.http.get(`${this.API}/package/cluster/B`)
               .map(res => res.json())
-              .subscribe(subs => {
-                  this.subs = subs
+              .subscribe(packages => {
+                  this.packages = packages
               })
       }
 }
