d062c9cfbb2ba2c321fc340a79d23c114000c47e
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-19 09:43:45 +0700

    update

--- assets/app/is/dashboard/content/addsubs.component.ts
+++ assets/app/is/dashboard/content/addsubs.component.ts
@@ -203,7 +203,7 @@ export class ContentAddSubsComponent implements OnInit {
                 })
         }
         this.packages = this.getAllPackagesByCluster(){
-            this.http.get(`${this.API}/package/cluster/${detailclusters.level}`)
+            this.http.get(`${this.API}/package/cluster/${this.detailclusters[level]}`)
                 .map(res => res.json())
                 .subscribe(packages => {
                     this.packages = packages
