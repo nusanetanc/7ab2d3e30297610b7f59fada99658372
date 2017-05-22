0b8e8ee4d187f1b42a38e37203877873fbfa760d
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-22 11:41:22 +0700

    update

--- assets/app/login-is/app.component.ts
+++ assets/app/login-is/app.component.ts
@@ -15,7 +15,7 @@ import {LoginComponent} from "./login.component";
 })
 
 @RouteConfig([
-    {path: '/is/signin', name: 'Login', component: LoginComponent, useAsDefault: true}
+    {path: '/login', name: 'Login', component: LoginComponent, useAsDefault: true}
 ])
 
 export class AppComponent{
