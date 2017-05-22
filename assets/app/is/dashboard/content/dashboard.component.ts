bfc6616177c0fc8b3a5241f81d17c8e5b16e66fb
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-22 16:32:50 +0700

    update

--- assets/app/is/dashboard/content/dashboard.component.ts
+++ assets/app/is/dashboard/content/dashboard.component.ts
@@ -12,7 +12,7 @@ import {Report} from './allreports';
                   <h3 id="home" class="fontWeight300">
                       <a id="menu-toggle" style="cursor:pointer" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
                       </a>
-                      &nbsp; Dashboard (MARKETING)
+                      &nbsp; Dashboard
                   </h3>
               </div>
 
@@ -78,12 +78,12 @@ import {Report} from './allreports';
           </div>
 
           <!-- START CONTENT -->
-          <div *ngIf="emps.accessrole == '3' || emps.accessrole == '301'" id="page-content-wrapper" >
+          <div *ngIf="emps.accessrole == '3' || emps.accessrole == '301' || emps.accessrole == '4' || emps.accessrole == '401' || emps.accessrole == '402'" id="page-content-wrapper" >
              <div class="content-header">
                 <h3 id="home">
                    <a id="menu-toggle" href="#" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
                    </a>
-                   &nbsp; Dashboard [Technical]
+                   &nbsp; Dashboard
                 </h3>
              </div>
              <div class="page-content inset" data-spy="scroll" data-target="#spy">
@@ -152,6 +152,265 @@ import {Report} from './allreports';
              </div>
           </div>
           <!-- END CONTENT -->
+
+          <!-- Page content -->
+                <div id="page-content-wrapper" *ngIf="emps.accessrole == '5' || emps.accessrole == '501' || emps.accessrole == '502'">
+                    <div class="content-header">
+                        <h3 id="home" class="fontWeight300">
+                            <a id="menu-toggle" style="cursor:pointer" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
+                            </a>
+                            &nbsp; Dashboard
+                        </h3>
+                    </div>
+
+                    <div class="page-content inset" data-spy="scroll" data-target="#spy">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="row marginLR15">
+                                    <div class="col-sm-4">
+                                          <a [routerLink]="['AllSubs']">
+                                                <div class="cardDashboardSub">
+                                                    <div class="row">
+                                                        <div class="col-sm-12">
+                                                            <h4 class="fontWeight300 marginT20 marginL20">WAITING VERIFICATION</h4>
+                                                            <p class="text-center numberInCardBilling" style="padding-top: 20px;"><b>12</b></p>
+                                                            <h4 class="text-center fontWeight300">SUBSCRIBERS</h4>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                          </a>
+                                    </div>
+                                    <div class="col-sm-4">
+                                          <a [routerLink]="['AllSubs']">
+                                            <div class="cardDashboardSub">
+                                                <div class="row">
+                                                    <div class="col-sm-12">
+                                                        <h4 class="fontWeight300 marginT20 marginL20">UNPAID</h4>
+                                                        <p class="text-center numberInCardBilling" style="padding-top: 20px;"><b>4</b></p>
+                                                        <h4 class="text-center fontWeight300">SUBSCRIBERS</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                          </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Content List -->
+                        <div class="row paddingLR15Margin20">
+                            <div class="col-sm-12">
+                                <div class="row headerList paddingLR30">
+                                    <div class="col-sm-12 paddingT20 paddingL35 headerSubList"><strong>LATEST USER REPORT</strong></div>
+                                </div>
+                                <div class="row subInfo fontWeight300" *ngFor="#complaint of complaints">
+                                    <div class="col-sm-3 invoiceId"><span>{{complaint.dateopen}}</span></div>
+                                    <div class="col-sm-7 invoiceList"><span>{{complaint.subcategory}}</span></div>
+                                    <div class="col-sm-2 invoiceList"><span class="red">{{complaint.status}}</span></div>
+                                </div>
+                                <div class="row subInfo">
+                                    <div class="col-sm-12 invoiceId"><span><a class="linkViewAll fontWeight300" [routerLink]="['AllReport']">View all reports</a></span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /Content List -->
+                    </div>
+                </div>
+
+
+                <!-- START CONTENT -->
+                <div *ngIf="emps.accessrole == '6' || emps.accessrole == '601'" id="page-content-wrapper">
+                   <div class="content-header">
+                      <h3 id="home">
+                         <a id="menu-toggle" href="#" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
+                         </a>
+                         &nbsp; Dashboard
+                      </h3>
+                   </div>
+                   <div class="page-content inset" data-spy="scroll" data-target="#spy">
+                      <div class="row">
+                         <div class="col-sm-12">
+                            <div class="row marginLR15">
+                                <div class="col-sm-4">
+                                    <a [routerLink]="['AllSubs']">
+                                        <div class="cardDashboardSub">
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <p class="text-center font70MarginT35"><b>{{subs.length}}</b></p>
+                                                    <h4 class="text-center fontWeight300">SUBSCRIBERS</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                                  <div class="col-sm-4">
+                                    <a [routerLink]="['Information']">
+                                      <div class="cardDashboardSub">
+                                          <div class="row marginB10">
+                                              <div class="col-sm-12 text-center">
+                                                  <i class="material-icons font100Margin30">info</i>
+                                                  <h4 class="marginLR20 fontWeight300">VIEW ALL INFORMATIONS</h4>
+                                              </div>
+                                          </div>
+                                      </div>
+                                    </a>
+                                  </div>
+                            </div>
+                         </div>
+                      </div>
+
+                      <!-- Content List -->
+                      <div *ngIf="emps.accessrole == '7' || emps.accessrole == '701' || emps.accessrole == '702'" class="row paddingLR15Margin20">
+                          <div class="col-sm-12">
+                              <div class="row headerList paddingLR30">
+                                  <div class="col-sm-12 paddingT20 paddingL35 headerSubList"><strong>LATEST USER REPORT</strong></div>
+                              </div>
+                              <div class="row subInfo fontWeight300" *ngFor="#complaint of complaints">
+                                  <div class="col-sm-3 invoiceId"><span>{{complaint.dateopen}}</span></div>
+                                  <div class="col-sm-7 invoiceList"><span>{{complaint.subcategory}}</span></div>
+                                  <div class="col-sm-2 invoiceList"><span class="red">{{complaint.status}}</span></div>
+                              </div>
+                              <div class="row subInfo">
+                                  <div class="col-sm-12 invoiceId"><span><a class="linkViewAll fontWeight300" [routerLink]="['AllReport']">View all reports</a></span></div>
+                              </div>
+                          </div>
+                      </div>
+                      <!-- /Content List -->
+                   </div>
+                </div>
+                <!-- END CONTENT -->
+
+                <!-- START CONTENT -->
+                <div id="page-content-wrapper">
+                   <div class="content-header">
+                      <h3 id="home">
+                         <a id="menu-toggle" href="#" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
+                         </a>
+                         &nbsp; Dashboard
+                      </h3>
+                   </div>
+                   <div class="page-content inset" data-spy="scroll" data-target="#spy">
+                      <div class="row">
+                         <div class="col-sm-12">
+                            <div class="row marginLR15">
+                               <div class="col-sm-4">
+                                    <a [routerLink]="['AllSubs']">
+                                        <div class="cardDashboardSub">
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <p class="text-center font70MarginT35"><b>{{subs.length}}</b></p>
+                                                    <h4 class="text-center fontWeight300">SUBSCRIBERS</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                               <div class="col-sm-4">
+                                    <a [routerLink]="['Information']">
+                                      <div class="cardDashboardSub">
+                                          <div class="row marginB10">
+                                              <div class="col-sm-12 text-center">
+                                                  <i class="material-icons font100Margin30">info</i>
+                                                  <h4 class="marginLR20 fontWeight300">VIEW ALL INFORMATIONS</h4>
+                                              </div>
+                                          </div>
+                                      </div>
+                                    </a>
+                               </div>
+                               <div class="col-sm-4">
+                                  <div class="cardDashboardSub">
+                                     <div class="row margin10White">
+                                        <div class="col-sm-12 text-center">
+                                        </div>
+                                     </div>
+                                  </div>
+                               </div>
+                            </div>
+                         </div>
+                      </div>
+
+                      <!-- Content List -->
+                      <div class="row paddingLR15Margin20">
+                          <div class="col-sm-12">
+                              <div class="row headerList paddingLR30">
+                                  <div class="col-sm-12 paddingT20 paddingL35 headerSubList"><strong>LATEST USER REPORT</strong></div>
+                              </div>
+                              <div class="row subInfo fontWeight300" *ngFor="#complaint of complaints">
+                                  <div class="col-sm-3 invoiceId"><span>{{complaint.dateopen}}</span></div>
+                                  <div class="col-sm-7 invoiceList"><span>{{complaint.subcategory}}</span></div>
+                                  <div class="col-sm-2 invoiceList"><span class="red">{{complaint.status}}</span></div>
+                              </div>
+                              <div class="row subInfo">
+                                  <div class="col-sm-12 invoiceId"><span><a class="linkViewAll fontWeight300" [routerLink]="['AllReport']">View all reports</a></span></div>
+                              </div>
+                          </div>
+                      </div>
+                      <!-- /Content List -->
+                   </div>
+                </div>
+                <!-- END CONTENT -->
+
+                <!-- Page content -->
+                      <div *ngIf="emps.accessrole == '8' || emps.accessrole == '801'" id="page-content-wrapper">
+                          <div class="content-header">
+                              <h3 id="home" class="fontWeight300">
+                                  <a id="menu-toggle" style="cursor:pointer" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
+                                  </a>
+                                  &nbsp; Dashboard
+                              </h3>
+                          </div>
+
+                          <div class="page-content inset" data-spy="scroll" data-target="#spy">
+                              <div class="row">
+                                  <div class="col-sm-12">
+                                      <div class="row marginLR15">
+                                          <div class="col-sm-4">
+                                            <a [routerLink]="['AddInformation']">
+                                              <div class="cardDashboardSub">
+                                                  <div class="row marginB10">
+                                                      <div class="col-sm-12 text-center">
+                                                          <i class="material-icons font100Margin30">mode_edit</i>
+                                                          <h4 class="marginLR20 fontWeight300">POST MAINTENANCE INFORMATION</h4>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                            </a>
+                                          </div>
+                                          <div class="col-sm-4">
+                                            <a [routerLink]="['Information']">
+                                              <div class="cardDashboardSub">
+                                                  <div class="row marginB10">
+                                                      <div class="col-sm-12 text-center">
+                                                          <i class="material-icons font100Margin30">info</i>
+                                                          <h4 class="marginLR20 fontWeight300">VIEW ALL INFORMATIONS</h4>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                            </a>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+
+                              <!-- Content List -->
+                              <div class="row paddingLR15Margin20">
+                                  <div class="col-sm-12">
+                                      <div class="row headerList paddingLR30">
+                                          <div class="col-sm-12 paddingT20 paddingL35 headerSubList"><strong>LATEST USER REPORT</strong></div>
+                                      </div>
+                                      <div class="row subInfo fontWeight300" *ngFor="#complaint of complaints">
+                                          <div class="col-sm-3 invoiceId"><span>{{complaint.dateopen}}</span></div>
+                                          <div class="col-sm-7 invoiceList"><span>{{complaint.subcategory}}</span></div>
+                                          <div class="col-sm-2 invoiceList"><span class="red">{{complaint.status}}</span></div>
+                                      </div>
+                                      <div class="row subInfo">
+                                          <div class="col-sm-12 invoiceId"><span><a class="linkViewAll fontWeight300" [routerLink]="['AllReport']">View all reports</a></span></div>
+                                      </div>
+                                  </div>
+                              </div>
+                              <!-- /Content List -->
+                          </div>
+                      </div>
     `,
     directives: [ROUTER_DIRECTIVES],
 })
