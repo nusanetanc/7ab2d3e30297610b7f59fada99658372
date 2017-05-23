fe42ec033e2db007de82021aa7f1bb6d37928abd
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-23 19:26:50 +0700

    update

--- assets/app/is/dashboard/content/coverage.component.ts
+++ assets/app/is/dashboard/content/coverage.component.ts
@@ -109,8 +109,6 @@ import {Emp} from "./emp";
 export class ContentCoverageComponent {
 // Link to our api, pointing to localhost
 API = 'http://202.162.207.164:3000';
-//Session_ID = '58b6a0d77dfd7052a9fe53c9';
-content_access = '202';
 
 emps: any[] = [];
 constructor(private http: Http) {}
