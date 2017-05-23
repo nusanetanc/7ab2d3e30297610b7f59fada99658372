c69e2da2a69a4cbf6a44e982d603e41a4c836dd2
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-23 17:23:48 +0700

    update

--- assets/app/is/dashboard/content/detailsubbill.component.ts
+++ assets/app/is/dashboard/content/detailsubbill.component.ts
@@ -182,7 +182,7 @@ import { Sub } from './subs';
             <!-- /Content List -->
         </div>
     </div>
-    <div *ngIf="emps.accessrole == '0' || emps.accessrole == '1' || emps.accessrole == '5' || emps.accessrole == '501' || emps.accessrole == '502'" class='fullscreenDiv'>
+    <div *ngIf="emps.accessrole != '0' || emps.accessrole != '1' || emps.accessrole != '5' || emps.accessrole != '501' || emps.accessrole != '502'" class='fullscreenDiv'>
         <div class="center"><span style="font-size: 72px; font-weight: 700; color: #c1c1c1;"><center>404</center> PAGE NOT FOUND</span><br><hr class="hr1"></div>
     </div>
     `,
