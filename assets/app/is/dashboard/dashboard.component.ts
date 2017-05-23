b4420a24f7bdfcf170d1a659e2db057e036f5384
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-23 11:30:08 +0700

    update

--- assets/app/is/dashboard/dashboard.component.ts
+++ assets/app/is/dashboard/dashboard.component.ts
@@ -104,7 +104,7 @@ import {Emp} from "./emp";
 })
 
 
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
     // Link to our api, pointing to localhost
     API = 'http://202.162.207.164:3000';
     //Session_ID = '58b6a0d77dfd7052a9fe53c9';
