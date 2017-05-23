de1c3a363042a3131e55a8da3bf8bb6767172a67
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-23 19:37:06 +0700

    update

--- assets/app/is/dashboard/content/coverage.component.ts
+++ assets/app/is/dashboard/content/coverage.component.ts
@@ -100,7 +100,7 @@ import {Emp} from "./emp";
             </div>
         </div>
     </div>
-    <div *ngIf="emps.accessrole != '0' || emps.accessrole != '1' || emps.accessrole != '6' || emps.accessrole != '601'" class='fullscreenDiv'>
+    <div *ngIf="emps.accessrole != '0' || emps.accessrole != '1' || emps.accessrole != '6' || emps.accessrole != '601'">
         <div class="center"><span style="font-size: 72px; font-weight: 700; color: #c1c1c1;"><center>404</center> PAGE NOT FOUND</span><br><hr class="hr1"></div>
     </div>
     `,
