e7d40e471f397aa7e79e54c332382ac424f840cb
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-24 11:26:01 +0700

    update

--- assets/app/is/dashboard/content/profileemp.component.ts
+++ assets/app/is/dashboard/content/profileemp.component.ts
@@ -21,7 +21,14 @@ import 'rxjs/add/operator/map';
                  <a [routerLink]="['AllEmployee']" class="btn btn-default buttonBack" type="button">
                     BACK
                  </a>
-                 
+                 <div *ngIf="emps.accessrole == '0' || emps.accessrole == '1' || emps.accessrole == '7' || emps.accessrole == '701'">
+                     <button *ngIf="emps.status=='Disabled'" type="submit" (click)="AccountEnabled()" class="btn btn-default buttonOrange right">
+                         Enable Account
+                     </button>
+                     <button *ngIf="emps.status=='Enabled'" type="submit" (click)="AccountDisabled()" class="btn btn-default buttonOrange right">
+                         Disabled Account
+                     </button>
+                  </div>
              </div>
           </div>
           <div class="row subInfo">
