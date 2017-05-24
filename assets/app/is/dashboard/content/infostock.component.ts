2806666ac3480b51e6b8e64dd3d227cf8c96ade6
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-24 09:21:05 +0700

    update

--- assets/app/is/dashboard/content/infostock.component.ts
+++ assets/app/is/dashboard/content/infostock.component.ts
@@ -8,7 +8,7 @@ import {Subscription} from "rxjs/Rx";
     selector: 'form-infostock',
     template: `
     <!-- Page content -->
-    <div id="page-content-wrapper">
+    <div *ngIf="emps.accessrole == '0' || emps.accessrole == '1' || emps.accessrole == '7' || emps.accessrole == '702'" id="page-content-wrapper">
         <div class="content-header">
             <h3 id="home" class="fontWeight300">
                 <a id="menu-toggle" style="cursor:pointer" class="glyphicon glyphicon-menu-hamburger btn-menu toggle">
@@ -43,6 +43,9 @@ import {Subscription} from "rxjs/Rx";
                 </div>
             </div>
         </div>
+    </div>
+    <div *ngIf="emps.accessrole == '2' || emps.accessrole == '201' || emps.accessrole == '202' || emps.accessrole == '3' || emps.accessrole == '301' || emps.accessrole == '4' || emps.accessrole == '401' || emps.accessrole == '402' || emps.accessrole == '6' || emps.accessrole == '601' || emps.accessrole == '701' || emps.accessrole == '5' || emps.accessrole == '501' || emps.accessrole == '502' || emps.accessrole == '8' || emps.accessrole == '801'">
+        <div class="center"><span style="font-size: 72px; font-weight: 700; color: #c1c1c1;"><center>404</center> PAGE NOT FOUND</span><br><hr class="hr1"></div>
     </div>
     `,
     directives: [ROUTER_DIRECTIVES],
