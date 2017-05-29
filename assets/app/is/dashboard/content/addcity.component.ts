3dd49906eaad64fdaf857bab575a3e88ca465d3f
fahmadrifki <fahmadrifki@gmail.com>
2017-05-29 13:53:35 +0700

    update

--- assets/app/is/dashboard/content/addcity.component.ts
+++ assets/app/is/dashboard/content/addcity.component.ts
@@ -39,7 +39,7 @@ import { City } from './cities';
                                             <form>
                                                 <input #cityname type="text" class="form-control inputForm" id="cityname" placeholder="New City">
                                                 <br/>
-                                                
+                                                <div class="g-recaptcha" data-sitekey="6LeSRiMUAAAAADnvbK81VC7BeXJiCch-G1lp8ma8">test</div>
                                                 <button type="submit" (click)="addCity(cityname.value)" class="btn btn-default buttonOrange">
                                                     SEND
                                                 </button>
