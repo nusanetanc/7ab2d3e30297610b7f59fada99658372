47d8878452763c957c75e7f2a7f54bae3cfcb2cd
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-29 15:07:05 +0700

    update

--- assets/app/is/dashboard/content/addsubs.component.ts
+++ assets/app/is/dashboard/content/addsubs.component.ts
@@ -122,7 +122,7 @@ import {Streetname} from "./street_name";
                                             </select><br/>
                                         </div>
                                     </div>
-                                </div>
+                                </div> <!--
                                 <div class="row">
                                     <div class="col-sm-12">
                                         <h4 class="titleH4">SUBSCRIPTION PLAN</h4>
@@ -137,7 +137,7 @@ import {Streetname} from "./street_name";
                                           </select><br/>
                                         </div>
                                     </div>
-                                </div>
+                                </div> -->
                                 <div class="row">
                                     <div class="col-sm-12 paddingR45">
                                         <div class="g-recaptcha" data-sitekey="6Ld3RSMUAAAAAPv1Xt2HB1ruXyKvYpZ4q9m9Sk4T"></div>
@@ -201,13 +201,6 @@ export class ContentAddSubsComponent implements OnInit {
                 .map(res => res.json())
                 .subscribe(detailclusters => {
                     this.detailclusters = detailclusters
-                })
-        }
-        this.packages = this.getAllPackagesByCluster(){
-            this.http.get(`${this.API}/package/cluster/${detailclusters['level']}`)
-                .map(res => res.json())
-                .subscribe(packages => {
-                    this.packages = packages
                 })
         }
     }
