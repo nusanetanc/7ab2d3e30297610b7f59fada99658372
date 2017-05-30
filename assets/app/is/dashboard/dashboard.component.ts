e4b664a34b41a30de5fd57d4cc4c2134244404d0
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-05-30 10:05:24 +0700

    update

--- assets/app/is/dashboard/dashboard.component.ts
+++ assets/app/is/dashboard/dashboard.component.ts
@@ -29,7 +29,7 @@ import {Emp} from "./emp";
                                     </div>
                                     <div class="dropdown-content" style="margin-top: 20px;">
                                       <a>Account</a>
-                                      <a (click)="Logout()">Logout</a>
+                                      <a type="submit" (click)="Logout()">Logout</a>
                                     </div>
                                  </div>
                                  <div class="row" style="padding: 5px 0px;">
