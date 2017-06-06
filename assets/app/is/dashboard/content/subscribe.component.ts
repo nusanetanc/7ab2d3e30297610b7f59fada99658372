dc465a50f9646bd0903e8a20a519ec5da20f48b5
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-06-06 09:57:38 +0700

    update

--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -47,7 +47,7 @@ import { ContentPackLevComponent } from './packlev.component';
                         </div>
                     </div>
 
-                    <div *ngIf="clickedItem.name == 'View'" class="row">
+                    <div class="row">
                         <div class="col-sm-6">
                             <div class="row marginTB10 marginL5">
                                 <div class="col-xs-6 col-sm-4">
@@ -121,80 +121,89 @@ import { ContentPackLevComponent } from './packlev.component';
                               </button>
                             </div>
                         </div>
-                        <div *ngIf="clickedItem.name == 'Edit' && sessionemps.accessrole == '601'" class="row">
-                            <div class="col-sm-6">
-                                <div class="row marginTB10 marginL5">
-                                    <div class="col-xs-6 col-sm-4">
-                                        <span>Full Name</span>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-1">
-                                        <span>:</span>
-                                    </div>
-                                    <div class="col-xs-12 col-md-7">
-                                        <span>{{ subs.name }}</span>
-                                    </div>
-                                </div>
-                                <div class="row marginTB10 marginL5">
-                                    <div class="col-xs-6 col-sm-4">
-                                        <span>Email</span>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-1">
-                                        <span>:</span>
-                                    </div>
-                                    <div class="col-xs-12 col-md-7">
-                                        <span>{{ subs.email }}</span>
-                                    </div>
-                                </div>
-                                <div class="row marginTB10 marginL5">
-                                    <div class="col-xs-6 col-sm-4">
-                                        <span>Handphone</span>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-1">
-                                        <span>:</span>
-                                    </div>
-                                    <div class="col-xs-12 col-md-7">
-                                        <span>{{ subs.phone }}</span>
-                                    </div>
-                                </div>
-                                <div class="row marginTB10 marginL5">
-                                    <div class="col-xs-6 col-sm-4">
-                                        <span>Address</span>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-1">
-                                        <span>:</span>
-                                    </div>
-                                    <div class="col-xs-12 col-md-7">
-                                        <span>{{ subs.address }} No. {{ subs.nohome }},<br>{{ subs.cluster }}, {{ subs.city }}</span>
-                                    </div>
-                                </div>
-                                <div class="row marginTB10 marginL5">
-                                    <div class="col-xs-6 col-sm-4">
-                                        <span>National Identy Card No.</span>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-1">
-                                        <span>:</span>
-                                    </div>
-                                    <div class="col-xs-12 col-md-7">
-                                        <span>{{ subs.idnumber }}</span>
-                                    </div>
-                                </div>
-                                <div class="row marginTB10 marginL5">
-                                    <div class="col-xs-6 col-sm-4">
-                                        <span>Date of Birth</span>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-1">
-                                        <span>:</span>
-                                    </div>
-                                    <div class="col-xs-12 col-md-7">
-                                        <span>{{ subs.datebrith }}</span>
-                                    </div>
-                                </div>
-                                <div class="col-sm-12">
-                                  <button type="submit" class="btn btn-default buttonOrange">
-                                      UPDATE
-                                  </button>
+                        <div class="col-sm-6">
+                            <div class="row">
+                                <div class="col-xs-6 col-md-3">
+                                    <img class="avaProfile" src="images/{{ subs._id }}.png" alt="{{ subs._id }}">
                                 </div>
                             </div>
+                        </div>
+                    </div>
+
+                                        <div class="row">
+                        <div class="col-sm-6">
+                            <div class="row marginTB10 marginL5">
+                                <div class="col-xs-6 col-sm-4">
+                                    <span>Full Name</span>
+                                </div>
+                                <div class="col-xs-6 col-sm-1">
+                                    <span>:</span>
+                                </div>
+                                <div class="col-xs-12 col-md-7">
+                                    <span>{{ subs.name }}</span>
+                                </div>
+                            </div>
+                            <div class="row marginTB10 marginL5">
+                                <div class="col-xs-6 col-sm-4">
+                                    <span>Email</span>
+                                </div>
+                                <div class="col-xs-6 col-sm-1">
+                                    <span>:</span>
+                                </div>
+                                <div class="col-xs-12 col-md-7">
+                                    <span>{{ subs.email }}</span>
+                                </div>
+                            </div>
+                            <div class="row marginTB10 marginL5">
+                                <div class="col-xs-6 col-sm-4">
+                                    <span>Handphone</span>
+                                </div>
+                                <div class="col-xs-6 col-sm-1">
+                                    <span>:</span>
+                                </div>
+                                <div class="col-xs-12 col-md-7">
+                                    <span>{{ subs.phone }}</span>
+                                </div>
+                            </div>
+                            <div class="row marginTB10 marginL5">
+                                <div class="col-xs-6 col-sm-4">
+                                    <span>Address</span>
+                                </div>
+                                <div class="col-xs-6 col-sm-1">
+                                    <span>:</span>
+                                </div>
+                                <div class="col-xs-12 col-md-7">
+                                    <span>{{ subs.address }} No. {{ subs.nohome }},<br>{{ subs.cluster }}, {{ subs.city }}</span>
+                                </div>
+                            </div>
+                            <div class="row marginTB10 marginL5">
+                                <div class="col-xs-6 col-sm-4">
+                                    <span>National Identy Card No.</span>
+                                </div>
+                                <div class="col-xs-6 col-sm-1">
+                                    <span>:</span>
+                                </div>
+                                <div class="col-xs-12 col-md-7">
+                                    <span>{{ subs.idnumber }}</span>
+                                </div>
+                            </div>
+                            <div class="row marginTB10 marginL5">
+                                <div class="col-xs-6 col-sm-4">
+                                    <span>Date of Birth</span>
+                                </div>
+                                <div class="col-xs-6 col-sm-1">
+                                    <span>:</span>
+                                </div>
+                                <div class="col-xs-12 col-md-7">
+                                    <span>{{ subs.datebrith }}</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                              <button type="submit" class="btn btn-default buttonOrange">
+                                  UPDATE
+                              </button>
+                            </div>
+                        </div>
                         <div class="col-sm-6">
                             <div class="row">
                                 <div class="col-xs-6 col-md-3">
@@ -353,8 +362,6 @@ import { ContentPackLevComponent } from './packlev.component';
     directives: [ContentPackLevComponent, ROUTER_DIRECTIVES],
 })
 export class ContentSubscribeComponent implements OnInit {
-
-public clickedItem = {name: "View"};
 
 selectedEmp1: Employee = new Employee(0, 'dummy');
 selectedEmp2: Employee = new Employee(0, 'dummy');
