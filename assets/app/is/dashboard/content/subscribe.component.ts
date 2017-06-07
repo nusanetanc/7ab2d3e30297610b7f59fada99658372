a197c9d5b248f322f58974c003a54da4f0ab27cb
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-06-07 19:24:03 +0700

    update

--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -363,8 +363,8 @@ onItemClicked(EditData) {
    this.clickedItem = {name: "EditData"};
 }
 
-onItemClicked(Cancel) {
-   this.clickedItem = {name: "View"};
+onItemClicked(EditData) {
+   this.clickedItem = {name: "EditData"};
 }
 selectedEmp1: Employee = new Employee(0, 'dummy');
 selectedEmp2: Employee = new Employee(0, 'dummy');
