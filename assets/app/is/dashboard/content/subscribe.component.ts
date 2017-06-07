ebe3275b8a7876e3db5d5d210320f2607632fd1e
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-06-07 17:22:56 +0700

    update

--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -119,9 +119,7 @@ import { ContentPackLevComponent } from './packlev.component';
                               <button (click)="onItemClicked(EditData)" type="submit" class="btn btn-default buttonOrange">
                                   UPDATE PERSONAL DATA
                               </button>
-                              <button (click)="onItemClicked(EditAddress)" type="submit" class="btn btn-default buttonOrange">
-                                  UPDATE ADDRESS
-                              </button>
+                              
                             </div>
                         </div>
                         <div class="col-sm-6">
