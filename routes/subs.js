c10798d6342852028c5c72ef99e3408fc1ca049e
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-06-09 09:46:17 +0700

    update

--- routes/subs.js
+++ routes/subs.js
@@ -237,12 +237,12 @@ router.get('/subsaddress', function(req, res, next) {
     }
     Home.findOne({_id: subs.groovyid}, function(err, homes) {
       city: homes.city,
-      property: homes.property,
+      //property: homes.property,
       cluster: homes.cluster,
       blokfloor: homes.blokfloor,
       streetname: homes.streetname,
       homeid: homes._id
-    });o
+    });
       });
     }
 
