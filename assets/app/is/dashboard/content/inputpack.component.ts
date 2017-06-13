97e4e4edaf78339ca60c79ad55562fd7d89c238c
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-06-13 10:43:30 +0700

    update

--- assets/app/is/dashboard/content/inputpack.component.ts
+++ assets/app/is/dashboard/content/inputpack.component.ts
@@ -29,13 +29,25 @@ import { Package } from './package';
             <input [(ngModel)]="packages.price" type="number" class="form-control inputForm" #packageprice id="packageprice" placeholder="Package Price" disabled/>
         </div>
     </div>
-    
+    <div class="row marginTB10 marginL5">
+        <div class="col-xs-6 col-sm-4">
+            <span>Promo Name</span>
+        </div>
+        <div class="col-xs-6 col-sm-1">
+            <span>:</span>
+        </div>
+        <div *ngIf="packages.type == 'Default'" class="col-xs-12 col-md-7">
+          <input [(ngModel)]="default" type="text" class="form-control inputForm" #promoname id="promoname" placeholder="Promo Name" disabled/>
+        </div>
+    </div>
     `,
     directives: [ROUTER_DIRECTIVES],
 })
 export class ContentInputPackComponent implements OnInit {
 @Input() packid: string;
 API = 'http://202.162.207.164:3000';
+promo = 'Yes';
+default = 'No';
 packages: any[] = [];
   ngOnInit() {
       this.getPackages();
