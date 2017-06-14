d89a60590b4f9ebd7334ec8f160effcd8757cd7e
ferdianrb <frizki7@gmail.com>
2017-06-14 11:17:03 +0700

    update

--- assets/app/is/dashboard/content/createinvoice.component.ts
+++ assets/app/is/dashboard/content/createinvoice.component.ts
@@ -141,7 +141,7 @@ import { ContentInputPackComponent } from './inputpack.component';
                                     <span>:</span>
                                 </div>
                                 <div class="col-xs-12 col-md-7">
-                                    <span>{{ stringAsDate(subs.activedate)}}</span>
+                                    <span>{{ stringAsDate(subs.activedate) }}</span>
                                 </div>
                             </div>
                             <div class="row marginTB10 marginL5">
