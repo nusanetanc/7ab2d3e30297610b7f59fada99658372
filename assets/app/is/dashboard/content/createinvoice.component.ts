78f0ae748ca32d95bb1fd041b4e7e5d25d9073ba
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-06-15 19:04:23 +0700

    update

--- assets/app/is/dashboard/content/createinvoice.component.ts
+++ assets/app/is/dashboard/content/createinvoice.component.ts
@@ -342,7 +342,7 @@ import { ContentInputPackComponent } from './inputpack.component';
                     <div class="col-sm-12">
                         <div class="g-recaptcha" data-sitekey="6LdqYiMUAAAAAG24p30ejQSqeWdvTpD0DK4oj5wv"></div>
                         <!-- Small modal -->
-                        <button type="submit" (click)="createInvoice(invoicedate.value, duedate.value, namepackage.value, packageprice.value, promoname.value, prorateprice.value, routerprice.value, stbprice.value, instalationprice.value, cablerj45price.value, pinaltyprice.value, subtotal.value, taxprice.value, totalprice.value)" class="btn btn-default buttonOrange marginT20 marginL20 paddingL10">CONFIRM</button>
+                        <button type="submit" (click)="createInvoice(invoicedate.value, duedate.value, namepackage.value, packageprice.value, promoname.value)" class="btn btn-default buttonOrange marginT20 marginL20 paddingL10">CONFIRM</button>
                     </div>
                 </div>
             </div>
@@ -406,7 +406,7 @@ total:number;
 
 
 // Add one person to the API
-  createInvoice(invoicedate, duedate, namepackage, packageprice, promoname, prorateprice, routerprice, stbprice, instalationprice, cablerj45price, pinaltyprice, subtotal, taxprice, totalprice) {
+  createInvoice(invoicedate, duedate, namepackage, packageprice, promoname) {
   var body = `sub=${this._routeParams.get('id')}&billdate=${invoicedate}&duedate=${duedate}&namepack=${namepackage}&pricepack=${packageprice}&promoname=${promoname}`;
   var headers = new Headers();
   headers.append('Content-Type', 'application/x-www-form-urlencoded');
