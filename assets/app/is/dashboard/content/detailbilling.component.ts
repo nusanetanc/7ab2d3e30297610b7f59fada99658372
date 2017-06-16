ef4133ee895faaf72d965a7a36d086bdc995e4d3
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-06-16 14:49:24 +0700

    update

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -188,7 +188,7 @@ declare let kendo;
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div *ngIf="bills.pricestb != null" *ngIf="bills.pricestb != '0'" class="row">
+                                                <div *ngIf="bills.pricestb !== null || bills.pricestb !== '0'"  class="row">
                                                     <div class="col-sm-12 listWhite">
                                                         <div class="col-sm-10">
                                                             <span>STB Rent</span>
@@ -198,7 +198,7 @@ declare let kendo;
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div *ngIf="bills.pricerj45cable != null || bills.pricerj45cable != '0'" class="row">
+                                                <div *ngIf="bills.pricerj45cable !== null || bills.pricerj45cable !== 0" class="row">
                                                     <div class="col-sm-12 listWhite">
                                                         <div class="col-sm-10">
                                                             <span>Cable & RJ45</span>
@@ -208,7 +208,7 @@ declare let kendo;
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div class="row"  *ngIf="bills.priceinstal != null" *ngIf="bills.priceinstal != '0'">
+                                                <div class="row"  *ngIf="bills.priceinstal !== null || bills.priceinstal !== '0'">
                                                     <div class="col-sm-12 listWhite">
                                                         <div class="col-sm-10">
                                                             <span>Instalation Charge</span>
@@ -218,7 +218,7 @@ declare let kendo;
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div *ngIf="bills.pinaltypay != '0'" *ngIf="bills.pinaltypay != null" class="row">
+                                                <div *ngIf="bills.pinaltypay != '0' || bills.pinaltypay != null" class="row">
                                                     <div  class="col-sm-12 listWhite">
                                                         <div class="col-sm-10">
                                                             <span>Pinalty Charge</span>
