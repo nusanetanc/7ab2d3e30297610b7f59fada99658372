413a2388a7a1f53fbe709c6de04e850b9e7c3169
fahmadrifki <fahmadrifki@gmail.com>
2017-06-19 15:10:45 +0700

    update

--- assets/app/login-is/login.component.ts
+++ assets/app/login-is/login.component.ts
@@ -36,7 +36,7 @@ import { Sub } from './subs';
             <!-- Modal -->
             <div id="failed" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
               <div class="modal-dialog modal-sm" role="document">
-                <div class="modal-content text-center" style="padding: 5px; background-color: #FC592E;">
+                <div class="text-center" style="padding: 5px; background-color: #FC592E;">
                   <h5 style="color: #FFF;">User could not be found</h5>
                 </div>
               </div>
