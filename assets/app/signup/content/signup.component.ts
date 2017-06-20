4cc61bb178207451829fc9fa2c8e50d9aebdc132
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-06-20 13:43:54 +0700

    update

--- assets/app/signup/content/signup.component.ts
+++ assets/app/signup/content/signup.component.ts
@@ -89,8 +89,8 @@ import {Streetname} from "./street_name";
           <div class="row" *ngIf="clickedItemSignUp.name == 'regInst'">
              <div class="col-md-4 col-md-offset-4">
                 <p>Please select a installation date</p>
-                <input [(ngModel)]='subdateinst' (input)="subdateinst=$event.target.value" #subdateinst id="subdateinst" type="date" class="form-control formInput" />
-             </div> 
+                <input #subdateinst id="subdateinst" type="date" class="form-control formInput" />
+             </div>
              <div class="col-md-4 col-md-offset-4">
                 <p>Please select a available timeslot for that date</p>
                 <div class="col-sm-6 col-sm-offset-4">
