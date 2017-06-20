717e132d4be5b1875e4a5a31582c0e7be2c3ccf4
ferdianrb <frizki7@gmail.com>
2017-06-20 10:24:26 +0700

    update message

--- assets/app/login-is/login.component.ts
+++ assets/app/login-is/login.component.ts
@@ -38,7 +38,7 @@ import { Sub } from './subs';
             <div id="failed" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
               <div class="modal-dialog" role="document" style="float: left; padding-left: 44%;">
                 <div class="text-center" style="padding: 5px; background-color: #FC592E; width: 200px; float: left; box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0);">
-                  <h5 style="color: #FFF;">{{ message }}</h5>
+                  <h5 id="message" style="color: #FFF;"></h5>
                 </div>
               </div>
             </div>
@@ -87,6 +87,7 @@ export class LoginComponent implements OnInit {
                 error => {
                     //alert(error.text());
                     //console.log(JSON.stringify(error.json()));
+                    document.getElementById("message").innerHTML = error.text();
                     $('#failed').modal('show');
                     $('.modal-backdrop').removeClass("modal-backdrop");
                 }
