96aa3295188a2f89bb67642ee5e9600a11696ea5
fahmadrifki <fahmadrifki@gmail.com>
2017-07-06 11:54:23 +0700

    update

--- routes/subs.js
+++ routes/subs.js
@@ -559,11 +559,19 @@ router.post('/signin', function(req, res){
             });
         }
         if (!doc) {
-           return res.status(404).json('User could not be found.');
-        }
+           return res.status(404).json({
+           title: "No user found",
+           error: {message: 'User could not be found.'}
+        });
+    }
 
         if (!passwordHash.verify(req.body.password, doc.password)) {
-                return res.status(404).json('Invalid Password');
+            if (err) {
+                return res.status(404).json({
+                    title: "Could not sign user in",
+                    error: {message: 'Invalid Password'}
+                });
+            }
         }
 
         var token = jwt.sign({sub:doc}, 'secret', {expiresIn: 7200});
