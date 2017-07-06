cae48a582ae032948dbc79f8ad63a7f8a685aac9
fahmadrifki <fahmadrifki@gmail.com>
2017-07-06 10:42:23 +0700

    update

--- assets/app/signin-my/signin.component.ts
+++ assets/app/signin-my/signin.component.ts
@@ -8,6 +8,43 @@ import { Sub } from './subs';
 @Component({
     selector: 'form-signin',
     template: `
+
+        <div class="container container-auth-client">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="row">
+                        <div class="col-sm-4 card-login">
+                            <form class="form-horizontal">
+                                <center><img src="images/logo-groovy.png" alt="Logo Groovy" width="40%">
+                                <h6 class="grey-text">INFORMATION SYSTEM</h6></center><br><br>
+                                <div class="form-group">
+                                    <label for="inputEmail3" class="control-label orange-text">Email</label><br>
+                                    <input type="email" class="form-login" id="signEmail" #signEmail placeholder="Type your mail"><br>
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputPassword3" class="control-label orange-text">Password</label><br>
+                                    <input type="password" class="form-login" id="signPassword" #signPassword placeholder="Type your password"><br>
+                                </div><br>
+                                <div class="form-group form-forgot">
+                                    <a href="" class="orange-text">I forgot password</a>
+                                    <button type="submit" id="logins" class="btn btn-login" (click)="signEmp(signEmail.value, signPassword.value)">LOGIN</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Modal -->
+        <div id="failed" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+          <div class="modal-dialog" role="document" style="float: left; padding-left: 44%;">
+            <div class="text-center" style="padding: 5px; background-color: #FC592E; width: 200px; float: left; box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0);">
+              <h5 id="message" style="color: #FFF;"></h5>
+            </div>
+          </div>
+        </div>
+        
+
         <div class="container container-auth-client">
            <div class="top-margin text-center">
               <form class="form">
