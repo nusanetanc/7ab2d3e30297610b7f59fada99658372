a45cdca6b4755a18e69c8afd412c4c620421f88c
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-07-12 15:00:53 +0700

    update

--- routes/information.js
+++ routes/information.js
@@ -9,7 +9,7 @@ router.get('/listinformation', function(req, res, next) {
   session=req.session;
      Information.find(function(err, informations) {
        console.log( informations );
-       console.log(session.emp);
+       console.log(session['emp']);
        res.json(informations);
    });
 });
