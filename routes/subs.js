1b3b31d2846e75f8a6138aab9808ae90aa1ce656
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-07-17 16:21:29 +0700

    update

--- routes/subs.js
+++ routes/subs.js
@@ -138,7 +138,9 @@ router.get('/listinformation', function(req, res, next) {
     Information.find({to: homes.city}, function(err, informations) {
       //if(informations.to === homes._id || informations.to === homes.city || informations.to === homes.property || informations.to === homes.cluster || informations.to === homes.blokfloor || informations.to === homes.streetname){
       //console.log( informations );
-       res.json({_id: informations._id});
+       res.json([{
+         _id: informations._id
+       }]);
      //}
    });
  });
