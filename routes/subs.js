dfbf000c67c390a9fda194df903ae573f38d36c8
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-07-17 15:06:09 +0700

    update

--- routes/subs.js
+++ routes/subs.js
@@ -133,9 +133,15 @@ router.get('/listinformation', function(req, res, next) {
       title: "Access not found"
     });
   } else {
-    Information.find(function(err, informations) {
+    Sub.findById(req.session.subs, function(err, subs) {
+      Home.findById(subs.groovyid, function(err, homes) {
+    Information.find({informations.to: homes._id, informations.to: homes.city, informations.to: homes.property, informations.to: homes.cluster, informations.to: homes.blokfloor, informations.to: homes.streetname}, function(err, informations) {
+      //if(informations.to === homes._id || informations.to === homes.city || informations.to === homes.property || informations.to === homes.cluster || informations.to === homes.blokfloor || informations.to === homes.streetname){
       console.log( informations );
        res.json(informations);
+     //}
+   });
+ });
 });
  }
 
