35a095ce9fb22ab1cb0f557dcde8071d6f74bd3e
ferdianrb <frizki7@gmail.com>
2017-07-19 14:51:26 +0700

    input currency format

--- assets/app/is/dashboard/content/packages.component.ts
+++ assets/app/is/dashboard/content/packages.component.ts
@@ -48,7 +48,7 @@ import { ContentClusterNameComponent } from './clustername.component';
                                                 <option value="Promo">Promo</option>
                                                 <option value="Regular">Regular</option>
                                             </select><br/><br/>
-                                            <input id="price" #price min="0" step="0.01" data-number-to-fixed="2" data-number-stepfactor="100" type="number" class="form-control inputForm" placeholder="Price" [ngFormControl]="myForm.find('price')">
+                                            <input id="price" #price min="0" step="0.01" data-number-to-fixed="2" data-number-stepfactor="100" type="text" class="form-control inputForm" placeholder="Price" [ngFormControl]="myForm.find('price')">
                                             </form>                                     
                                         <div class="g-recaptcha" data-sitekey="6LdqYiMUAAAAAG24p30ejQSqeWdvTpD0DK4oj5wv"></div>
                                         <button [disabled]="!myForm.valid" type="submit" (click)="addPackage(level.value, cluster.value, detail.value, type.value, price.value)" class="btn btn-default buttonOrange">
