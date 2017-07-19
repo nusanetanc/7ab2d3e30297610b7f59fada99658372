7146fffe2786639bf07595456a08eaccfa1974d9
ferdianrb <frizki7@gmail.com>
2017-07-19 14:22:31 +0700

    input currency format

--- assets/app/is/dashboard/content/packages.component.ts
+++ assets/app/is/dashboard/content/packages.component.ts
@@ -169,4 +169,10 @@ getAllCluster() {
             }
           )
     }
+
+    webshims.setOptions('forms-ext', {
+        replaceUI: 'auto',
+        types: 'number'
+    });
+    webshims.polyfill('forms forms-ext');
 }
