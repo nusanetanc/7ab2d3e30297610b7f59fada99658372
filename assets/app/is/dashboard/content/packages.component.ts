675add2c90eaae7451f3d310f7159d0cf1cfe594
ferdianrb <frizki7@gmail.com>
2017-07-20 09:55:37 +0700

    input type currency

--- assets/app/is/dashboard/content/packages.component.ts
+++ assets/app/is/dashboard/content/packages.component.ts
@@ -94,6 +94,8 @@ import { ContentClusterNameComponent } from './clustername.component';
     </div>
     <div *ngIf="emps.accessrole == '2' || emps.accessrole == '201' || emps.accessrole == '202' || emps.accessrole == '3' || emps.accessrole == '301' || emps.accessrole == '4' || emps.accessrole == '401' || emps.accessrole == '402' || emps.accessrole == '5' || emps.accessrole == '501' || emps.accessrole == '502' || emps.accessrole == '7' || emps.accessrole == '701' || emps.accessrole == '702' || emps.accessrole == '8' || emps.accessrole == '801'">
         <div class="center"><span style="font-size: 72px; font-weight: 700; color: #c1c1c1;"><center>404</center> PAGE NOT FOUND</span><br><hr class="hr1"></div>
+    <script src="http://afarkas.github.io/webshim/js-webshim/minified/polyfiller.js"></script>
+                                            <input type="number" id="price" #price min="0" step="0.01" data-number-to-fixed="2" data-number-stepfactor="100" class="form-control inputForm" placeholder="Price">
     </div>
     <!-- Modal -->
     <div id="failed" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
@@ -105,8 +107,6 @@ import { ContentClusterNameComponent } from './clustername.component';
         </div>
     </div>
     <!-- END CONTENT -->
-    <script src="http://afarkas.github.io/webshim/js-webshim/minified/polyfiller.js"></script>
-                                            <input type="number" id="price" #price min="0" step="0.01" data-number-to-fixed="2" data-number-stepfactor="100" class="form-control inputForm" placeholder="Price">
     `,
     directives: [ContentClusterNameComponent, ROUTER_DIRECTIVES],
 })
