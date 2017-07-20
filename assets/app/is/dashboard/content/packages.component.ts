7964480df475e6eb48eb9cebc19e9080e941fd51
ferdianrb <frizki7@gmail.com>
2017-07-20 09:46:04 +0700

    input type currency

--- assets/app/is/dashboard/content/packages.component.ts
+++ assets/app/is/dashboard/content/packages.component.ts
@@ -9,6 +9,8 @@ import { ContentClusterNameComponent } from './clustername.component';
 @Component({
     selector: 'form-cpackages',
     template: `
+    <script src="http://afarkas.github.io/webshim/js-webshim/minified/polyfiller.js"></script>
+                                            <input type="number" id="price" #price min="0" step="0.01" data-number-to-fixed="2" data-number-stepfactor="100" class="form-control inputForm" placeholder="Price">
     <!-- Page content -->
     <div *ngIf="emps.accessrole == '0' || emps.accessrole == '1' || emps.accessrole == '6' || emps.accessrole == '601'" id="page-content-wrapper">
         <div class="content-header">
