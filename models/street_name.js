5a2c9e3031a2a88987b9d966dfe9069be01c4019
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-07-24 14:38:26 +0700

    update

--- models/street_name.js
+++ models/street_name.js
@@ -1,11 +1,12 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
-var BlokfloorSchema = require('../models/blokfloor');
+var Cluster = require('../models/cluster');
+//var BlokfloorSchema = require('../models/blokfloor');
 
 var StreetnameSchema   = new Schema({
     name: String,
     blok: String,
     cluster: {type: Schema.Types.ObjectId, ref: 'Cluster'},
-    blokfloor: {type: Schema.Types.ObjectId, ref: 'BlokfloorSchema'}
+    //blokfloor: {type: Schema.Types.ObjectId, ref: 'BlokfloorSchema'}
 });
 module.exports = mongoose.model('Streetname', StreetnameSchema)
