3c7aa0a204cf8c754a42b9dac99976b13b0fbb44
ferdianrb <frizki7@gmail.com>
2017-08-01 14:01:53 +0700

    virtual account substring

--- assets/app/is/boot.ts
+++ assets/app/is/boot.ts
@@ -1,5 +1,6 @@
 ///<reference path="../../../node_modules/angular2/typings/browser.d.ts"/>
 import {bootstrap} from 'angular2/platform/browser';
+import { SlicePipe } from 'angular2/common';
 import {AppComponent} from "./app.component";
 import {HTTP_PROVIDERS} from 'angular2/http';
 //import {GithubService} from './GithubService'
