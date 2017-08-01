ff9803d33da00262defc8ed6e42c15ee3af3456b
ferdianrb <frizki7@gmail.com>
2017-08-01 14:38:57 +0700

    update

--- assets/app/is/dashboard/content/coveragestreet.component.ts
+++ assets/app/is/dashboard/content/coveragestreet.component.ts
@@ -53,7 +53,7 @@ import { Street } from './street';
                                           <option value="0" disabled="true">-- Select Cluster --</option>
                                           <option *ngFor="#cluster of clusters" value={{cluster._id}}>{{ cluster.name }}</option>
                                       </select><br/><br/>
-                                    <input [ngFormControl]="myForm.find('blokname')" type="text" class="form-control inputForm" #streetblok id="streetblok" placeholder="Blok">
+                                    <input [ngFormControl]="myForm.find('streetblok')" type="text" class="form-control inputForm" #streetblok id="streetblok" placeholder="Blok">
                                     <input type="text" class="form-control inputForm" #streetname id="streetname" placeholder="Street Name">
                                     <div class="g-recaptcha" data-sitekey="6LdqYiMUAAAAAG24p30ejQSqeWdvTpD0DK4oj5wv"></div>
                                     <button [disabled]="!myForm.valid" type="submit" (click)="addBlock(streetname.value, streetblok.value, streetcluster.value)" class="btn btn-default buttonOrange">
