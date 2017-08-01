2a950a24b8077134f525a612e0be80be35a03aa1
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-08-01 10:47:59 +0700

    update

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -73,12 +73,12 @@ declare let kendo;
                                 </div>
                             </div>
                         </div><br />
-                        
-                        
-                        
-                        
-                        
-                        
+
+
+
+
+
+
                         <div class="row rowBillInfoContList" *ngIf="bills.status != 'Paid'">
                             <div class="col-md-12">
                                 <div class="row headerList">
@@ -122,11 +122,11 @@ declare let kendo;
                                 </div>
                             </div>
                         </div>
-                        
-                        
-                        
-                        
-                        
+
+
+
+
+
 
                         <div class="row rowBillInfoContList" *ngIf="clickedItem.name == 'regBill'">
                             <div class="col-md-12">
@@ -344,7 +344,7 @@ declare let kendo;
                                                 <h5>Subscriber hightlight information :</h5>
                                                 <ul>
                                                     <li style="margin-bottom: 10px;">1. Mohon melakukan pembayaran sebelum tanggal jatuh tempo untuk menghindari denda keterlambatan sejumlah Rp. 25.000,- (sebelum pajak) yang akan ditambahkan pada tagihan Anda di bulan berikutnya.</li>
-                                                    <li style="margin-bottom: 10px;">2. Pembayaran khusus nasabah BCA atau giro (bank lain dan BCA) ditunjukan ke Rekening Virtual BCA No. Rekening 02750<script>var subs_id={{bills.subid}}; echo"subs_id.substring(2,8)";</script> a/n GROOVY {{ bills.name }}.</li>
+                                                    <li style="margin-bottom: 10px;">2. Pembayaran khusus nasabah BCA atau giro (bank lain dan BCA) ditunjukan ke Rekening Virtual BCA No. Rekening 02750{{ bills.subid }} a/n GROOVY {{ bills.name }}.</li>
                                                     <li style="margin-bottom: 10px;">3. Cantumkan Subscriber ID pada saat pembayaran.</li>
                                                     <li style="margin-bottom: 10px;">4. Konfirmasi pembayaran ke email &nbsp; <a mailto="billing@groovy.id"><u>billing@groovy.id</u></a> &nbsp; atau telepon ke 021-5276616.</li>
                                                     <li style="margin-bottom: 10px;">5. Pembayaran diterima setelah adanya konfirmasi.</li>
