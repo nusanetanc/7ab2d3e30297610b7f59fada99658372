b3516b2ace9fc0d6d104819f8ee68b690c3e16ec
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-08-01 10:58:29 +0700

    update

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -335,7 +335,6 @@ declare let kendo;
                                                 <h5>Subscriber hightlight information :</h5>
                                                 <ul>
                                                     <li style="margin-bottom: 10px;">1. Mohon melakukan pembayaran sebelum tanggal jatuh tempo untuk menghindari denda keterlambatan sejumlah Rp. 25.000,- (sebelum pajak) yang akan ditambahkan pada tagihan Anda di bulan berikutnya.</li>
-                                                    <li style="margin-bottom: 10px;">2. Pembayaran khusus nasabah BCA atau giro (bank lain dan BCA) ditunjukan ke Rekening Virtual BCA No. Rekening 02750{{ bills.subid }} a/n GROOVY {{ bills.name }}.</li>
                                                     <li style="margin-bottom: 10px;">2. Pembayaran khusus nasabah BCA atau giro (bank lain dan BCA) ditunjukan ke Rekening Virtual BCA No. Rekening 02750{{bills.subid | slice:2:8}} a/n GROOVY {{ bills.name }}.</li>
                                                     <li style="margin-bottom: 10px;">3. Cantumkan Subscriber ID pada saat pembayaran.</li>
                                                     <li style="margin-bottom: 10px;">4. Konfirmasi pembayaran ke email &nbsp; <a mailto="billing@groovy.id"><u>billing@groovy.id</u></a> &nbsp; atau telepon ke 021-5276616.</li>
@@ -348,7 +347,6 @@ declare let kendo;
                                                 <h5>Subscriber hightlight information :</h5>
                                                 <ul>
                                                     <li style="margin-bottom: 10px;">1. Please make payment before due date to avoid late payment fee of Rp. 25.000,- (before VAT) which will be added to your next month's invoice.</li>
-                                                    <li style="margin-bottom: 10px;">2. Payment BCA customers or giro (another bank and BCA) is streetnameed to BCA Virtual Account No. 02750{{ bills.subid }} on behalf of GROOVY {{ bills.name }} (confirmation not required).</li>
                                                     <li style="margin-bottom: 10px;">2. Payment BCA customers or giro (another bank and BCA) is streetnameed to BCA Virtual Account No. 02750{{bills.subid | slice:2:8}} on behalf of GROOVY {{ bills.name }} (confirmation not required).</li>
                                                     <li style="margin-bottom: 10px;">3. Please quote your Subscriber ID during payment.</li>
                                                     <li style="margin-bottom: 10px;">4. Payment confirmation by sending an email to &nbsp; <a mailto="billing@groovy.id"><u>billing@groovy.id</u></a> &nbsp; or contact number 021-5276616.</li>
