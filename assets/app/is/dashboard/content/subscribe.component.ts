31c971a71de2ff997d5156ea6e7faf736e25f7cf
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-08-04 11:02:09 +0700

    tes

--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -500,6 +500,51 @@ import { ContentPackLevComponent } from './packlev.component';
                 </div> -->
 
             </div>
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h4>BILLING INFORMATION</h4>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="row marginTB10 marginL5">
+                            <div class="col-xs-6 col-sm-4">
+                                <span>Current Package</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-1">
+                                <span>:</span>
+                            </div>
+                            <div class="col-xs-12 col-md-7">
+                                <form-packlev *ngIf="subs.idpackage" [packid]=subs.idpackage></form-packlev>
+                            </div>
+                        </div>
+                        <div class="row marginTB10 marginL5">
+                            <div class="col-xs-6 col-sm-4">
+                                <span>Status</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-1">
+                                <span>:</span>
+                            </div>
+                            <div class="col-xs-12 col-md-7">
+                                <span class="green">{{ subs.status }}</span>
+                            </div>
+                        </div>
+                        <div class="row marginTB10 marginL5">
+                            <div class="col-xs-6 col-sm-4">
+                                <span>Virtual Account No.</span>
+                            </div>
+                            <div class="col-xs-6 col-sm-1">
+                                <span>:</span>
+                            </div>
+                            <div class="col-xs-12 col-md-7">
+                                <span>{{ subs.nova }}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     `,
