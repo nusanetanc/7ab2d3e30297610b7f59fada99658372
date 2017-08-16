ab99e946772e3bc34e91c00fb753295bbd5b9d11
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-08-16 09:40:57 +0700

    tes

--- assets/app/is/dashboard/content/detailbilling.component.ts
+++ assets/app/is/dashboard/content/detailbilling.component.ts
@@ -859,7 +859,7 @@ export class ContentDetailBillingComponent implements OnInit {
             })
     }
     AddPay(pinaltyInput, paydateInput, pinaltyBy) {
-        var body = `paydate=${paydateInput}&pinaltypay=${pinaltyInput}&payby=${pinaltyBy}&secretkey="4b0b09d8941d66b5bbcb3a4ca3eaa296"`;
+        var body = `paydate=${paydateInput}&pinaltypay=${pinaltyInput}&payby=${pinaltyBy}&secretkey='4b0b09d8941d66b5bbcb3a4ca3eaa296'`;
         var headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded');
         this.http
