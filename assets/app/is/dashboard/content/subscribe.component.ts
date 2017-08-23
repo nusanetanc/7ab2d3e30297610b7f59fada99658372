4ac47a9fe1df951c824740df588f2d0c468bf80a
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-08-23 14:51:45 +0700

    tes

--- assets/app/is/dashboard/content/subscribe.component.ts
+++ assets/app/is/dashboard/content/subscribe.component.ts
@@ -324,7 +324,7 @@ import { ContentPackLevComponent } from './packlev.component';
                                 </div>
                             </div>
                             <div class="col-sm-12">
-                              <button (click)="editSubs(editname.value, editemail.value, editphone.value, editid.value, editwifiid.value, editvendor.value,  editipaddr.value, bandwidth.value, editbrithdate.value, editregisby.value, editregisref.value, editregissales.value)" type="submit" class="btn btn-default buttonOrange">
+                              <button (click)="editSubs(editname.value, editemail.value, editphone.value, editid.value, editwifiid.value, editvendor.value,  editipaddr.value, editbandwidth.value, editbrithdate.value, editregisby.value, editregisref.value, editregissales.value)" type="submit" class="btn btn-default buttonOrange">
                                   SUBMIT
                               </button>
                               <button (click)="onItemClicked1(Cancel)" class="btn btn-default buttonOrange">
@@ -735,8 +735,8 @@ onSelectStreet(_id) {
                 console.log(JSON.stringify(error.json()));
             });
     }
-    editSubs(editname, editemail, editphone, editid, editwifiid, editvendor, editipaddr, bandwidth, editbrithdate, editregisby, editregisref, editregissales) {
-        var body = `name=${editname}&email=${editemail}&phone=${editphone}&idnumber=${editid}&wifiid=${editwifiid}&vendorfo=${editvendor}&ipaddr=${editipaddr}&datebirth=${editbrithdate}&regisby=${editregisby}&regisref=${editregisref}&sales=${editregissales}`;
+    editSubs(editname, editemail, editphone, editid, editwifiid, editvendor, editipaddr, editbandwidth, editbrithdate, editregisby, editregisref, editregissales) {
+        var body = `name=${editname}&email=${editemail}&phone=${editphone}&idnumber=${editid}&wifiid=${editwifiid}&vendorfo=${editvendor}&ipaddr=${editipaddr}&bandwidth=${editbandwidth}&datebirth=${editbrithdate}&regisby=${editregisby}&regisref=${editregisref}&sales=${editregissales}`;
         var headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded');
         this.http
